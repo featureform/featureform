4df0745f32b83bad82b38ddda9b2bd613c6ec205
Simba <simba@streamsql.io>
2020-10-20 03:28:21 -0700

    Add initial Python client for EmbeddingStore
    EmbeddingStoreClient is a thin wrapper over the gRPC stub. When run as a
    binary, set and get commands can be issued through stdin.

---
+++ client/client.py
@@ -0,0 +1,51 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v.2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import grpc
+import sys
+
+from embeddingstore import embedding_store_pb2, embedding_store_pb2_grpc
+
+
+class EmbeddingStoreClient:
+
+    def __init__(self, host="localhost", port=50051):
+        connection_str = "%.%".format(host, port)
+        self._channel = grpc.insecure_channel('localhost:50051')
+        self._stub = embedding_store_pb2_grpc.EmbeddingStoreStub(self._channel)
+
+    def close():
+        return self._channel.close()
+
+    def set(self, key, embedding):
+        req = SetRequest()
+        req.key = key
+        req.embedding.values[:] = embedding
+        self._stub.Set(req)
+
+    def get(self, key):
+        resp = self._stub.Get(embedding_store_pb2.GetRequest(key=key))
+        return resp.embedding.values
+
+
+# When run, it takes a command per line:
+# Either:
+# get [key]
+# OR
+# set [key] [values...]
+if __name__ == '__main__':
+    client = EmbeddingStoreClient()
+    for line in sys.stdin:
+        line = line.rstrip()
+        tokens = line.split()
+        cmd = tokens[0]
+        key = tokens[1]
+        if cmd == "get":
+            print(client.get(key))
+        elif cmd == "set":
+            val = [float(tok) for tok in tokens[2:]]
+            client.set(key, val)
+            print("SUCCESS")
+        else:
+            print("UNKNOWN COMMAND")

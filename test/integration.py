77cfdb77c7c88d6a1e5d64bc8e9a3502224a99af
Simba <simba@streamsql.io>
2020-10-22 14:57:05 -0700

    Add initial integration test
    The integration test runs the embedding_server in a separate process and
    uses the Python client to interact with it.

---
+++ test/integration.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v.2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import subprocess
+import sys
+import os
+import pytest
+
+import client.client as es
+
+@pytest.fixture
+def embedding_store_proc():
+    proc = subprocess.Popen(os.environ["TEST_SRCDIR"] + "/__main__/embeddingstore/main")
+    yield proc
+    proc.kill()
+
+@pytest.fixture
+def es_client(embedding_store_proc):
+    client = es.EmbeddingStoreClient()
+    yield client
+    client.close()
+
+def test_set_get(es_client):
+    emb = [1, 2, 3]
+    es_client.set("a", emb);
+    assert es_client.get("a") == emb
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__]))

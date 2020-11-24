13a14a135946001da834f430a5a989cf7a6c2bb0
Simba <simba@streamsql.io>
2020-11-24 11:14:38 -0800

    Add storage module
    The storage module handles serializing and storing embeddings. It can be
    extended in the future to work on remote server storage and more.
    
    The current version serializes embeddings into protobuf and stores them
    in rocksdb.

---
+++ embeddingstore/storage.cc
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "storage.h"
+
+#include "embeddingstore/embedding_store.grpc.pb.h"
+#include "rocksdb/db.h"
+
+namespace featureform {
+
+namespace embedding {
+
+std::unique_ptr<EmbeddingStorage> EmbeddingStorage::New(std::string path, int dims) {
+  rocksdb::Options options;
+  options.create_if_missing = true;
+  rocksdb::DB* db_ptr;
+  rocksdb::Status status = rocksdb::DB::Open(options, path, &db_ptr);
+  std::unique_ptr<rocksdb::DB> db(db_ptr);
+  return std::unique_ptr<EmbeddingStorage>(new EmbeddingStorage(std::move(db), dims));
+}
+
+EmbeddingStorage::EmbeddingStorage(std::unique_ptr<rocksdb::DB> db, int dims)
+    : db_(std::move(db)), dims_(dims) {
+}
+
+void EmbeddingStorage::set(std::string key, std::vector<float> val) {
+  auto proto = proto::Embedding();
+  *proto.mutable_values() = {val.begin(), val.end()};
+  std::string serialized;
+  proto.SerializeToString(&serialized);
+  db_->Put(rocksdb::WriteOptions(), key, serialized);
+}
+
+std::vector<float> EmbeddingStorage::get(const std::string& key) const {
+  std::string serialized;
+  db_->Get(rocksdb::ReadOptions(), key, &serialized);
+  auto proto = proto::Embedding();
+  proto.ParseFromString(serialized);
+  auto vals = proto.values();
+  return std::vector<float>(vals.begin(), vals.end());
+}
+
+}
+}
---
+++ embeddingstore/storage.h
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include <memory>
+
+#include "rocksdb/db.h"
+
+namespace featureform {
+
+namespace embedding {
+
+class EmbeddingStorage {
+ public:
+  static std::unique_ptr<EmbeddingStorage> New(std::string path, int dims);
+  EmbeddingStorage() = delete;
+  void set(std::string key, std::vector<float> value);
+  std::vector<float> get(const std::string& key) const;
+ private:
+  EmbeddingStorage(std::unique_ptr<rocksdb::DB> DB, int dims);
+  std::unique_ptr<rocksdb::DB> db_;
+  int dims_;
+};
+}
+}

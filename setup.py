5e4a6ee2dfef022ff342a5e14ffe28ab2ab421a0
Simba Khadder <simba@featureform.io>
2021-06-04 14:24:22 -0700

    Create PyPI package
    Creates all the necessary files needed for the PyPI package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="embeddingstore",
+    version="0.0.1",
+    author="featureform",
+    author_email="hello@featureform.com",
+    description="Data infrastructure for machine learning embeddings.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/featureform/embeddingstore",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "client"},
+    packages=setuptools.find_packages(where="client"),
+    python_requires=">=3.6",
+)

effae75f03de15663896ca8f48154b143aa6b080
shabbyjoon <32000779+shabbyjoon@users.noreply.github.com>
2021-06-07 17:24:44 -0700

    Clean up docstrings, update napoleon config

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ html_static_path = ['_static']
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = True
+napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False

e8b6bc463560cae1e9612bc2d05fee94326d1caa
Simba Khadder <simba@featureform.io>
2021-06-10 10:46:43 -0700

    Use python -m yapf instead of yapf directly.
    This allows lint and format to work even if pip binaries aren't in the path.

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 find . -type f | grep -e '.*\.cc' -e '.*\.h' | xargs clang-format -i --sort-includes -style=google;
-find . -type f | grep -e '.py$' | xargs yapf -i --style google;
+find . -type f | grep -e '.py$' | xargs python -m yapf -i --style google;
 bazelisk run :buildifier
--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 find . -type f | grep -e '.*\.cc' -e '.*\.h' | xargs clang-format --sort-includes -style=google -Werror -n;
-find . -type f | grep -e '.py$' | xargs yapf --style google -q;
+find . -type f | grep -e '.py$' | xargs python -m yapf --style google -q;
 bazelisk test :buildifier_test

27c1b61c4540fd236da75af462019c7075bc25a8
shabbyjoon <32000779+shabbyjoon@users.noreply.github.com>
2021-06-11 15:20:02 -0500

    Add additional check for linux OS and clean up lines

--- rocksdb_install.sh
+++ rocksdb_install.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then 
-        sudo apt-get install libgflags-dev
-        sudo apt-get install libsnappy-dev
-        sudo apt-get install zlib1g-dev
-        sudo apt-get install libbz2-dev
-        sudo apt-get install liblz4-dev
-        sudo apt-get install libzstd-dev
+if [[ "$OSTYPE" == "linux-gnu"* ]] && [[-x "$(command -v apt-get)" ]]]; then 
+        sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         brew deps --tree --1 rocksdb
 else

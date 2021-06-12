10fc9b5092e1f51fe8100a351f9d8048be68c1ed
shabbyjoon <32000779+shabbyjoon@users.noreply.github.com>
2021-06-11 19:54:46 -0500

    Add space to command

--- rocksdb_install.sh
+++ rocksdb_install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ "$OSTYPE" == "linux-gnu"* ]] && [[-x "$(command -v apt-get)" ]]; then 
+if [[ "$OSTYPE" == "linux-gnu"* ]] && [[ -x "$(command -v apt-get)" ]]; then 
         sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         brew install gflags lz4 snappy zstd

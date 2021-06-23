7774779d2268cc86453c806ddcadf9ca917fcd07
Simba Khadder <simba@featureform.com>
2021-06-23 12:03:25 -0700

    Add immutability to spaces
    We include a method to make spaces immutable and allow Set to return
    an Error.

--- embeddingstore/space.cc
+++ embeddingstore/space.cc
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <memory>
 
+#include "error.h"
 #include "rocksdb/db.h"
 
 namespace featureform {
@@ -15,24 +16,37 @@ namespace featureform {
 namespace embedding {
 
 std::shared_ptr<Space> Space::load_or_create(std::string path, std::string name,
-                                             int dims) {
+                                             int dims, bool immutable) {
   auto storage = EmbeddingStorage::load_or_create(path, dims);
-  return std::unique_ptr<Space>(new Space(std::move(storage), name, dims));
+  return std::unique_ptr<Space>(
+      new Space(std::move(storage), name, dims, immutable));
 }
 
 Space::Space(std::shared_ptr<EmbeddingStorage> storage, std::string name,
-             int dims)
-    : storage_{std::move(storage)}, name_{name}, dims_{dims}, idx_{nullptr} {}
+             int dims, bool immutable)
+    : storage_{std::move(storage)},
+      name_{name},
+      dims_{dims},
+      immutable_{immutable},
+      idx_{nullptr} {}
 
 std::string Space::name() const { return name_; }
 
 int Space::dims() const { return dims_; }
 
-void Space::set(std::string key, std::vector<float> val) {
+bool Space::immutable() const { return immutable_; }
+
+void Space::make_immutable() { immutable_ = true; }
+
+Error Space::set(std::string key, std::vector<float> val) {
+  if (immutable_) {
+    return UpdateImmutableSpaceError::create(name_);
+  }
   storage_->set(key, val);
   if (idx_ != nullptr) {
     idx_->set(key, val);
   }
+  return nullptr;
 }
 
 std::vector<float> Space::get(const std::string& key) const {

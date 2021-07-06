a5aba3f78915a8ee6a453f451e3e17288046c1bb
shabbyjoon <32000779+shabbyjoon@users.noreply.github.com>
2021-07-06 14:32:52 -0700

    Update embedding_store naming from store to hub

--- embeddingstore/embedding_store.cc
+++ embeddingstore/embedding_store.cc
@@ -13,7 +13,7 @@ namespace featureform {
 
 namespace embedding {
 
-std::shared_ptr<EmbeddingStore> EmbeddingStore::load_or_create(
+std::shared_ptr<EmbeddingHub> EmbeddingHub::load_or_create(
     std::string path) {
   std::filesystem::path metadata_path = path;
   std::filesystem::create_directories(path);
@@ -23,15 +23,15 @@ std::shared_ptr<EmbeddingStore> EmbeddingStore::load_or_create(
   rocksdb::DB* db_ptr;
   rocksdb::Status status = rocksdb::DB::Open(options, metadata_path, &db_ptr);
   std::unique_ptr<rocksdb::DB> db(db_ptr);
-  return std::shared_ptr<EmbeddingStore>(
-      new EmbeddingStore(metadata_path, std::move(db)));
+  return std::shared_ptr<EmbeddingHub>(
+      new EmbeddingHub(metadata_path, std::move(db)));
 }
 
-EmbeddingStore::EmbeddingStore(std::filesystem::path base_path,
+EmbeddingHub::EmbeddingHub(std::filesystem::path base_path,
                                std::unique_ptr<rocksdb::DB> db)
     : base_path_{base_path}, db_{std::move(db)}, loaded_spaces_{} {}
 
-std::shared_ptr<Space> EmbeddingStore::create_space(const std::string& name) {
+std::shared_ptr<Space> EmbeddingHub::create_space(const std::string& name) {
   if (is_space_loaded(name)) {
     return loaded_spaces_.at(name);
   }
@@ -45,7 +45,7 @@ std::shared_ptr<Space> EmbeddingStore::create_space(const std::string& name) {
   return Space::load_or_create(path, name);
 }
 
-std::optional<std::shared_ptr<Space>> EmbeddingStore::get_space(
+std::optional<std::shared_ptr<Space>> EmbeddingHub::get_space(
     const std::string& name) {
   if (is_space_loaded(name)) {
     return loaded_spaces_.at(name);
@@ -62,7 +62,7 @@ std::optional<std::shared_ptr<Space>> EmbeddingStore::get_space(
   return std::optional{space};
 }
 
-bool EmbeddingStore::is_space_loaded(const std::string& name) const {
+bool EmbeddingHub::is_space_loaded(const std::string& name) const {
   return loaded_spaces_.find(name) != loaded_spaces_.end();
 }
 

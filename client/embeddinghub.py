081121fb98aa339c4fb040f8ab4b4d727a7673e8
Simba Khadder <simba@featureform.com>
2021-07-16 17:47:27 -0700

    Make calls be optionally async in the client

--- client/embeddinghub.py
+++ client/embeddinghub.py
@@ -12,7 +12,9 @@ Example:
     |  assert client.get("a") == [1, 2, 3]
 """
 
+import concurrent.futures
 import grpc
+from collections.abc import Mapping
 from client import embedding_store_pb2_grpc
 from client import embedding_store_pb2
 
@@ -36,7 +38,7 @@ class EmbeddingHubClient:
         """
         return self._channel.close()
 
-    def create_space(self, name, dims):
+    def create_space(self, name, dims, wait=True):
         """Create a new space in the embedding hub.
 
         A space is essentially a table. It can contain multiple different
@@ -47,13 +49,20 @@ class EmbeddingHubClient:
             name: The name of the space to create.
             dims: The number of dimensions that an embedding in the space will
             have.
+            wait: A bool which specifies if the call should be synchronous.
+
+        Returns:
+            A future if wait is False.
         """
         req = embedding_store_pb2.CreateSpaceRequest()
         req.name = str(name)
         req.dims = dims
-        self._stub.CreateSpace(req)
+        future = self._stub.CreateSpace.future(req)
+        if wait:
+            return future.result()
+        return future
 
-    def freeze_space(self, name):
+    def freeze_space(self, name, wait=True):
         """Make an existing space immutable.
 
         After this call, the space cannot be updated. This call cannot be
@@ -61,12 +70,19 @@ class EmbeddingHubClient:
 
         Args:
             name: The name of the space to freeze.
+            wait: A bool which specifies if the call should be synchronous.
+
+        Returns:
+            A future if wait is False.
         """
         req = embedding_store_pb2.FreezeSpaceRequest()
         req.name = str(name)
-        self._stub.FreezeSpace(req)
+        future = self._stub.FreezeSpace.future(req)
+        if wait:
+            return future.result()
+        return future
 
-    def set(self, space, key, embedding):
+    def set(self, space, key, embedding, wait=True):
         """Set key to embedding on the server.
 
         Sets an embedding record with a key.
@@ -77,33 +93,46 @@ class EmbeddingHubClient:
             space: The name of the space to write to.
             key: The embedding index for retrieval.
             embedding: A python list of the embedding vector to be stored. 
+            wait: A bool which specifies if the call should be synchronous.
+
+        Returns:
+            A future if wait is False.
         """
         req = embedding_store_pb2.SetRequest()
         req.space = str(space)
         req.key = str(key)
         req.embedding.values[:] = embedding
-        try:
-            self._stub.Set(req)
-        except grpc.RpcError as e:
-            if e.code() == grpc.StatusCode.FAILED_PRECONDITION:
-                raise TypeError(e.details())
-            raise
+        future = self._stub.Set.future(req)
+        if wait:
+            try:
+                future.result()
+            except grpc.RpcError as e:
+                if e.code() == grpc.StatusCode.FAILED_PRECONDITION:
+                    raise TypeError(e.details())
+                raise
+        return future
 
-    def get(self, space, key):
+    def get(self, space, key, wait=True):
         """Retrieves an embedding record from the server.
     
         Args:
             space: The name of the space to write to.
             key: The embedding index for retrieval.
+            wait: A bool which specifies if the call should be synchronous.
 
         Returns:
-            An embedding, which is a python list of floats.
+            An embedding, which is a python list of floats. If wait is False,
+            the value will be wrapped in a future.
         """
-        resp = self._stub.Get(
-            embedding_store_pb2.GetRequest(space=str(space), key=str(key)))
-        return resp.embedding.values
+        req = embedding_store_pb2.GetRequest(space=str(space), key=str(key))
+        future = self._stub.Get.future(req)
+        transform_fn = lambda res: res.embedding.values
+        wrapped_future = FutureTransformWrapper(future, transform_fn)
+        if wait:
+            return wrapped_future.result()
+        return wrapped_future
 
-    def multiset(self, space, embedding_dict):
+    def multiset(self, space, embedding_tuples):
         """Set multiple embeddings at once.
 
         Take a dictionary of key embedding pairs and set them on the server.
@@ -111,10 +140,13 @@ class EmbeddingHubClient:
 
         Args:
             space: The name of the space to write to.
-            embedding_dict: A dictionary from key to embedding, where key is a string and 
-            embedding is a python list.
+            embedding_tuples: A dictionary from key to embedding or an iterator
+            of key, embedding pairs where key is a string and embedding is a
+            python list.
         """
-        it = self._embedding_dict_iter(space, embedding_dict)
+        if isinstance(embedding_tuples, Mapping):
+            embedding_tuples = embedding_tuples.items()
+        it = self._embedding_tuples_iter(space, embedding_tuples)
         self._stub.MultiSet(it)
 
     def multiget(self, space, keys):
@@ -124,7 +156,7 @@ class EmbeddingHubClient:
 
         Args:
             space: The name of the space to get from.
-            keys: A list of embedding indices for retrieval.
+            keys: An iterator of embedding indices for retrieval.
 
         Returns:
             A list of embeddings.
@@ -132,34 +164,44 @@ class EmbeddingHubClient:
         it = self._key_iter(space, keys)
         return self._embedding_iter(self._stub.MultiGet(it))
 
-    def nearest_neighbor(self, space, key, num):
+    def nearest_neighbor(self, space, key, num, wait=False):
         """Finds N nearest neighbors for a given embedding record.
 
         Args:
             space: The name of the space to retrieve from.
             key: The embedding index for retrieval.
             num: The number of nearest neighbors.
+            wait: A bool which specifies if the call should be synchronous.
 
         Returns:
-            A num size list of embedding vectors that are closest to the provided vector embedding.
+            A num size list of embedding vectors that are closest to the
+            provided vector embedding. If wait is False, the value will
+            be wrapped in a future.
         """
         req = embedding_store_pb2.NearestNeighborRequest(space=str(space),
                                                          key=str(key),
                                                          num=num)
-        return self._stub.NearestNeighbor(req).keys
+        future = self._stub.NearestNeighbor.future(req)
+        transform_fn = lambda res: res.keys
+        wrapped_future = FutureTransformWrapper(future, transform_fn)
+        if wait:
+            return wrapped_future.result()
+        return wrapped_future
 
-    def _embedding_dict_iter(self, space, embedding_dict):
-        """Create a MultiSetRequest iterator from a space and an embedding dict.
+    def _embedding_tuples_iter(self, space, it):
+        """Create a MultiSetRequest iterator from a space and an iterator of
+        key-embedding tuples.
 
         Args:
             space: The name of the space to set in the MultiSetRequests.
-            embedding_dict: A dictionary from key to embedding, where key is a string and 
-            embedding is a python list. These will be transformed into MutliSetRequests.
+            it: An iterator of key, embedding tuples, where key is a string and
+            embedding is a python list. These will be transformed into
+            MultiSetRequests.
 
         Returns:
             An iterator of MultiSetRequest.
         """
-        for key, embedding in embedding_dict.items():
+        for key, embedding in it:
             req = embedding_store_pb2.MultiSetRequest()
             req.space = str(space)
             req.key = str(key)
@@ -193,3 +235,35 @@ class EmbeddingHubClient:
         """
         for resp in resps:
             yield resp.embedding.values
+
+
+class FutureTransformWrapper:
+    """A wrapper around a future that runs transform_fn on the result.
+
+    Some libraries like gRPC return futures. This class allows us to return a
+    future that does a simple transformation on the result, and is equivalent
+    to the wrapped future in all other ways.
+    """
+
+    def __init__(self, future, transform_fn):
+        self._future = future
+        self._transform = transform_fn
+
+    def __getattr__(self, attr):
+        """Pass all other attribute access to the inner _future object.
+        """
+        return getattr(self._future, attr)
+
+    def result(self, timeout=None):
+        raw_result = self._future.result(timeout)
+        return self._transform(raw_result)
+
+    def add_done_callback(self, fn):
+
+        def wrapped_callback_fn(fut):
+            unwrapped_val = self._transform(fut.result())
+            inner_fut = concurrent.futures.Future()
+            inner_fut.set_result(unwrapped_val)
+            fn(inner_fut)
+
+        self._future.add_done_callback(wrapped_callback_fn)

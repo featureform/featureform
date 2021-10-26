b0dfc99c097b0e1d5ccb8006fbd119d9005dfd83
Sam Inloes <sam@featureform.com>
2021-10-26 15:36:07 -0700

    homepage styling

--- dashboard/src/components/tilelinks/TilePanel.js
+++ dashboard/src/components/tilelinks/TilePanel.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Tile from "./Tile.js";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    opacity: 0,
+  },
+}));
 const TilePanel = ({ sections }) => {
+  let classes = useStyles();
   return (
     <div>
       <Grid container justifyContent="center" lg={12}>
         {sections.map((section, i) => {
           return (
-            <Grid item xs={4} lg={4} key={`tile-grid-${i}`}>
-              <Tile detail={section} key={`tile-${i}`} />
+            <Grid item xs={2} lg={2} key={`tile-grid-${i}`}>
+              <Tile detail={section} key={`tile-${i}`} id={i} />
             </Grid>
           );
         })}

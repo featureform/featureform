8e976c41059e6ec1b4e13196787dcdb2544284c3
Sam Inloes <sam@featureform.com>
2021-10-26 14:29:09 -0700

    add radial gradient background

--- dashboard/src/styles/theme/index.js
+++ dashboard/src/styles/theme/index.js
@@ -6,7 +6,22 @@ export const PURPLE = "#8e44ad";
 export const FEATUREFORM_RED = "#f7195c";
 
 const themeSpec = {
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          background:
+            "radial-gradient(circle farthest-side at 100% 55%, #E4D0FA 0%, transparent 40%), radial-gradient(circle farthest-side at 10% 140%, #F7195C 0%, transparent 40%);",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+        },
+      },
+    },
+  },
   palette: {
+    background: {
+      default: "#FFFFFF",
+    },
     primary: { main: FEATUREFORM_RED, contrastText: "#FFFFFF" },
     secondary: { main: PURPLE },
     text: {

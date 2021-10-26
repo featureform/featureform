6fde9d34a0fe38dddc582d9f58a1c1460bf5aa8d
Sam Inloes <sam@featureform.com>
2021-10-26 16:35:47 -0700

    change entity page styling

--- dashboard/src/components/entitypage/EntityPageView.js
+++ dashboard/src/components/entitypage/EntityPageView.js
@@ -46,8 +46,23 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0),
     backgroundColor: theme.palette.background.paper,
   },
+  border: {
+    background: "white",
+    border: "2px solid #F5F6F7",
+    borderRadius: "16px",
+  },
+  data: {
+    background: "white",
+    border: "2px solid #F5F6F7",
+    borderRadius: "16px",
+  },
+  appbar: {
+    background: "transparent",
+    boxShadow: "none",
+    color: "black",
+  },
   metadata: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
   },
   small: {
     width: theme.spacing(3),
@@ -73,8 +88,8 @@ const useStyles = makeStyles((theme) => ({
   },
 
   resourceData: {
-    flexGrow: 2,
-    padding: theme.spacing(2),
+    flexGrow: 1,
+    padding: theme.spacing(0),
   },
 }));
 
@@ -124,131 +139,127 @@ const EntityPageView = ({ entity, setVersion, activeVersions }) => {
 
   return true || (!resources.loading && !resources.failed && resources.data) ? (
     <div>
-      <Container maxWidth="xl">
-        <Paper elevation={3}>
-          <div className={classes.metadata}>
-            <Grid
-              container
-              className={classes.topContainer}
-              lg={12}
-              justifyContent="flex-start"
-            >
-              <Grid item xs={false} className={classes.icon}>
-                <Icon>{icon}</Icon>
-              </Grid>
-              <Grid item xs={9} lg={8}>
-                <Typography variant="h4" component="h4">
-                  {resources.name}
-                </Typography>
-                <Typography variant="subtitle1">
-                  Last updated: {metadata["revision"]}
-                </Typography>
-              </Grid>
+      <Container maxWidth="xl" className={classes.border}>
+        <div className={classes.metadata}>
+          <Grid
+            container
+            className={classes.topContainer}
+            lg={12}
+            justifyContent="flex-start"
+          >
+            <Grid item xs={false} className={classes.icon}>
+              <Icon>{icon}</Icon>
+            </Grid>
+            <Grid item xs={9} lg={8}>
+              <Typography variant="h4" component="h4">
+                {resources.name}
+              </Typography>
+              <Typography variant="subtitle1">
+                Last updated: {metadata["revision"]}
+              </Typography>
+            </Grid>
 
-              <Grid item xs={2} className={classes.versionControl}>
-                <VersionControl
-                  version={version}
-                  versions={allVersions}
-                  handleVersionChange={handleVersionChange}
-                  type={type}
-                  name={name}
-                />
+            <Grid item xs={2} className={classes.versionControl}>
+              <VersionControl
+                version={version}
+                versions={allVersions}
+                handleVersionChange={handleVersionChange}
+                type={type}
+                name={name}
+              />
+            </Grid>
+          </Grid>
+          <div className={classes.resourceData}>
+            <Grid container spacing={0}>
+              <Grid item xs={7}>
+                <Typography variant="body1" className={classes.description}>
+                  {metadata["description"]}
+                </Typography>
+
+                <div className={classes.titleBox}>
+                  <Typography display="inline" variant="body1">
+                    Owner:{"  "}
+                  </Typography>
+                  <Avatar
+                    alt={metadata["owner"]}
+                    src="/static/images/avatar/1.jpg"
+                    className={classes.small}
+                  />
+                </div>
+                <div className={classes.owner}></div>
+                {metadata["dimensions"] ? (
+                  <Typography variant="body1">
+                    Dimensions: {metadata["dimensions"]}
+                  </Typography>
+                ) : (
+                  <Typography></Typography>
+                )}
+                {metadata["source"] ? (
+                  <Typography variant="body1">
+                    Source: {metadata["source"]}
+                  </Typography>
+                ) : (
+                  <Typography></Typography>
+                )}
+
+                {metadata["entity"] ? (
+                  <Typography variant="body1">
+                    Entity: {metadata["entity"]}
+                  </Typography>
+                ) : (
+                  <Typography></Typography>
+                )}
+              </Grid>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={3}>
+                <TagBox tags={metadata["tags"]} />
               </Grid>
             </Grid>
-            <Paper>
-              <div className={classes.resourceData}>
-                <Grid container spacing={0}>
-                  <Grid item xs={7}>
-                    <Typography variant="body1" className={classes.description}>
-                      {metadata["description"]}
-                    </Typography>
-
-                    <div className={classes.titleBox}>
-                      <Typography display="inline" variant="body1">
-                        Owner:{"  "}
-                      </Typography>
-                      <Avatar
-                        alt={metadata["owner"]}
-                        src="/static/images/avatar/1.jpg"
-                        className={classes.small}
-                      />
-                    </div>
-                    <div className={classes.owner}></div>
-                    {metadata["dimensions"] ? (
-                      <Typography variant="body1">
-                        Dimensions: {metadata["dimensions"]}
-                      </Typography>
-                    ) : (
-                      <Typography></Typography>
-                    )}
-                    {metadata["source"] ? (
-                      <Typography variant="body1">
-                        Source: {metadata["source"]}
-                      </Typography>
-                    ) : (
-                      <Typography></Typography>
-                    )}
-
-                    {metadata["entity"] ? (
-                      <Typography variant="body1">
-                        Entity: {metadata["entity"]}
-                      </Typography>
-                    ) : (
-                      <Typography></Typography>
-                    )}
-                  </Grid>
-                  <Grid item xs={2}></Grid>
-                  <Grid item xs={3}>
-                    <TagBox tags={metadata["tags"]} />
-                  </Grid>
-                </Grid>
-              </div>
-            </Paper>
           </div>
-          <div className={classes.root}>
-            <Paper>
-              <AppBar position="static">
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  aria-label="simple tabs example"
-                >
-                  <Tab label={"metrics"} {...a11yProps(0)} />
-                  {Object.keys(resourceData).map((key, i) => (
-                    <Tab label={key} {...a11yProps(i + 1)} />
-                  ))}
-                </Tabs>
-              </AppBar>
-              <TabPanel
-                className={classes.tabChart}
+        </div>
+        <div className={classes.root}>
+          <Container className={classes.data}>
+            <AppBar position="static" className={classes.appbar}>
+              <Tabs
                 value={value}
-                key={"metrics"}
-                index={0}
+                onChange={handleChange}
+                aria-label="simple tabs example"
               >
-                <MetricsDropdown />
-              </TabPanel>
+                <Tab label={"metrics"} {...a11yProps(0)} />
+                {Object.keys(resourceData).map((key, i) => (
+                  <Tab label={key} {...a11yProps(i + 1)} />
+                ))}
+              </Tabs>
+            </AppBar>
+            <TabPanel
+              className={classes.tabChart}
+              value={value}
+              key={"metrics"}
+              index={0}
+            >
+              <MetricsDropdown />
+            </TabPanel>
 
-              {Object.keys(resourceData).map((key, i) => (
-                <TabPanel value={value} key={key} index={i + 1}>
-                  <MaterialTable
-                    title={capitalize(key)}
-                    options={{
-                      toolbar: false,
-                    }}
-                    columns={Object.keys(resourceData[key][0]).map((item) => ({
-                      title: capitalize(item),
-                      field: item,
-                    }))}
-                    data={resourceData[key].map((o) => ({ ...o }))}
-                    onRowClick={(event, rowData) =>
-                      history.push("/" + key + "/" + rowData.name)
-                    }
-                  />
-                </TabPanel>
-              ))}
-            </Paper>
-          </div>
-        </Paper>
+            {Object.keys(resourceData).map((key, i) => (
+              <TabPanel value={value} key={key} index={i + 1}>
+                <MaterialTable
+                  title={capitalize(key)}
+                  options={{
+                    toolbar: false,
+                  }}
+                  columns={Object.keys(resourceData[key][0]).map((item) => ({
+                    title: capitalize(item),
+                    field: item,
+                  }))}
+                  data={resourceData[key].map((o) => ({ ...o }))}
+                  onRowClick={(event, rowData) =>
+                    history.push("/" + key + "/" + rowData.name)
+                  }
+                />
+              </TabPanel>
+            ))}
+          </Container>
+        </div>
       </Container>
     </div>
   ) : (

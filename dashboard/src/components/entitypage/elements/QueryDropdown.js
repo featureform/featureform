57c52f289ba79035ce519634cb4da3d33ee50a42
Sam Inloes <sam@featureform.com>
2021-11-23 19:55:17 -0500

    add reducer to generate query based on additional labels

--- dashboard/src/components/entitypage/elements/QueryDropdown.js
+++ dashboard/src/components/entitypage/elements/QueryDropdown.js
@@ -2,11 +2,16 @@ import { Container } from "@material-ui/core/";
 
 import PrometheusGraph from "./PrometheusGraph";
 import React from "react";
-function QueryDropdown({ type, name }) {
+function QueryDropdown({ type, name, query_type, add_labels }) {
   return (
     <div>
       <Container>
-        <PrometheusGraph type={type} name={name} />
+        <PrometheusGraph
+          type={type}
+          name={name}
+          query_type={query_type}
+          add_labels={add_labels}
+        />
       </Container>
     </div>
   );

7a46415e1deb7d5572206c3d7132d3edde365081
Sam Inloes <sam@featureform.com>
2021-12-03 14:11:27 -0800

    add time dropdown

--- dashboard/src/components/entitypage/elements/PrometheusGraph.js
+++ dashboard/src/components/entitypage/elements/PrometheusGraph.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { useEffect } from "react";
 import { Chart } from "chart.js";
+import { makeStyles } from "@material-ui/core/styles";
+
 import { connect } from "react-redux";
+
+const useStyles = makeStyles((theme) => ({
+  graphBox: {
+    height: "30%",
+  },
+}));
 
 const minutesToMilliseconds = (minutes) => {
   return parseInt(minutes * 60 * 1000);
@@ -17,13 +25,14 @@ const PrometheusGraph = ({
   query_type,
   add_labels,
 }) => {
+  const classes = useStyles();
   const add_labels_string = add_labels
     ? Object.keys(add_labels).reduce(
         (acc, label) => `${acc} ${label}:"${add_labels[label]}"`,
         ""
       )
     : "";
-  const query_string = `http_request_${query_type}{type="${type}" name="${name} ${add_labels_string}}`;
+
   useEffect(() => {
     var myChart = new Chart(chartRef.current, {
       type: "line",
@@ -37,7 +46,7 @@ const PrometheusGraph = ({
             prometheus: {
               endpoint: "http://localhost:9090",
             },
-            query: query_string,
+            query: `${query}${add_labels_string}`,
             timeRange: {
               type: "relative",
               //timestamps in miliseconds relative to current time.
@@ -53,12 +62,20 @@ const PrometheusGraph = ({
     return () => {
       myChart.destroy();
     };
-  }, [query, time, timeRange, metricsSelect.metrics, type, name, query_string]);
+  }, [
+    query,
+    time,
+    timeRange,
+    metricsSelect.metrics,
+    type,
+    name,
+    add_labels_string,
+  ]);
   const chartRef = React.useRef(null);
 
   return (
-    <div>
-      <canvas ref={chartRef} />
+    <div className={classes.graphBox}>
+      <canvas style={{ height: "100%", width: "100%" }} ref={chartRef} />
     </div>
   );
 };

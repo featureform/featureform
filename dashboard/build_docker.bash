5810d9da4dbc440ee0ee95ca98abb96170af6bde
Sam Inloes <sam@featureform.com>
2022-02-07 15:08:18 -0800

    add serve and remove nginx config

--- dashboard/build_docker.bash
+++ dashboard/build_docker.bash
@@ -1,3 +1,2 @@
-#!/bin/bash
 docker build -t sami1309/featureform-dashboard .
 docker push sami1309/featureform-dashboard
--- dashboard/run_docker.bash
+++ dashboard/run_docker.bash
@@ -1,2 +1,1 @@
-#!/bin/bash
 docker run --name featureform-dashboard -p 5000:3001 --rm -d featureform-dashboard

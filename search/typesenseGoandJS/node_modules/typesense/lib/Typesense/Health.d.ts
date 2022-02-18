47a0a10e7ff5f3c1030a4e57f96fc3df6868a44b
saadhvi umesh <saadhvi@saadhvis-MacBook-Pro.local>
2022-02-17 17:57:58 -0800

    Remove unused files.

--- search/typesenseGoandJS/node_modules/typesense/lib/Typesense/Health.d.ts
+++
@@ -1,9 +0,0 @@
-import ApiCall from './ApiCall';
-export interface HealthResponse {
-    status: string;
-}
-export default class Health {
-    private apiCall;
-    constructor(apiCall: ApiCall);
-    retrieve(): Promise<HealthResponse>;
-}

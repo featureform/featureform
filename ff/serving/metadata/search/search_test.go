e2585ea5dd6d7443315011b2e4f9d3022cebc1c2
saadhvi umesh <saadhvi27@berkeley.edu>
2022-03-11 12:52:15 -0800

    Add token seperator to schema

--- ff/serving/metadata/search/search_test.go
+++ ff/serving/metadata/search/search_test.go
@@ -30,6 +30,84 @@ func TestFullSearch(t *testing.T) {
 	}
 }
 
+func TestCharacters(t *testing.T) {
+	params := TypeSenseParams{
+		Host:   "localhost",
+		Port:   "8108",
+		ApiKey: "xyz",
+	}
+	oldsearcher, err := NewTypesenseSearch(&params)
+	if err != nil {
+		t.Fatalf("Failed to initialize pre deletion %s", err)
+	}
+	if err := oldsearcher.DeleteAll(); err != nil {
+		t.Fatalf("Failed to Delete %s", err)
+	}
+	searcher, errSearcher := NewTypesenseSearch(&params)
+	if errSearcher != nil {
+		t.Fatalf("Failed to initialize post deletion %s", errSearcher)
+	}
+	resources := []ResourceDoc{
+		{
+			Name:    "hero-typesense-film",
+			Variant: "default_variant",
+			Type:    "string-normal",
+		}, {
+			Name:    "wine_sonoma_county",
+			Variant: "second_variant",
+			Type:    "general",
+		}, {
+			Name:    "hero",
+			Variant: "default-shirt",
+			Type:    "string",
+		}, {
+			Name:    "Hero",
+			Variant: "second_variant",
+			Type:    "Entity",
+		}, {
+			Name:    "juice-dataset-sonome",
+			Variant: "third_variant_backup",
+			Type:    "Feature",
+		},
+	}
+	for _, resource := range resources {
+		if err := searcher.Upsert(resource); err != nil {
+			t.Fatalf("Failed to Upsert %s", err)
+		}
+	}
+	results, errRunsearch := searcher.RunSearch("film")
+	if errRunsearch != nil {
+		t.Fatalf("Failed to start search %s", errRunsearch)
+	}
+	namesFilm := []string{
+		"hero-typesense-film",
+	}
+	if len(results) == 0 {
+		t.Fatalf("Failed to return any search")
+	}
+	for i, hit := range results {
+		if hit.Name != namesFilm[i] {
+			t.Fatalf("Failed to return correct search'film'")
+		}
+	}
+	resultsSonoma, errRunsearch := searcher.RunSearch("sonoma")
+	if errRunsearch != nil {
+		t.Fatalf("Failed to start search %s", errRunsearch)
+	}
+	namesSonoma := []string{
+		"wine_sonoma_county",
+		"juice-dataset-sonome",
+	}
+	if len(results) == 0 {
+		t.Fatalf("Failed to return any search")
+	}
+	for i, hit := range resultsSonoma {
+		if hit.Name != namesSonoma[i] {
+			t.Fatalf("Failed to return correct search for 'sonoma'")
+		}
+	}
+}
+
 func TestOrder(t *testing.T) {
 	params := TypeSenseParams{
 		Host:   "localhost",

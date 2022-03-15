9b8d3088b68b73ef1e447f6c7917729b5c1ad233
Sam Inloes <sam@featureform.com>
2022-03-14 17:45:51 -0700

    Fix variable reference conventions

--- ff/serving/metrics/test/metrics_test.go
+++ ff/serving/metrics/test/metrics_test.go
@@ -52,7 +52,11 @@ func serving(obs metrics.PromFeatureObserver, promMetrics metrics.PromMetricsHan
 func TestMetrics(t *testing.T) {
 	start := time.Now()
 	instanceName := "test"
-    promMetrics := metrics.NewMetrics("test")
+    promMetrics := metrics.NewMetrics(instanceName)
+	featureName := "example_feature"
+	featureVariant := "example_variant"
+	trainingDatasetName := "example_dataset"
+	trainingDatasetVariant := "example_variant"
 	
 	servingNum := 5
 	servingErrorNum := 5
@@ -61,40 +65,40 @@ func TestMetrics(t *testing.T) {
 	latencyServingCount := servingNum + 1
 	latencyTrainingCount := trainingNum + 1
 
-	servingObserver := promMetrics.BeginObservingOnlineServe("amt_spent", "30d").(metrics.PromFeatureObserver)
-	trainingObserver := promMetrics.BeginObservingTrainingServe("is_fraud", "default").(metrics.TrainingDataObserver)
+	servingObserver := promMetrics.BeginObservingOnlineServe(featureName, featureVariant).(metrics.PromFeatureObserver)
+	trainingObserver := promMetrics.BeginObservingTrainingServe(trainingDatasetName, trainingDatasetVariant).(metrics.TrainingDataObserver)
 	serving(servingObserver, promMetrics, start, servingNum, servingErrorNum)
 	training(trainingObserver, promMetrics, start, trainingNum, trainingErrorNum)
 
-	servingCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, "amt_spent", "30d", "row serving")
+	servingCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, featureName, featureVariant, "row serving")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(servingCounterValue),servingNum, "5 feature rows should be served")
 
-	servingErrorCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, "amt_spent", "30d", "error")
+	servingErrorCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, featureName, featureVariant, "error")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(servingErrorCounterValue),servingErrorNum, "5 feature rows should be recorded")
-	trainingCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, "is_fraud", "default", "row serve")
+	trainingCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, trainingDatasetName, trainingDatasetVariant, "row serve")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(trainingCounterValue),trainingNum, "5 training data rows should be recorded")
-	trainingErrorCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, "is_fraud", "default", "error")
+	trainingErrorCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, trainingDatasetName, trainingDatasetVariant, "error")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(trainingErrorCounterValue),trainingErrorNum, "5 training data errors should be recorded")
-	latencyCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, "amt_spent", "30d", "")
+	latencyCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, featureName, featureVariant, "")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(latencyCounterValue),latencyServingCount , "Feature latency records 6 events")
-	latencyTrainingCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, "is_fraud", "default", "")
+	latencyTrainingCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, trainingDatasetName, trainingDatasetVariant, "")
 	if err != nil {
-		fmt.Println("error")
+		fmt.Println("error", err)
 	}
 	assert.Equal(t, int(latencyTrainingCounterValue),latencyTrainingCount, "Training latency records 6 events")
 

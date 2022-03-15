6f4b0ecda397f941a6263ca2111cbdc9fdd75090
Sam Inloes <sam@featureform.com>
2022-03-14 17:42:11 -0700

    Add full testing protocols to testing file

--- ff/serving/metrics/test/metrics_test.go
+++ ff/serving/metrics/test/metrics_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"time"
 	"testing"
 
@@ -28,27 +27,26 @@ func GetHistogramValue(metric *prometheus.HistogramVec, labelValues ...string) (
     return m.GetHistogram().GetSampleCount(), nil
 }
 
-func oscillationFactor(start time.Time) float64 {
-	oscillationPeriod := 10*time.Minute
-	return 2 + math.Sin(math.Sin(2*math.Pi*float64(time.Since(start))/float64(oscillationPeriod)))
-}
-
-func training(obs metrics.TrainingDataObserver, promMetrics metrics.PromMetricsHandler, start time.Time) {
-	obs.SetError()
-	for i := 0; i < 8; i++{
-				obs.ServeRow()
-		}
-		obs.Finish()
-}
-
-func serving(obs metrics.PromFeatureObserver, promMetrics metrics.PromMetricsHandler, start time.Time) {
-	obs.SetError()
-	for i := 0; i < 4; i++ {
-		time.Sleep(time.Duration(3*oscillationFactor(start)) * time.Millisecond)
-		obs.ServeRow()
-		obs.Finish()
-	
+func training(obs metrics.TrainingDataObserver, promMetrics metrics.PromMetricsHandler, start time.Time, num int, errors int) {
+	for i := 0; i < errors; i++{
+		obs.SetError()
 	}
+	
+	for i := 0; i < num; i++{
+		obs.ServeRow()
+	}
+		
+	obs.Finish()
+}
+
+func serving(obs metrics.PromFeatureObserver, promMetrics metrics.PromMetricsHandler, start time.Time, num int, errors int) {
+	for i := 0; i < errors; i++{
+		obs.SetError()
+	}
+	for i := 0; i < num; i++ {
+		obs.ServeRow()
+	}
+	obs.Finish()
 }
 
 func TestMetrics(t *testing.T) {
@@ -56,45 +54,48 @@ func TestMetrics(t *testing.T) {
 	instanceName := "test"
     promMetrics := metrics.NewMetrics("test")
 	
-	servingNum := 4
-	servingErrorNum := 1
+	servingNum := 5
+	servingErrorNum := 5
+	trainingNum := 5
+	trainingErrorNum := 5
+	latencyServingCount := servingNum + 1
+	latencyTrainingCount := trainingNum + 1
 
 	servingObserver := promMetrics.BeginObservingOnlineServe("amt_spent", "30d").(metrics.PromFeatureObserver)
 	trainingObserver := promMetrics.BeginObservingTrainingServe("is_fraud", "default").(metrics.TrainingDataObserver)
-	serving(servingObserver, promMetrics, start)
-	training(trainingObserver, promMetrics, start)
+	serving(servingObserver, promMetrics, start, servingNum, servingErrorNum)
+	training(trainingObserver, promMetrics, start, trainingNum, trainingErrorNum)
 
-	//fmt.Println(testutil.CollectAndCount(servingObserver.Count, "test"))
 	servingCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, "amt_spent", "30d", "row serving")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(servingCounterValue),servingNum, "4 feature rows should be served")
+	assert.Equal(t, int(servingCounterValue),servingNum, "5 feature rows should be served")
 
 	servingErrorCounterValue, err := GetCounterValue(servingObserver.Count, instanceName, "amt_spent", "30d", "error")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(servingErrorCounterValue),servingErrorNum, "1 feature rows should be recorded")
+	assert.Equal(t, int(servingErrorCounterValue),servingErrorNum, "5 feature rows should be recorded")
 	trainingCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, "is_fraud", "default", "row serve")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(trainingCounterValue),8, "8 training data rows should be recorded")
+	assert.Equal(t, int(trainingCounterValue),trainingNum, "5 training data rows should be recorded")
 	trainingErrorCounterValue, err := GetCounterValue(trainingObserver.Row_Count, instanceName, "is_fraud", "default", "error")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(trainingErrorCounterValue),1, "1 training data errors should be recorded")
+	assert.Equal(t, int(trainingErrorCounterValue),trainingErrorNum, "5 training data errors should be recorded")
 	latencyCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, "amt_spent", "30d", "")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(latencyCounterValue),5, "Feature latency records 5 events")
+	assert.Equal(t, int(latencyCounterValue),latencyServingCount , "Feature latency records 6 events")
 	latencyTrainingCounterValue, err := GetHistogramValue(promMetrics.Hist, instanceName, "is_fraud", "default", "")
 	if err != nil {
 		fmt.Println("error")
 	}
-	assert.Equal(t, int(latencyTrainingCounterValue),2, "Training latency records 2 events")
+	assert.Equal(t, int(latencyTrainingCounterValue),latencyTrainingCount, "Training latency records 6 events")
 
 }

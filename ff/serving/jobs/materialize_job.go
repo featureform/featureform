269c8335435ffedf9f5d2a934c923fc799bddd3f
Sam Inloes <sam@featureform.com>
2022-03-16 16:35:01 -0700

    change to boolean done status and remove mock

--- ff/serving/jobs/materialize_job.go
+++ ff/serving/jobs/materialize_job.go
@@ -1,6 +1,7 @@
 package jobs
 
-import ()
+import (
+)
 
 type Runner interface {
 	Run() (CompletionStatus, error)
@@ -31,5 +32,58 @@ type OnlineTable interface {
 type FeatureIterator interface {
 	Next() bool
 	Err() error
-	Value() (interface{}, error)
+	Entity() string
+	Value() interface{}
+}
+
+func (m *MaterializedChunkRunner) Run() (CompletionStatus, error) {
+
+	done := make(chan bool, 1)	
+	go func() {
+		it, err := m.Materialized.IterateSegment(m.ChunkIdx, m.ChunkIdx+m.ChunkSize)
+		if err != nil {
+			panic(err)
+		}
+		for ok := true; ok; ok = it.Next() {
+			value := it.Value()
+			if err != nil {
+				panic(err)
+			}
+			entity := it.Entity()
+			m.Table.Set(entity, value)
+		}
+
+		done <- true
+	}()
+
+	return &MaterializeChunkJobCompletionStatus{
+		Done: done,
+		Complete: false,
+	}, nil
+}
+
+type MaterializeChunkJobCompletionStatus struct {
+	Done chan bool
+	Complete bool
+}
+
+func (m *MaterializeChunkJobCompletionStatus) PercentComplete() float32 {
+	if m.Complete{
+		return 1.0
+	}
+	select {
+	case <-m.Done:
+		m.Complete = true
+		close(m.Done)
+		return 1.0
+	default :
+		return 0.0
+	}
+}
+
+func (m *MaterializeChunkJobCompletionStatus) String() string {
+	if m.PercentComplete() == 0 {
+		return "Job not complete."
+	}
+	return "Job complete"
 }

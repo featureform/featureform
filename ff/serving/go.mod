a6e3a21eaf679756fe39e26bcb0720fa82b7421c
Sam Inloes <sam@featureform.com>
2022-03-22 17:49:23 -0700

    write kubernetes.go and mock functions for unit testing

--- ff/serving/go.mod
+++ ff/serving/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go v1.42.31
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.3.0
 	github.com/lib/pq v1.10.4
 	github.com/prometheus/client_golang v1.12.1
 	github.com/prometheus/client_model v0.2.0
@@ -15,6 +16,9 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
+	k8s.io/api v0.23.5
+	k8s.io/apimachinery v0.23.5
+	k8s.io/client-go v0.23.5
 )
 
 require (
@@ -23,11 +27,15 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deepmap/oapi-codegen v1.9.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -42,11 +50,22 @@ require (
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/klog/v2 v2.30.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
+	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
+	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
 
 require (
--- ff/serving/runner/kubernetes_test.go
+++ ff/serving/runner/kubernetes_test.go
@@ -1,39 +1,65 @@
 package runner
 
-// import (
-// 	"testing"
-// )
-// func TestKubernetesRunnerCreate(t *testing.T) {
-// 	mockRunner := &MockRunner{}
-// 	mockFactory := func(config Config) (Runner, error) {
-// 		return mockRunner, nil
-// 	}
-// 	if err := RegisterFactory("kubernetes_runner_factory", mockFactory); err != nil {
-// 		t.Fatalf("Error registering factory: %v", err)
-// 	}
+import (
+	"github.com/google/uuid"
+	batchv1 "k8s.io/api/batch/v1"
+	watch "k8s.io/apimachinery/pkg/watch"
+	"testing"
+)
 
-// 	envVars := make(map[string]string)
-// 	image := "test_image"
-// 	numTasks := 1
-// 	mockKubernetesConfig := KubernetesRunnerConfig{
-// 		EnvVars: envVars,
-// 		Image: image,
-// 		NumTasks: numTasks,
-// 	}
+func NewMockKubernetesRunner(config KubernetesRunnerConfig) (Runner, error) {
+	jobSpec := newJobSpec(config)
+	jobName := uuid.New().String()
+	namespace := "default"
+	jobClient := MockJobClient{
+		JobName:   jobName,
+		Namespace: namespace,
+	}
+	return KubernetesRunner{
+		jobClient: jobClient,
+		jobSpec:   &jobSpec,
+	}, nil
+}
 
-// 	runner, err := NewKubernetesRunner(mockKubernetesConfig)
-// 	if err != nil {
-// 		t.Fatalf("Error creating kubernetes runner: %v", err)
-// 	}
+type MockJobClient struct {
+	JobName   string
+	Namespace string
+}
 
-// 	completionStatus, err := runner.Run()
-// 	if err != nil {
-// 		t.Fatalf("Kubernetes runner failed to run: %v", err)
-// 	}
+func (m MockJobClient) Get() (*batchv1.Job, error) {
+	return &batchv1.Job{}, nil
+}
 
-// 	completionStatus.Wait()
-// 	if !completionStatus.Complete() {
-// 		t.Fatalf("Runner failed to complete")
-// 	}
+func (m MockJobClient) Watch() (watch.Interface, error) {
+	return watch.NewEmptyWatch(), nil
+}
 
-// }
+func (m MockJobClient) Create(jobSpec *batchv1.JobSpec) (*batchv1.Job, error) {
+	return &batchv1.Job{}, nil
+}
+func TestKubernetesRunnerCreate(t *testing.T) {
+
+	runner, err := NewMockKubernetesRunner(KubernetesRunnerConfig{envVars: map[string]string{"test": "envVar"}, image: "test", numTasks: 1})
+	if err != nil {
+		t.Fatalf("Failed to create Kubernetes runner")
+	}
+
+	completionWatcher, err := runner.Run()
+	if err != nil {
+		t.Fatalf("Failed to initialize run of Kubernetes runner")
+	}
+
+	if err := completionWatcher.Wait(); err != nil {
+		t.Fatalf("Kubernetes runner failed while running")
+	}
+
+	if completionWatcher.Err() != nil {
+		t.Fatalf("Wait failed to report error")
+	}
+
+	if !completionWatcher.Complete() {
+		t.Fatalf("Kubernetes runner failed to set complete")
+	}
+	completionWatcher.String()
+
+}

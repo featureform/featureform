26f5824bb1f963c6bd88008661b96ac21399752d
Simba Khadder <simba@featureform.com>
2022-03-22 21:47:01 -0700

    Add provider factory

---
+++ ff/serving/provider/provider.go
@@ -0,0 +1,32 @@
+package provider
+
+import (
+	"fmt"
+)
+
+type SerializedConfig []byte
+
+type Provider interface {
+}
+
+type Factory func(SerializedConfig) (Provider, error)
+
+type Type string
+
+var factories map[Type]Factory = make(map[Type]Factory)
+
+func RegisterFactory(t Type, f Factory) error {
+	if _, has := factories[t]; has {
+		return fmt.Errorf("%s provider factory already exists.", t)
+	}
+	factories[t] = f
+	return nil
+}
+
+func Get(t Type, config SerializedConfig) (Provider, error) {
+	f, has := factories[t]
+	if !has {
+		return nil, fmt.Errorf("No provider of type: %s", t)
+	}
+	return f(config)
+}
---
+++ ff/serving/provider/provider_test.go
@@ -0,0 +1,43 @@
+package provider
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var mockConfig SerializedConfig = SerializedConfig("abc")
+
+func mockFactory(c SerializedConfig) (Provider, error) {
+	if !reflect.DeepEqual(c, mockConfig) {
+		return nil, fmt.Errorf("Not mock config")
+	}
+	return nil, nil
+}
+
+func TestFactory(t *testing.T) {
+	mockType := Type("mock")
+	if err := RegisterFactory(mockType, mockFactory); err != nil {
+		t.Fatalf("Failed to register factory: %s", err)
+	}
+	if _, err := Get(mockType, mockConfig); err != nil {
+		t.Fatalf("Failed to get provider: %s", err)
+	}
+
+}
+
+func TestFactoryExists(t *testing.T) {
+	mockType := Type("already exists")
+	if err := RegisterFactory(mockType, mockFactory); err != nil {
+		t.Fatalf("Failed to register factory: %s", err)
+	}
+	if err := RegisterFactory(mockType, mockFactory); err == nil {
+		t.Fatalf("Succeeded in registering factory twice")
+	}
+}
+
+func TestFactoryDoesntExists(t *testing.T) {
+	if provider, err := Get(Type("Doesnt exist"), mockConfig); err == nil {
+		t.Fatalf("Succeeded in getting unregistered provider: %v", provider)
+	}
+}

eded42ee7f47d4846a9b034a6d5e5286a5d74305
Sam Inloes <sam@featureform.com>
2022-03-22 17:49:23 -0700

    write kubernetes.go and mock functions for unit testing

--- ff/serving/runner/kubernetes.go
+++ ff/serving/runner/kubernetes.go
@@ -1,202 +1,17 @@
 package runner
 
-// import (
-
-// 	"errors"
-// 	"context"
-// 	"fmt"
-// 	"github.com/google/uuid"
-// 	"encoding/json"
-// 	batchv1 "k8s.io/api/batch/v1"
-// 	v1 "k8s.io/api/core/v1"
-// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-// 	kubernetes "k8s.io/client-go/kubernetes"
-// 	rest "k8s.io/client-go/rest"
-// )
-
-// // type CompletionStatus interface {
-// // 	Complete() bool
-// // 	String() string
-// // 	Wait() error
-// // 	Err() error
-// // }
-
-// type KubernetesCompletionStatus struct {
-// 	jobManager KubernetesJobManager
-// }
-
-// type KubernetesJobConfig struct {
-// 	jobName string
-// 	nameSpace string
-// 	envVars map[string]string
-// 	image string
-// 	numPods int
-// 	numParalell int
-// }
-
-// type KubernetesJobManager interface {
-// 	Run() error
-// 	Get() metav1.Job, error
-// 	Completed() bool
-// 	Watch() error
-// }
-
-// type MockKubernetesJobManager struct {
-
-// }
-
-// func newJob(config KubernetesJobConfig) (KubernetesJobManager, error) {
-// 	jobs := k.Clientset.BatchV1().Jobs("default")
-// 	containerLabel := "test"
-// 	compMode := batchv1.IndexedCompletion
-// 	jobID := uuid.New().String()
-// 	podNum := int32(k.KubeConfig.NumTasks)
-
-// 	envVars := make([]v1.EnvVar, len(k.KubeConfig.EnvVars)+1)
-// 	envVars[0] = v1.EnvVar{Name: "JOB_ID", Value: jobID}
-// 	i := 1
-// 	for key, element := range k.KubeConfig.EnvVars {
-// 		envVars[i] = v1.EnvVar{Name: key, Value: element}
-// 		i++
-// 	}
-
-// 	jobSpec := &batchv1.Job{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      jobName,
-// 			Namespace: "default",
-// 		},
-// 		Spec: batchv1.JobSpec{
-// 			Completions:    &podNum,
-// 			Parallelism:    &podNum,
-// 			CompletionMode: &compMode,
-// 			Template: v1.PodTemplateSpec{
-// 				Spec: v1.PodSpec{
-// 					Containers: []v1.Container{
-// 						{
-// 							Name:  containerLabel,
-// 							Image: k.KubeConfig.Container,
-// 							Env:   envVars,
-// 						},
-// 					},
-// 					RestartPolicy: v1.RestartPolicyNever,
-// 				},
-// 			},
-// 		},
-// 	}
-
-// 	_, err := jobs.Create(context.TODO(), jobSpec, metav1.CreateOptions{})
-// }
-
-// func (m *MockKubernetesJobManager) Run(config KubernetesJobConfig) error {
-
-// }
-
-// func (m *MockKubernetesJobManager) Get(config KubernetesJobConfig) metav1.Job, error {
-
-// }
-
-// func (m *MockKubernetesJobManager) Completed(config KubernetesJobConfig) bool {
-
-// }
-
-// type KubernetesCompletionWatcher struct {
-// 	kubeJobManager KubernetesJobManager
-// }
-
-// func (k *KubernetesCompletionWatcher) Complete() bool {
-
-// }
-
-// func (k *KubernetesCompletionWatcher) String() string {
-
-// }
-
-// func (k *KubernetesCompletionWatcher) Wait() error {
-
-// }
-
-// func (k *KubernetesCompletionWatcher) Err() error {
-
-// }
-
-// func (k *KubernetesCompletionWatcher) PercentComplete() float32 {
-
-// }
-
-// type KubernetesRunnerConfig struct {
-// 	envVars map[string]string
-// 	image string
-// 	numTasks int
-// }
-
-// type KubernetesRunner struct {
-// 	Clientset  *kubernetes.Clientset
-// 	KubeConfig KubernetesRunnerConfig
-// 	JobName    *string
-// }
-
-// func newJobConfig(config KubernetesRunnerConfig) KubernetesJobConfig{
-// 	return &KubernetesJobConfig{
-// 		jobName: uuid.New().String(),
-// 		nameSpace: "default",
-// 		envVars: config.envVars,
-// 		image: config.image,
-// 		numPods: config.numTasks,
-// 		numParalell: config.numTasks,
-// 	}
-// }
-
-// func (k *KubernetesRunner) Run() (CompletionStatus, error) {
-	
-
-// 	jobConfig := newJobConfig(k.KubeConfig)
-
-// 	job := newJob(jobConfig)
-
-// 	err := job.Run(); err != nil {
-// 		return err
-// 	}
-
-// 	return KubernetesCompletionStatus{
-// 		jobManager: job
-// 	}
-// }
-
-
-
-// 	jobName := fmt.Sprintf("%d-%s-pods", k.KubeConfig.NumTasks, jobID)
-
-	
-// 	if err != nil {
-// 		fmt.Println("Failed to create K8s job.", err)
-// 		return err
-
-// 	}
-// 	k.JobName = &jobName
-// 	fmt.Printf("Created K8s job %s successfully\n", jobName)
-
-// 	if k.JobName == nil {
-// 		return nil, errors.New("no job created")
-// 	}
-// 	getOptions := metav1.GetOptions{}
-// 	job, err := k.Clientset.BatchV1().Jobs("default").Get(context.TODO(), *k.JobName, getOptions)
-// 	if err != nil {
-// 		return nil, errors.New("Could not get job")
-// 	}
-// 	totalTasks := int(job.Status.Succeeded + job.Status.Active + job.Status.Failed)
-// 	tasksFailed := int(job.Status.Failed)
-// 	completedTasks := int(job.Status.Succeeded)
-// 	return &KubernetesCompletionStatus{
-// 		TotalTasks:    totalTasks,
-// 		TasksComplete: completedTasks,
-// 		TasksFailed:   tasksFailed,
-// 	}, nil
-// 	return nil
-// }
-
-// func (k *KubernetesRunner) Config() KubernetesRunnerConfig {
-// 	return k.KubeConfig
-// }
+import (
+	"context"
+	// "encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	watch "k8s.io/apimachinery/pkg/watch"
+	kubernetes "k8s.io/client-go/kubernetes"
+	rest "k8s.io/client-go/rest"
+)
 
 // func (k *KubernetesRunnerConfig) Serialize() (Config, error) {
 // 	serialized, err := json.Marshal(k)
@@ -214,10 +29,157 @@ package runner
 // 	return nil
 // }
 
-// func ConnectToKubernetes() error {
+func generateKubernetesEnvVars(envVars map[string]string) []v1.EnvVar {
+	kubeEnvVars := make([]v1.EnvVar, len(envVars))
+	i := 0
+	for key, element := range envVars {
+		kubeEnvVars[i] = v1.EnvVar{Name: key, Value: element}
+		i++
+	}
+	return kubeEnvVars
+}
 
-// }
+func newJobSpec(config KubernetesRunnerConfig) batchv1.JobSpec {
+	containerID := uuid.New().String()
+	envVars := generateKubernetesEnvVars(config.envVars)
+	completionMode := batchv1.IndexedCompletion
+	return batchv1.JobSpec{
+		Completions:    &config.numTasks,
+		Parallelism:    &config.numTasks,
+		CompletionMode: &completionMode,
+		Template: v1.PodTemplateSpec{
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Name:  containerID,
+						Image: config.image,
+						Env:   envVars,
+					},
+				},
+				RestartPolicy: v1.RestartPolicyNever,
+			},
+		},
+	}
 
-// func NewKubernetesRunner(config KubernetesRunnerConfig) (Runner, error) {
+}
 
-// }
+type KubernetesRunnerConfig struct {
+	envVars  map[string]string
+	image    string
+	numTasks int32
+}
+
+type JobClient interface {
+	Get() (*batchv1.Job, error)
+	Watch() (watch.Interface, error)
+	Create(jobSpec *batchv1.JobSpec) (*batchv1.Job, error)
+}
+
+type KubernetesRunner struct {
+	jobClient JobClient
+	jobSpec   *batchv1.JobSpec
+}
+
+type KubernetesCompletionWatcher struct {
+	jobClient JobClient
+}
+
+func (k KubernetesCompletionWatcher) Complete() bool {
+	job, err := k.jobClient.Get()
+	if err != nil {
+		return false
+	}
+	if job.Status.Active == 0 && job.Status.Failed == 0 {
+		return true
+	}
+	return false
+}
+
+func (k KubernetesCompletionWatcher) String() string {
+	job, err := k.jobClient.Get()
+	if err != nil {
+		return "Could not fetch job."
+	}
+	return fmt.Sprintf("%d jobs succeeded. %d jobs active. %d jobs failed", job.Status.Succeeded, job.Status.Active, job.Status.Failed)
+}
+
+func (k KubernetesCompletionWatcher) Wait() error {
+	watcher, err := k.jobClient.Watch()
+	if err != nil {
+		return err
+	}
+	watchChannel := watcher.ResultChan()
+	for jobEvent := range watchChannel {
+		if failed := jobEvent.Object.(*batchv1.Job).Status.Failed; failed > 0 {
+			return fmt.Errorf("job failed while running")
+		}
+
+	}
+	return nil
+}
+
+func (k KubernetesCompletionWatcher) Err() error {
+	job, err := k.jobClient.Get()
+	if err != nil {
+		return err
+	}
+	if job.Status.Failed > 0 {
+		return fmt.Errorf("job failed while running")
+	}
+	return nil
+}
+
+func (k KubernetesRunner) Run() (CompletionWatcher, error) {
+	_, err := k.jobClient.Create(k.jobSpec)
+	if err != nil {
+		return nil, err
+	}
+	return KubernetesCompletionWatcher{jobClient: k.jobClient}, nil
+}
+
+//Everything below cannot be run unless inside Kubernetes cluster
+
+func NewKubernetesRunner(config KubernetesRunnerConfig) (Runner, error) {
+	jobSpec := newJobSpec(config)
+	jobName := uuid.New().String()
+	namespace := "default"
+	jobClient, err := NewKubernetesJobClient(jobName, namespace)
+	if err != nil {
+		return nil, err
+	}
+	return KubernetesRunner{
+		jobClient: jobClient,
+		jobSpec:   &jobSpec,
+	}, nil
+}
+
+type KubernetesJobClient struct {
+	Clientset *kubernetes.Clientset
+	JobName   string
+	Namespace string
+}
+
+func (k KubernetesJobClient) Get() (*batchv1.Job, error) {
+	return k.Clientset.BatchV1().Jobs(k.Namespace).Get(context.TODO(), k.JobName, metav1.GetOptions{})
+}
+
+func (k KubernetesJobClient) Watch() (watch.Interface, error) {
+	return k.Clientset.BatchV1().Jobs(k.Namespace).Watch(context.TODO(), metav1.ListOptions{FieldSelector: fmt.Sprintf("metadata.name=%s", k.JobName)})
+}
+
+func (k KubernetesJobClient) Create(jobSpec *batchv1.JobSpec) (*batchv1.Job, error) {
+	job := &batchv1.Job{ObjectMeta: metav1.ObjectMeta{Name: k.JobName, Namespace: k.Namespace}, Spec: *jobSpec}
+	return k.Clientset.BatchV1().Jobs(k.Namespace).Create(context.TODO(), job, metav1.CreateOptions{})
+}
+
+func NewKubernetesJobClient(name string, namespace string) (*KubernetesJobClient, error) {
+	kubeConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &KubernetesJobClient{Clientset: clientset, JobName: name, Namespace: namespace}, nil
+}

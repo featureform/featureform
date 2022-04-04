613a0a4fedfb591518e96166dfbd4a21ce19565e
Simba Khadder <simba@featureform.com>
2022-04-04 13:37:44 -0700

    Add new serving impl

---
+++ ff/serving/serving/main/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net"
+
+	"github.com/featureform/serving/metadata"
+	"github.com/featureform/serving/metrics"
+	"github.com/featureform/serving/serving"
+
+	pb "github.com/featureform/serving/proto"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	logger := zap.NewExample().Sugar()
+
+	port := ":8080"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		logger.Panicw("Failed to listen on port", "Err", err)
+	}
+
+	promMetrics := metrics.NewMetrics("test")
+	metrics_port := ":2112"
+
+    meta, err := metadata.NewClient("localhost:8081", logger)
+	if err != nil {
+		logger.Panicw("Failed to connect to metadata", "Err", err)
+	}
+
+	serv, err := serving.NewFeatureServer(meta, promMetrics, logger)
+
+	grpcServer := grpc.NewServer()
+	if err != nil {
+		logger.Panicw("Failed to create training server", "Err", err)
+	}
+	pb.RegisterFeatureServer(grpcServer, serv)
+	logger.Infow("Serving metrics", "Port", metrics_port)
+	go promMetrics.ExposePort(metrics_port)
+	logger.Infow("Server starting", "Port", port)
+	serveErr := grpcServer.Serve(lis)
+	if serveErr != nil {
+		logger.Errorw("Serve failed with error", "Err", serveErr)
+	}
+
+}

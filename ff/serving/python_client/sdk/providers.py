65b1117160bf72140b0b8244c25001e83b584adc
Shabnam Mokhtarani <samokhtarani@yahoo.com>
2022-04-08 17:23:30 -0700

    Remove provider class, adds register_db methhod

--- ff/serving/python_client/sdk/providers.py
+++ ff/serving/python_client/sdk/providers.py
@@ -1,56 +1,39 @@
 providers_to_register = []
 
-class Provider:
-    def __init__(self, name = str, provider_type = str, host = str, db = int, ssl = bool, client_certificate= str, bucket = str):
-        self.name = name
-        self.provider_type = provider_type
-        self.host = host
-        self.db = db
-        self.ssl = ssl
-        self.client_certificate = client_certificate
-        self.bucket = bucket
-      
-
-def register_provider(name, provider_type, host, db, ssl, client_certificate, bucket):
-    provider = Provider(name, provider_type, host, db, ssl, client_certificate, bucket)
+def register_db(name = "", provider_type = "", host = "", db= None, ssl= "", client_certificate = ""):
+    provider = {"name": name, "provider_type": provider_type, "host": host, "db": db, "ssl" : ssl, "client_certificate" : client_certificate}
     providers_to_register.append(provider)
 
-def register_redis(*args, **kwargs):
-    register_provider(kwargs['name'], 'Redis', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
 
-def register_bigquery(*args, **kwargs):
-    register_provider(kwargs['name'], 'BigQuery', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_redis(name = "", host = "", db = None, ssl= "", client_certificate = ""):
+    register_db(name, "redis", host, db, ssl, client_certificate)
 
-def register_cassandra(*args, **kwargs):
-    register_provider(kwargs['name'], 'Cassandra', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_bigquery(name = "", host = "", db= None, ssl= "", client_certificate = ""):
+    register_db(name, "bigquery", host, db, ssl, client_certificate)
 
-def register_postgres(*args, **kwargs):
-    register_provider(kwargs['name'], 'Postgres', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_cassandra(name = "", host = "", db= None, ssl= "", client_certificate = ""):
+    register_db(name, "cassandra", host, db, ssl, client_certificate)
 
-def register_mysql(*args, **kwargs):
-    register_provider(kwargs['name'], 'MySQL', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_postgres(name = "", host = "", db= None, ssl= "", client_certificate = ""):
+    register_db(name, "postgres", host, db, ssl, client_certificate)
 
-def register_deltalake(*args, **kwargs):
-    register_provider(kwargs['name'], 'DeltaLake', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_mysql(name = "", host = "", db= None, ssl= "", client_certificate = ""):
+    register_db(name, "mysql", host, db, ssl, client_certificate)
 
-def register_s3(*args, **kwargs):
-    register_provider(kwargs['name'], 'S3', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'],kwargs['bucket'])
+def register_deltalake(name = "", host = "", db= None, ssl= "", client_certificate = ""):
+    register_db(name, "deltalake", host, db, ssl, client_certificate)
 
-def register_spark(*args, **kwargs):
-    register_provider(kwargs['name'], 'Spark', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_s3(name = "", host = "", db= None, ssl= "", client_certificate = "", bucket= ""):
+    provider = {"name": name, "provider_type": "s3", "host": host, "db": db, "ssl" : ssl, "client_certificate" : client_certificate, "bucket" : bucket}
+    providers_to_register.append(provider)
 
-def register_flink(*args, **kwargs):
-    register_provider(kwargs['name'], 'Flink', kwargs['host'], kwargs['db'], kwargs['ssl'], kwargs['client_certificate'], kwargs['bucket'])
+def register_spark(name = "", host = "", ssl= "", client_certificate = ""):
+    provider = {"name": name, "provider_type": "spark", "host": host, "ssl" : ssl, "client_certificate" : client_certificate}
+    providers_to_register.append(provider)
 
-
-register_redis(
-    name = "demo-redis",
-    host = "https://demo-redis-env.alias",
-    db = 1,
-    ssl = True,
-    client_certificate = "~/.demo/redis-cert.pem",
-    bucket = ""
-)
+def register_flink(name = "", host = "", ssl= "", client_certificate = ""):
+    provider = {"name": name, "provider_type": "flink", "host": host, "ssl" : ssl, "client_certificate" : client_certificate}
+    providers_to_register.append(provider)
 
 
 

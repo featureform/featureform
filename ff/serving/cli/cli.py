c56a471dea84b0e82a563d3048afeaf1845112a3
Simba Khadder <simba@featureform.com>
2022-04-19 13:02:47 -0700

    Add base CLI

---
+++ ff/serving/cli/cli.py
@@ -0,0 +1,60 @@
+import click
+
+resource_types = [
+    "feature",
+    "source",
+    "training-set",
+    "label",
+    "entity",
+    "provider",
+    "transformation",
+]
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    """
+    \b
+    ______         _                   __                     
+    |  ___|       | |                 / _|                    
+    | |_ ___  __ _| |_ _   _ _ __ ___| |_ ___  _ __ _ __ ___  
+    |  _/ _ \/ _` | __| | | | '__/ _ \  _/ _ \| '__| '_ ` _ \ 
+    | ||  __/ (_| | |_| |_| | | |  __/ || (_) | |  | | | | | |
+    \_| \___|\__,_|\__|\__,_|_|  \___|_| \___/|_|  |_| |_| |_|
+
+    Interact with the Featureform's Feature Store via the command line.
+    """
+    pass
+
+@cli.command()
+@click.argument("resource_type")
+def list(resource_type):
+    """list resources of a given type.
+    """
+    pass
+
+@cli.command()
+@click.argument("resource_type", type=click.Choice(resource_types, case_sensitive=False))
+@click.argument("resources", nargs=-1, required=True)
+def get(resource_type, resoruces):
+    """get resources of a given type.
+    """
+    pass
+
+@cli.command()
+@click.argument("files", nargs=-1, required=True, type=click.Path(exists=True))
+def plan(files):
+    """print out resources that would be changed by applying these files.
+    """
+    pass
+
+@cli.command()
+@click.argument("files", nargs=-1, required=True, type=click.Path(exists=True))
+def apply(files):
+    """apply changes to featureform
+    """
+    pass
+
+if __name__ == '__main__':
+    cli()

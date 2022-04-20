7bc7cdb214de47a72d1930afc2f379b31b2c6ef3
sdreyer <sterling@featureform.com>
2022-04-20 10:58:34 -0700

    Terraform build EKS, RDS, EC

---
+++ ff/serving/terraform-sandbox/elasticache.tf
@@ -0,0 +1,15 @@
+resource "aws_elasticache_subnet_group" "ec_subnet_grp" {
+  name       = "tf-test-cache-subnet"
+  subnet_ids = module.vpc.public_subnets
+}
+
+resource "aws_elasticache_cluster" "cluster" {
+  cluster_id           = local.cluster_name
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+  subnet_group_name = aws_elasticache_subnet_group.ec_subnet_grp.name
+}

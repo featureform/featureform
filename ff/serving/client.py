9f4883d51e2be7c1d7e0838c34f69dd0ab899d60
Simba Khadder <simba@featureform.com>
2022-04-19 17:14:04 -0700

    Add licenses to ff files

--- ff/serving/client.py
+++ ff/serving/client.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import grpc
 import numpy as np
 import proto.serving_pb2

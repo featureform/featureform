9c49907f990e12cde8c61c30dc73ec506ed196cf
Sam Inloes <sam@featureform.com>
2022-04-21 13:42:05 -0700

    refactor coordinator

--- ff/serving/coordinator/coordinator.go
+++ ff/serving/coordinator/coordinator.go
@@ -17,7 +17,7 @@ import (
 type Coordinator struct {
 	Metadata    *metadata.Client
 	Logger      *zap.SugaredLogger
-	ETCD_Client *clientv3.Client
+	ETCDClient *clientv3.Client
 	Spawner     JobSpawner
 }
 
@@ -46,9 +46,7 @@ const (
 
 type CoordinatorJob struct {
 	Attempts int
-	Type     metadata.ResourceType
-	Name     string
-	Variant  string
+	Resource metadata.ResourceID
 }
 
 func (c *CoordinatorJob) Serialize() ([]byte, error) {
@@ -74,7 +72,7 @@ func (c *Coordinator) StartTimedJobWatcher() error {
 	}
 	defer s.Close()
 	for {
-		getResp, err := kvc.Get(context.Background(), "JOB", clientv3.WithPrefix())
+		getResp, err := keyValueClient.Get(context.Background(), "JOB", clientv3.WithPrefix())
 		if err != nil {
 			return err
 		}
@@ -136,19 +134,18 @@ func (c *Coordinator) GetJobRunner(jobName string, config runner.Config) (runner
 }
 
 func (c *Coordinator) runTrainingSetJob(name string, variant string) error {
-	ctx := context.Background()
-	ts, err := c.Metadata.GetTrainingSetVariant(ctx, metadata.NameVariant{name, variant})
+	ts, err := c.Metadata.GetTrainingSetVariant(context.Background(), metadata.NameVariant{name, variant})
 	if err != nil {
 		return nil, err
 	}
 	status := ts.GetStatus()
 	if status == ResourceStatus.Ready || status == ResourceStatus.Failed {
-		return fmt.Errorf("Training Set set to %s", status)
+		return fmt.Errorf("Training Set already set to %s", status)
 	}
 	if err := c.SetResourceStatus("TRAINING_SET_VARIANT", name, variant, ResourceStatus.Pending); err != nil {
 		return err
 	}
-	providerEntry, err := ts.FetchProvider(c.Metadata, ctx)
+	providerEntry, err := ts.FetchProvider(c.Metadata, context.Background())
 	if err != nil {
 		return err
 	}
@@ -172,17 +169,17 @@ func (c *Coordinator) runTrainingSetJob(name string, variant string) error {
 	if err != nil {
 		return err
 	}
-	training_set_def := provider.TrainingSetDef{
+	trainingSetDef := provider.TrainingSetDef{
 		ID:       provider.ResourceID{Name: name, Variant: variant},
 		Label:    provider.ResourceID{Name: label.GetName(), Variant: label.GetVariant()},
 		Features: featureList,
 	}
-	ts_runner_config := runner.TrainingSetRunnerConfig{
+	tsRunnerConfig := runner.TrainingSetRunnerConfig{
 		OfflineType:   providerEntry.GetType(),
 		OfflineConfig: providerEntry.GetSerializedConfig(),
-		Def:           training_set_def,
+		Def:           trainingSetDef,
 	}
-	jobRunner, err := c.GetJobRunner("CREATE_TRAINING_SET", ts_runner_config)
+	jobRunner, err := c.GetJobRunner("CREATE_TRAINING_SET", tsRunnerConfig)
 	if err != nil {
 		return err
 	}
@@ -193,81 +190,99 @@ func (c *Coordinator) runTrainingSetJob(name string, variant string) error {
 	if err := completionWatcher.Wait(); err != nil {
 		return err
 	}
-	if err := c.SetResourceStatus("TRAINING_SET_VARIANT", name, variant, ResourceStatus.Ready); err != nil {
+	if err := c.SetResourceStatus(metadata.TRAINING_SET_VARIANT, name, variant, ResourceStatus.Ready); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *Coordinator) syncHandleJob(job_key string, s *concurrency.Session) error {
-	l := concurrency.NewMutex(s, fmt.Sprintf("LOCK_%s", job_key))
-	kvc := clientv3.NewKV(c.ETCD_Client)
-	ctx := context.Background()
-	get_job_data_txn := (kvc).Txn(ctx)
-	if err := l.Lock(ctx); err != nil {
+func (c *Coordinator) getJobIfOwner(mtx *concurrency.Mutex, jobKey string) (CoordinatorJob, error) {
+	keyValueClient := clientv3.NewKV(c.ETCDClient)
+	txn := (keyValueClient).Txn(context.Background())
+	response, err := txn.If(mtx.IsOwner()).Then(clientv3.OpGet(jobKey)).Commit()
+	if err != nil {
+		return nil, fmt.Errorf("transaction did not succeed: %v", err)
+	}
+	if !response.Succeeded {
+		return nil, fmt.Errorf("transaction did not succeed")
+	}
+	var responseData []byte
+	if len(response.Responses[0].GetResponseRange().GetKvs()) > 0 {
+		responseData = response.Responses[0].GetResponseRange().GetKvs()[0].Value
+	} else {
+		return nil, fmt.Errorf("Coordinator job does not exist. %v")
+	}
+	job := &CoordinatorJob{}
+	err := job.Deserialize(responseData)
+	if err != nil {
+		return nil, fmt.Errorf("Could not deserialize coordinator job: %v", err)
+	}
+	return *job, nil
+
+}
+
+func (c *Coordinator) incrementJobAttempts(mtx *concurrency.Mutex, job CoordinatorJob, jobKey string) error {
+	job.Attempts += 1
+	serializedJob, err := job.Serialize()
+	if err != nil {
+		return fmt.Errorf("Could not serialize coordinator job. %v", err)
+	}
+	keyValueClient := clientv3.NewKV(c.ETCDClient)
+	txn := (keyValueClient).Txn(context.Background())
+	if _, err := txn.If(l.IsOwner()).Then(clientv3.OpPut(jobKey, serialized)).Commit(); err != nil {
+		return fmt.Errorf("Could not set iterated coordinator job. %v", err)
+	}
+	return nil
+}
+
+func (c *Coordinator) deleteJob(mtx *concurrency.Mutex, jobKey string) error {
+	keyValueClient := clientv3.NewKV(c.ETCDClient)
+	txn = (keyValueClient).Txn(context.Background())
+	response, err := txn.If(mtx.IsOwner()).Then(clientv3.OpDelete(jobKey)).Commit()
+	if err != nil {
+		return fmt.Errorf("Delete job transaction failed: %v", err)
+	}
+	if !response.Succeeded {
+		return fmt.Errorf("Delete job transaction failed")
+	}
+	didDelete := response.Responses[0].GetResponseDeleteRange().Deleted
+	if didDelete != 1 {
+		return fmt.Errorf("Job Already deleted")
+	}
+	return nil
+}
+
+func (c *Coordinator) syncHandleJob(jobKey string, s *concurrency.Session) error {
+	mtx := concurrency.NewMutex(s, fmt.Sprintf("LOCK_%s", job_key))
+	if err := mtx.Lock(context.Background()); err != nil {
 		return err
 	}
-	unlockAndExit := func() error {
-		if err := l.Unlock(ctx); err != nil {
-			return err
+	defer func() {
+		if err := mtx.Unlock(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
+	job, err := c.getJobIfOwner(mtx, jobKey)
+	if err != nil {
+		return err
+	}
+	if job.Attempts > MAX_ATTEMPTS {
+		if err := c.SetResourceStatus(job.Resource, ResourceStatus.Failed); err != nil {
+			return fmt.Errorf("Could not set job status to failed: %v", err)
 		}
 		return nil
 	}
-	get_job_data_txn_resp, err := get_job_data_txn.If(l.IsOwner()).Then(clientv3.OpGet(job_key)).Commit()
-	if err != nil {
-		return fmt.Errorf("transaction did not succeed: %v. %v", err, unlockAndExit())
+	if err := c.incrementJobAttempts(mtx, job, jobKey); err != nil {
+		return err
 	}
-	if !get_job_data_txn_resp.Succeeded {
-		return fmt.Errorf("transaction did not succeed. %v", unlockAndExit())
-	}
-	var getResp []byte
-	if len(get_job_data_txn_resp.Responses[0].GetResponseRange().GetKvs()) > 0 {
-		getResp = get_job_data_txn_resp.Responses[0].GetResponseRange().GetKvs()[0].Value
-	} else {
-		return fmt.Errorf("Coordinator job does not exist. %v", unlockAndExit())
-	}
-	c_job := &CoordinatorJob{}
-	err := c_job.Deserialize(getResp)
-	if err != nil {
-		return fmt.Errorf("Could not deserialize coordinator job: %v. %v", err, unlockAndExit())
-	}
-	if c_job.Attempts > MAX_ATTEMPTS {
-		if err := c.SetResourceStatus(c_job.Type, c_job.Name, c_job.Variant, ResourceStatus.Failed); err != nil {
-			return fmt.Errorf("Could not set job status to failed: %v. %v", err, unlockAndExit())
-		}
-		return unlockAndExit()
-	}
-	c_job.Attempts += 1
-	serialized, err := c_job.Serialize()
-	if err != nil {
-		return fmt.Errorf("Could not serialize coordinator job. %v", unlockAndExit())
-	}
-	set_new_attempts_txn := (kvc).Txn(ctx)
-	set_new_attempts_txn_resp, err := set_new_attempts_txn.If(l.IsOwner()).Then(clientv3.OpPut(job_key, serialized)).Commit()
-	if err != nil {
-		return fmt.Errorf("Could not set iterated coordinator job. %v", unlockAndExit())
-	}
-	prev_kv := set_new_attempts_txn_resp.GetResponsePut().GetPrevKv()
-	if prev_kv.Key != job_key || prev_kv.Value != getResp {
-		return fmt.Errorf("Prev key did not match actual prev key. %v", unlockAndExit())
-	}
-	switch c_job.Type {
+	switch job.Resource.Type {
 	case metadata.TRAINING_SET_VARIANT:
-		if err := runTrainingSetJob(c_job.Name, c_job.Variant); err != nil {
-			return fmt.Errorf("Training set job failed: %v. %v", err, unlockAndExit())
+		if err := c.runTrainingSetJob(job.Resource.Name, job.Resource.Variant); err != nil {
+			return fmt.Errorf("Training set job failed: %v", err)
 		}
 	}
-	delete_job_txn = (kvc).Txn(ctx)
-	delete_job_txn_resp, err := delete_job_txn.If(l.IsOwner()).Then(clientv3.OpDelete(job_id)).Commit()
-	if err != nil {
-		return fmt.Errorf("Delete job transaction failed: %v. %v", err, unlockAndExit())
+	if err := c.deleteJob(mtx, jobKey); err != nil {
+		return err
 	}
-	if !transaction.Succeeded {
-		return fmt.Errorf("Delete job transaction failed. %v", unlockAndExit())
-	}
-	didDelete := delete_job_txn_resp.Responses[0].GetResponseDeleteRange().Deleted
-	if didDelete != 1 {
-		return fmt.Errorf("Job Already deleted: %v", unlockAndExit())
-	}
-	return unlockAndExit()
+	return nil
 }

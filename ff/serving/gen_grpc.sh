bf889515830aacd2ce91fbd9e560cb59be4d2ff4
sdreyer <sterling@featureform.com>
2022-05-20 14:25:06 -0400

    Allow proto directory creation to fail if exists

--- ff/serving/gen_grpc.sh
+++ ff/serving/gen_grpc.sh
@@ -1,9 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
-set -e
 
 mkdir client/src/featureform/proto/
+set -e
 cp metadata/proto/metadata.proto client/src/featureform/proto/metadata.proto
 cp proto/serving.proto client/src/featureform/proto/serving.proto
 

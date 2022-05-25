f5bc082d410013438832096bf5a38175ed7eca9c
sdreyer <sterling@featureform.com>
2022-05-25 16:55:29 -0700

    Python client in CICD

--- ff/serving/client/tests/connection_test.py
+++ ff/serving/client/tests/connection_test.py
@@ -1,21 +1,11 @@
-import subprocess
-from time import sleep
-
-import pytest
 from featureform import ServingClient, ResourceClient
 import grpc
-from subprocess import Popen
-
-def pytest_configure(config):
-    def cleanup(proc: Popen):
-        proc.kill()
-    p = subprocess.Popen(["nohup", "go", "run", "../../api/api.go"])
-    sleep(3)
-    config.add_cleanup(p.kill())
+import os
 
 
 def test_metadata_connection():
-    client = ResourceClient("localhost:7878", False)
+    host = os.getenv('API_ADDRESS', "localhost:7878")
+    client = ResourceClient(host, False)
     client.register_user("test")
     try:
         client.apply()
@@ -25,11 +15,10 @@ def test_metadata_connection():
 
 
 def test_serving_connection():
-    client = ServingClient("localhost:7878", False)
+    host = os.getenv('API_ADDRESS', "localhost:7878")
+    client = ServingClient(host, False)
     try:
         client.features([("f1", "v1")], {"user": "a"})
     except grpc.RpcError as e:
         assert (e.details() == "connection error: desc = \"transport: Error while dialing dial tcp: lookup "
                                "sandbox-serving-server: no such host\"")
-
-

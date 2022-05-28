7f94ea0e2c03664a930cee44f9017d2443b8d642
sdreyer <sterling@featureform.com>
2022-05-28 11:23:33 -0700

    Added print statement to see message contents

--- metadata/etcd.go
+++ metadata/etcd.go
@@ -289,6 +289,7 @@ func (lookup etcdResourceLookup) serializeResource(res Resource) ([]byte, error)
 		Message:      p,
 		StorageType:  RESOURCE,
 	}
+	fmt.Println("Attempting to serialize: ", msg)
 	serialMsg, err := json.Marshal(msg)
 	if err != nil {
 		return nil, err

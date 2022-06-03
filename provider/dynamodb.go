12b12972995283785a5e821f6dbe86a934e6b058
Riddhi Bagadiaa <riddhi_b@berkeley.edu>
2022-06-03 14:42:35 -0700

    Passed all Dyanamodb tests

--- provider/dynamodb.go
+++ provider/dynamodb.go
@@ -7,28 +7,41 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	sn "github.com/mrz1836/go-sanitize"
+	"strconv"
 )
 
-//Added by Riddhi
 type dynamodbTableKey struct {
 	Prefix, Feature, Variant string
 }
 
-//Added by Riddhi
 func (t dynamodbTableKey) String() string {
 	marshalled, _ := json.Marshal(t)
 	return string(marshalled)
 }
 
-//added by Riddhi
 type dynamodbOnlineStore struct {
 	client *dynamodb.DynamoDB
 	prefix string
 	BaseProvider
 }
 
-//added by Riddhi
+type dynamodbOnlineTable struct {
+	client    *dynamodb.DynamoDB
+	key       dynamodbTableKey
+	valueType ValueType
+}
+
+type dynamodbItem struct {
+	Entity string `dynamodbav:"Entity"`
+	Value  string `dynamodbav:"FeatureValue"`
+}
+
+type Metadata struct {
+	Tablename string `dynamodbav:"Tablename"`
+	Valuetype string `dynamodbav:"ValueType"`
+}
+
 func dynamodbOnlineStoreFactory(serialized SerializedConfig) (Provider, error) {
 	dynamodbConfig := &DynamodbConfig{}
 	if err := dynamodbConfig.Deserialize(serialized); err != nil {
@@ -40,15 +53,14 @@ func dynamodbOnlineStoreFactory(serialized SerializedConfig) (Provider, error) {
 	return NewDyanamodbOnlineStore(dynamodbConfig), nil
 }
 
-// DONE
-// added by Riddhi
 func NewDyanamodbOnlineStore(options *DynamodbConfig) *dynamodbOnlineStore {
-	mySession := session.Must(session.NewSession())
-	dynamodbClient := dynamodb.New(mySession)
-
-	// create metadata table
+	config := &aws.Config{
+		Endpoint: &options.Addr,
+		Region:   aws.String(options.Region),
+	}
+	sess := session.Must(session.NewSession(config))
+	dynamodbClient := dynamodb.New(sess)
 	CreateMetadataTable(dynamodbClient)
-
 	return &dynamodbOnlineStore{dynamodbClient, options.Prefix, BaseProvider{
 		ProviderType:   DynamoDBOnline,
 		ProviderConfig: options.Serialized(),
@@ -56,18 +68,22 @@ func NewDyanamodbOnlineStore(options *DynamodbConfig) *dynamodbOnlineStore {
 	}
 }
 
+func (store *dynamodbOnlineStore) AsOnlineStore() (OnlineStore, error) {
+	return store, nil
+}
+
 func CreateMetadataTable(dynamodbClient *dynamodb.DynamoDB) {
 	params := &dynamodb.CreateTableInput{
 		TableName: aws.String("Metadata"),
 		AttributeDefinitions: []*dynamodb.AttributeDefinition{
 			{
-				AttributeName: aws.String("table_name"),
+				AttributeName: aws.String("Tablename"),
 				AttributeType: aws.String("S"),
 			},
 		},
 		KeySchema: []*dynamodb.KeySchemaElement{
 			{
-				AttributeName: aws.String("table_name"),
+				AttributeName: aws.String("Tablename"),
 				KeyType:       aws.String("HASH"),
 			},
 		},
@@ -76,94 +92,41 @@ func CreateMetadataTable(dynamodbClient *dynamodb.DynamoDB) {
 			WriteCapacityUnits: aws.Int64(5),
 		},
 	}
-
 	dynamodbClient.CreateTable(params)
 }
 
-// Dymanodb createtable
-func (store *dynamodbOnlineStore) CreateTable(feature, variant string, valueType ValueType) (OnlineStoreTable, error) {
-	key := dynamodbTableKey{store.prefix, feature, variant}
-	params := &dynamodb.CreateTableInput{
-		TableName: aws.String(key.String()),
-		AttributeDefinitions: []*dynamodb.AttributeDefinition{
-			{
-				AttributeName: aws.String(feature),
-				AttributeType: aws.String("S"),
-			},
-		},
-		KeySchema: []*dynamodb.KeySchemaElement{
-			{
-				AttributeName: aws.String(feature),
-				KeyType:       aws.String("HASH"),
-			},
-		},
-		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-			ReadCapacityUnits:  aws.Int64(10),
-			WriteCapacityUnits: aws.Int64(5),
-		},
-	}
-
-	store.client.CreateTable(params)
-	store.UpdateMetadataTable(key.String(), valueType)
-	return &dynamodbOnlineTable{store.client, key, valueType}, nil
-}
-
 func (store *dynamodbOnlineStore) UpdateMetadataTable(tablename string, valueType ValueType) error {
-	primaryKey := map[string]string{
-		"table_name": tablename,
-	}
-	feature, err := dynamodbattribute.MarshalMap(primaryKey)
-	if err != nil {
-		return err
-	}
-
-	upd := expression.
-		Set(expression.Name("value_type"), expression.Value(valueType))
-	expr, err := expression.NewBuilder().WithUpdate(upd).Build()
-	if err != nil {
-		return err
-	}
-
 	input := &dynamodb.UpdateItemInput{
-		TableName:                 aws.String("Metadata"),
-		Key:                       feature,
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
-		UpdateExpression:          expr.Update(),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":valtype": {
+				S: aws.String(string(valueType)),
+			},
+		},
+		TableName: aws.String("Metadata"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Tablename": {
+				S: aws.String(tablename),
+			},
+		},
+		UpdateExpression: aws.String("set ValueType = :valtype"),
 	}
-
-	_, err = store.client.UpdateItemWithContext(ctx, input)
+	_, err := store.client.UpdateItem(input)
 	return err
 }
 
-type Metadata struct {
-	tablename string `dynamodbav:"table_name"`
-	valuetype string `dynamodbav:"value_type"`
-}
-
 func (store *dynamodbOnlineStore) GetFromMetadataTable(tablename string) (ValueType, error) {
-	primaryKey := map[string]string{
-		"table_name": tablename,
-	}
-	table, err := dynamodbattribute.MarshalMap(primaryKey)
-	if err != nil {
-		return NilType, err
-	}
-
-	proj := expression.NamesList(expression.Name("value_type"))
-	expr, err := expression.NewBuilder().WithProjection(proj).Build()
-	if err != nil {
-		return NilType, err
-	}
-
 	input := &dynamodb.GetItemInput{
-		TableName:                aws.String("Metadata"),
-		Key:                      table,
-		ExpressionAttributeNames: expr.Names(),
-		ProjectionExpression:     expr.Projection(),
+		TableName: aws.String("Metadata"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Tablename": {
+				S: aws.String(tablename),
+			},
+		},
 	}
-
-	output_val, err := store.client.GetItemWithContext(ctx, input)
+	output_val, err := store.client.GetItem(input)
+	if len(output_val.Item) == 0 {
+		return NilType, &CustomError{"Table not found"}
+	}
 	if err != nil {
 		return NilType, err
 	}
@@ -171,16 +134,14 @@ func (store *dynamodbOnlineStore) GetFromMetadataTable(tablename string) (ValueT
 	err = dynamodbattribute.UnmarshalMap(output_val.Item, &metadata_item)
 
 	if err != nil {
-		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		return NilType, err
 	}
-
-	return ValueType(metadata_item.valuetype), err
+	return ValueType(metadata_item.Valuetype), err
 }
 
 func (store *dynamodbOnlineStore) GetTable(feature, variant string) (OnlineStoreTable, error) {
 	key := dynamodbTableKey{store.prefix, feature, variant}
-	typeOfValue, err := store.GetFromMetadataTable(key.String())
-	// vType, err := store.client.HGet(ctx, fmt.Sprintf("%s__tables", store.prefix), key.String()).Result()
+	typeOfValue, err := store.GetFromMetadataTable(sn.Custom(key.String(), "[^a-zA-Z0-9_.\\-]"))
 	if err != nil {
 		return nil, &TableNotFound{feature, variant}
 	}
@@ -188,78 +149,101 @@ func (store *dynamodbOnlineStore) GetTable(feature, variant string) (OnlineStore
 	return table, nil
 }
 
-type dynamodbOnlineTable struct {
-	client    *dynamodb.DynamoDB
-	key       dynamodbTableKey
-	valueType ValueType
-}
-
-type dynamodbItem struct {
-	entity string      `dynamodbav:"entity"`
-	value  interface{} `dynamodbav:"value"`
+func (store *dynamodbOnlineStore) CreateTable(feature, variant string, valueType ValueType) (OnlineStoreTable, error) {
+	key := dynamodbTableKey{store.prefix, feature, variant}
+	_, err := store.GetFromMetadataTable(sn.Custom(key.String(), "[^a-zA-Z0-9_.\\-]"))
+	if err == nil {
+		return nil, &TableAlreadyExists{feature, variant}
+	}
+	params := &dynamodb.CreateTableInput{
+		TableName: aws.String(sn.Custom(key.String(), "[^a-zA-Z0-9_.\\-]")),
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String(feature),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String(feature),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+	}
+	err = store.UpdateMetadataTable(sn.Custom(key.String(), "[^a-zA-Z0-9_.\\-]"), valueType)
+	if err != nil {
+		return nil, err
+	}
+	_, err = store.client.CreateTable(params)
+	if err != nil {
+		return nil, err
+	}
+	return &dynamodbOnlineTable{store.client, key, valueType}, nil
 }
 
 func (table dynamodbOnlineTable) Set(entity string, value interface{}) error {
-
-	primaryKey := map[string]string{
-		table.key.Feature: entity,
-	}
-	feature, err := dynamodbattribute.MarshalMap(primaryKey)
-	if err != nil {
-		return err
-	}
-
-	upd := expression.
-		Set(expression.Name("value"), expression.Value(value))
-	expr, err := expression.NewBuilder().WithUpdate(upd).Build()
-	if err != nil {
-		return err
-	}
-
 	input := &dynamodb.UpdateItemInput{
-		TableName:                 aws.String(table.key.String()),
-		Key:                       feature,
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
-		UpdateExpression:          expr.Update(),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":val": {
+				S: aws.String(fmt.Sprintf("%v", value)),
+			},
+		},
+		TableName: aws.String(sn.Custom(table.key.String(), "[^a-zA-Z0-9_.\\-]")),
+		Key: map[string]*dynamodb.AttributeValue{
+			table.key.Feature: {
+				S: aws.String(entity),
+			},
+		},
+		UpdateExpression: aws.String("set FeatureValue = :val"),
 	}
-
-	_, err = table.client.UpdateItemWithContext(ctx, input)
+	_, err := table.client.UpdateItem(input)
 	return err
 }
 
 func (table dynamodbOnlineTable) Get(entity string) (interface{}, error) {
-	primaryKey := map[string]string{
-		table.key.Feature: entity,
-	}
-	feature, err := dynamodbattribute.MarshalMap(primaryKey)
-	if err != nil {
-		return nil, err
-	}
-
-	proj := expression.NamesList(expression.Name("value"))
-	expr, err := expression.NewBuilder().WithProjection(proj).Build()
-	if err != nil {
-		return nil, err
-	}
-
 	input := &dynamodb.GetItemInput{
-		TableName:                aws.String(table.key.String()),
-		Key:                      feature,
-		ExpressionAttributeNames: expr.Names(),
-		ProjectionExpression:     expr.Projection(),
+		TableName: aws.String(sn.Custom(table.key.String(), "[^a-zA-Z0-9_.\\-]")),
+		Key: map[string]*dynamodb.AttributeValue{
+			table.key.Feature: {
+				S: aws.String(entity),
+			},
+		},
 	}
-
-	output_val, err := table.client.GetItemWithContext(ctx, input)
+	output_val, err := table.client.GetItem(input)
+	if len(output_val.Item) == 0 {
+		return nil, &EntityNotFound{entity}
+	}
 	if err != nil {
 		return nil, err
 	}
 	dynamodb_item := dynamodbItem{}
 	err = dynamodbattribute.UnmarshalMap(output_val.Item, &dynamodb_item)
-
 	if err != nil {
-		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		return nil, &EntityNotFound{entity}
 	}
-
-	return dynamodb_item.value, err
+	var result interface{}
+	var result_float float64
+	switch table.valueType {
+	case NilType, String:
+		result, err = dynamodb_item.Value, nil
+	case Int:
+		result, err = strconv.Atoi(dynamodb_item.Value)
+	case Int64:
+		result, err = strconv.ParseInt(dynamodb_item.Value, 0, 64)
+	case Float32:
+		result_float, err = strconv.ParseFloat(dynamodb_item.Value, 32)
+		result = float32(result_float)
+	case Float64:
+		result, err = strconv.ParseFloat(dynamodb_item.Value, 64)
+	case Bool:
+		result, err = strconv.ParseBool(dynamodb_item.Value)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }

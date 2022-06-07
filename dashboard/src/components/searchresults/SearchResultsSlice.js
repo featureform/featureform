911a95438df67e05a000619202a83ff569cac9ab
Sam Inloes <sam@featureform.com>
2022-06-06 17:27:24 -0700

    remove changed search result slice

--- dashboard/src/components/searchresults/SearchResultsSlice.js
+++ dashboard/src/components/searchresults/SearchResultsSlice.js
@@ -14,39 +14,31 @@ const searchResultsSlice = createSlice({
   name: "searchResults",
   // initialState is a map between each resource type to an empty object.
   initialState: initialState,
-  extraReducers: (builder) => {
-    builder.addCase(fetchSearch.fulfilled, (state, action) => {
-      // Add user to the state array
+  extraReducers: {
+    [fetchSearch.pending]: (state, action) => {
       const requestId = action.meta.requestId;
       state.requestId = requestId;
+      state.resources = null;
+      state.loading = true;
+      state.failed = false;
+    },
+    [fetchSearch.fulfilled]: (state, action) => {
+      const requestId = action.meta.requestId;
+      if (requestId !== state.requestId) {
+        return;
+      }
+      state.resources = action.payload;
       state.loading = false;
       state.failed = false;
-      state.resources = action.payload;
-    })
-    // [fetchSearch.pending]: (state, action) => {
-    //   const requestId = action.meta.requestId;
-    //   state.requestId = requestId;
-    //   state.resources = null;
-    //   state.loading = true;
-    //   state.failed = false;
-    // },
-    // [fetchSearch.fulfilled]: (state, action) => {
-    //   const requestId = action.meta.requestId;
-    //   if (requestId !== state.requestId) {
-    //     return;
-    //   }
-    //   state.resources = action.payload;
-    //   state.loading = false;
-    //   state.failed = false;
-    // },
-    // [fetchSearch.rejected]: (state, action) => {
-    //   const requestId = action.meta.requestId;
-    //   if (requestId !== state.requestId) {
-    //     return;
-    //   }
-    //   state.loading = false;
-    //   state.failed = true;
-    // },
+    },
+    [fetchSearch.rejected]: (state, action) => {
+      const requestId = action.meta.requestId;
+      if (requestId !== state.requestId) {
+        return;
+      }
+      state.loading = false;
+      state.failed = true;
+    },
   },
 });
 

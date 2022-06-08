26e0d4e4db05fb98dd8d92f5f56654b269567ec0
sdreyer <sterling@featureform.com>
2022-06-07 17:54:53 -0700

    Shortend dashboard backend build

---
+++ client/examples/quickstart.py
@@ -0,0 +1,78 @@
+# Run in command line with:
+# python3 -m featureform examples/cli.py -host <hostname:port>
+import featureform as ff
+
+user = ff.register_user("test")
+user.make_default_owner()
+postgres = ff.postgres(
+    name="snowflake",
+    host="quickstart-postgres",
+    port=5432,
+    user="postgres",
+    password="password",
+    database="postgres",
+    description="Postgres",
+    team="Featureform Success Team",
+)
+table = postgres.register_table(
+    name="transactions",
+    variant="v1",
+    table="Transactions",
+    description="Transactions file from Kaggle",
+)
+
+#
+# @postgres.sql_transformation(variant="v17")
+# def user_transaction_count():
+#     """the number of transactions for each user"""
+#     return 'SELECT CustomerID as user_id, COUNT(*) as user_transaction_count FROM {{transactions.v2}} GROUP BY user_id' #Removed timestamp since it doesnt make sense to group by user and timestamp unless its binned
+#
+#
+# @postgres.sql_transformation(variant="v17")
+# def average_user_transaction():
+#     """the average transaction amount for a user """
+#     return 'SELECT CustomerID as user_id, avg(TransactionAmount) as avg_transaction_amt from {{transactions.v2}} GROUP BY user_id'
+#
+#
+# @postgres.sql_transformation(variant="v17")
+# def user_has_fraud():
+#     """if the user has had a fraudulent transaction """
+#     return 'SELECT CustomerID as user_id, IFF(COUNT_IF(ISFRAUD=TRUE) > 0, TRUE, FALSE) as has_fraud from {{transactions.v2}} GROUP BY user_id'
+#
+#
+# entity = ff.register_entity("user")
+# redis = ff.register_redis(
+#     name="redis2",
+#     host="my-redis-release-master",
+#     port=6379,
+#     password="pyhm0Bqpcl"
+# )
+#
+# user_transaction_count.register_resources(
+#     entity=entity,
+#     entity_column="user_id",
+#     inference_store=redis,
+#     features=[
+#         {"name": "transaction_count", "variant": "v9", "column": "user_transaction_count", "type": "int64"},
+#     ],
+# )
+#
+# average_user_transaction.register_resources(
+#     entity=entity,
+#     entity_column="user_id",
+#     inference_store=redis,
+#     features=[
+#         {"name": "average_transaction", "variant": "v9", "column": "avg_transaction_amt", "type": "float32"},
+#     ],
+# )
+#
+# user_has_fraud.register_resources(
+#     entity=entity,
+#     entity_column="user_id",
+#     inference_store=redis,
+#     labels=[
+#         {"name": "has_fraud", "variant": "v9", "column": "has_fraud", "type": "bool"},
+#     ],
+# )
+#
+# ff.register_training_set("fraud_training", "v7", label=("has_fraud", "v9"), features=[("transaction_count", "v9"), ("average_transaction", "v9")])

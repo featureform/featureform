5367e3194aa92b167f90ee0a2df9452481e37ba7
sdreyer <sterling@featureform.com>
2022-06-21 15:23:54 -0700

    Reduced worker container size

--- client/src/featureform/register.py
+++ client/src/featureform/register.py
@@ -513,7 +513,6 @@ class Registrar:
 
 
 class Client(Registrar):
-
     def __init__(self, host, tls_verify=True, cert_path=None):
         super().__init__()
         env_cert_path = os.getenv('FEATUREFORM_CERT')

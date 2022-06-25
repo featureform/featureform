22d93402440f0d1398a6af30f8d35f27dc04a54a
Riddhi Bagadiaa <riddhi_b@berkeley.edu>
2022-06-25 00:45:30 -0700

    serving is working

--- client/src/featureform/serving_test.py
+++ client/src/featureform/serving_test.py
@@ -5,3 +5,5 @@ fpf = client.features([("SepalLength", "centimeters")], ("Id", 1))
 print(fpf)
 # fpf = client.features([("SepalLength", "centimeters")], {"CustomerID": "1"})
 # Run features through model
+training_set = client.training_set("iris_training", "quickstart")
+print(training_set)

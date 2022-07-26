867c79824eda6f26c95429d9d7c8686f415397a6
Simba Khadder <simba@featureform.com>
2022-07-26 06:29:50 -0700

    Make hosted/local mode serving have the same API.

--- client/src/featureform/serving.py
+++ client/src/featureform/serving.py
@@ -160,9 +160,11 @@ class Client:
         new_data = func(*dataframes)
         return new_data
 
-    def _local_features(self, feature_variant_list, entity_tuple):
+    def _local_features(self, feature_variant_list, entity):
         if len(feature_variant_list) == 0:
             raise Exception("No features provided")
+        # This code was originally written to take a tuple, this is a quick fix but this whole code block should be refactored.
+        entity_tuple = entity.items()[0]
         dataframe_mapping = []
         all_feature_df = None
         for featureVariantTuple in feature_variant_list:

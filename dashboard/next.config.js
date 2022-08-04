e357fc1e8c92e47e292926a41d8032881628f255
Steffi Tan <steffitlw@berkeley.edu>
2022-08-04 14:46:41 -0700

    Fix nextjs flask deployment

---
+++ dashboard/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: '/.',
+  assetPrefix: '/./'
+}
+
+module.exports = nextConfig

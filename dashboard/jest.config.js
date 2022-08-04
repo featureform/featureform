643cd3e88352c5eef1f319f8d065d859e63f273a
Steffi Tan <steffitlw@berkeley.edu>
2022-08-04 11:32:57 -0700

    Fix bug with jest dirname

--- dashboard/jest.config.js
+++ dashboard/jest.config.js
@@ -11,7 +11,7 @@ const customJestConfig = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['./jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', './'],
+  moduleDirectories: ['node_modules', __dirname],
   testEnvironment: 'jest-environment-jsdom',
 }
 

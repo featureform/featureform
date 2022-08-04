f3519bb8c6e16466945635304d3b15e2d7ba9a7d
Steffi Tan <steffitlw@berkeley.edu>
2022-08-04 14:46:41 -0700

    Fix nextjs flask deployment

---
+++ dashboard/out/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='./', static_url_path='')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)

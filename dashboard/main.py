d9cb589be132e9f153b62c3c739c17b30279c72e
Steffi Tan <steffitlw@berkeley.edu>
2022-08-09 16:29:37 -0700

    Add flask app endpoints for dashboard app

--- dashboard/main.py
+++ dashboard/main.py
@@ -7,6 +7,13 @@ app = Flask(__name__, static_folder='./out/', static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/<type>')
+def type(type):
+    return app.send_static_file('[type].html')
+
+@app.route('/<type>/<entity>')
+def entity(type, entity):
+    return app.send_static_file('[type]/[entity].html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)

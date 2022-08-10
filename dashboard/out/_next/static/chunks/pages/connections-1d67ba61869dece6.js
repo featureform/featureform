564f1a4b9b3840448ff88ae9ccc01243f0f950d1
Steffi Tan <steffitlw@berkeley.edu>
2022-08-09 17:16:13 -0700

    Fixed static asset paths

---
+++ dashboard/out/_next/static/chunks/pages/connections-1d67ba61869dece6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[13],{13860:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/connections",function(){return c(75027)}])},75027:function(a,b,c){"use strict";c.r(b);var d=c(14731);b.default=d.Z}},function(a){a.O(0,[9774,2888,179],function(){var b;return a(a.s=13860)}),_N_E=a.O()}])
---
+++ dashboard/out/_next/static/chunks/react-syntax-highlighter_languages_refractor_hsts.5e3fe34e0eab5fdb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3140],{60561:function(a){function b(a){a.languages.hsts={directive:{pattern:/\b(?:includeSubDomains|max-age|preload)(?=[\s;=]|$)/i,alias:"property"},operator:/=/,punctuation:/;/}}a.exports=b,b.displayName="hsts",b.aliases=[]}}])

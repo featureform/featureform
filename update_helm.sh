db039a517846b84c5ec1cfa314f9ff788c9df0bd
sdreyer <sterling@featureform.com>
2022-08-23 16:05:49 -0700

    Add copy for only non existing items

--- update_helm.sh
+++ update_helm.sh
@@ -3,5 +3,7 @@ set -e
 gsutil cp -r gs://featureform-helm ./
 sed -i -e "s/0.0.0/$1/g" ./charts/featureform/values.yaml # Sets the default image tag value in the chart
 helm package ./charts/featureform -d featureform-helm --app-version $1 --version $1
+rm ./featureform-helm/index.yaml
 helm repo index ./featureform-helm
-gsutil cp ./featureform-helm/* gs://featureform-helm
+gsutil cp ./featureform-helm/index.yaml gs://featureform-helm/index.yaml
+gsutil cp -n ./featureform-helm/* gs://featureform-helm

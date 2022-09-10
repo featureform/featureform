6f63f154299bb8ae0d4b967882f14ea1eb0db0f6
Ahmad Nazeri <nazeriahmed@gmail.com>
2022-09-09 17:27:50 -0700

    adding behave tests

---
+++ features/environment.py
@@ -0,0 +1,32 @@
+"""
+supported methods:
+    before_step(context, step), after_step(context, step)
+        These run before and after every step.
+        The step passed in is an instance of Step.
+    before_scenario(context, scenario), after_scenario(context, scenario)
+        These run before and after each scenario is run.
+        The scenario passed in is an instance of Scenario.
+    before_feature(context, feature), after_feature(context, feature)
+        These run before and after each feature file is exercised.
+        The feature passed in is an instance of Feature.
+    before_tag(context, tag), after_tag(context, tag)
+"""
+
+import os
+import random
+import string
+
+from dotenv import load_dotenv
+
+import featureform as ff 
+
+def before_all(context):
+    load_dotenv("/Users/kempytoor/Documents/Featureform/e2e/featureform/.env")
+
+    redis_provider = os.getenv("REDIS_PROVIDER_NAME")
+    context.redis = ff.get_redis(redis_provider)
+    context.name_suffix = get_random_string()
+
+def get_random_string():
+    return "".join(random.choice(string.ascii_lowercase) for _ in range(5))
+
---
+++ features/steps/step_spark.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+
+from behave import given, when, then
+
+import featureform as ff
+from featureform import ResourceClient
+
+
+@given("Featureform is installed")
+def step_impl(context):
+    output = subprocess.check_output(["pip", "list"], text=True)
+    assert "featureform" in output
+
+
+@given("Featureform is hosted on k8s")
+def step_impl(context):
+    host = os.getenv("FEATUREFORM_HOST")
+    context.rc = ResourceClient(host=host)
+    assert context.rc != None
+
+
+@when("we register a Spark provider")
+def step_impl(context):
+    args = {
+                "name": f"spark-{context.name_suffix}",
+                "description": "test",
+                "team": "featureform",
+                "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
+                "aws_secret_access_key": os.getenv("AWS_SECRET_KEY"),
+                "bucket_path": os.getenv("S3_BUCKET_PATH"),
+                "bucket_region": os.getenv("S3_BUCKET_REGION"),
+                "emr_cluster_id": os.getenv("AWS_EMR_CLUSTER_ID"),
+                "emr_cluster_region": os.getenv("AWS_EMR_CLUSTER_REGION"),
+            }
+    context.spark = ff.register_spark(**args)
+    
+
+
+@when("a Spark parquet file is registered")
+def step_impl(context):
+    context.file = context.spark.register_parquet_file(
+        name=f"transaction_short_{context.name_suffix}",
+        variant="e2e_testing",
+        owner="featureformer",
+        file_path="s3://featureform-spark-testing/featureform/source_datasets/transaction_short/",
+    )
+
+
+@when("Spark SQL transformation is registered")
+def step_impl(context):
+    @context.spark.sql_transformation(name=f"sql_transaction_transformation_{context.name_suffix}", variant="e2e_testing")
+    def average_user_score():
+        """the average score for a user """
+        return f"SELECT CustomerID as user_id, avg(TransactionAmount) as avg_transaction_amt from {{{{ transaction_short_{context.name_suffix}.e2e_testing }}}} GROUP BY user_id"
+
+    context.user = ff.register_entity("user")
+
+    average_user_score.register_resources(
+        entity=context.user,
+        owner=f"featureformer",
+        entity_column="user_id",
+        inference_store=context.redis,
+        features=[
+            {"name": f"avg_transaction_{context.name_suffix}", "variant": "quickstart", "column": "avg_transaction_amt", "type": "float32"},
+        ],
+    )
+
+
+@when("a label is registered")
+def step_impl(context):
+    context.file.register_resources(
+        entity=context.user,
+        owner=f"featureformer_{context.name_suffix}",
+        entity_column="CustomerID",
+        labels=[
+            {"name": f"fraudulent_{context.name_suffix}", "variant": "quickstart", "column": "isfraud", "type": "bool"},
+        ],
+    )
+
+
+@when("training set is registered")
+def step_impl(context):
+    ff.register_training_set(
+        f"fraud_training_{context.name_suffix}", "quickstart",
+        owner=f"featureformer_{context.name_suffix}",
+        label=(f"fraudulent_{context.name_suffix}", "quickstart"),
+        features=[(f"avg_transaction_{context.name_suffix}", "quickstart")],
+    )
+
+
+@then("we can serve the training set")
+def step_impl(context):
+    client = ff.ServingClient()
+    dataset = client.training_set(f"fraud_training_{context.name_suffix}", "quickstart")
+    training_dataset = dataset.repeat(10).shuffle(1000).batch(8)
+    
+    dataset_size = 0
+    for i, feature_batch in enumerate(training_dataset):
+        dataset_size += len(feature_batch.to_list())
+    
+    assert dataset_size == 8000

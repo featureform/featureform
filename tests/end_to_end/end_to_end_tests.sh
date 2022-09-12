5f313926fecf867b139deeb33d6881c5b99c0dc8
Ahmad Nazeri <nazeriahmed@gmail.com>
2022-09-12 12:56:02 -0400

    fixed log message

--- tests/end_to_end/end_to_end_tests.sh
+++ tests/end_to_end/end_to_end_tests.sh
@@ -3,6 +3,6 @@ TESTING_DIRECTORY="$( cd "$(dirname "$0")"/ ; pwd -P )"
 echo "Running the spark definition $TESTING_DIRECTORY/spark_definition.py script"
 featureform apply $TESTING_DIRECTORY/spark_definition.py
 
-echo "Waiting 4 minutes for the jobs to finish"
+echo "Waiting 3 minutes for the jobs to finish"
 sleep 180
 python $TESTING_DIRECTORY/spark_serving.py

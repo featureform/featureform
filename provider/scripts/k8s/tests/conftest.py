f36df15045eedbde8c8daad4f67c2fc935c27194
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2022-10-06 10:29:08 -0700

    Task/k8s pandas df local transformation (#475)
    * adding pandas local sql support
    
    * ignore contents of the output directory but leave include the directory
    
    * fixing the df method to use pandas
    
    * fixing df transformations
    
    * adding df local transformation support
    
    * adding pytest requirements
    
    * added etcd host and port variables

--- provider/scripts/k8s/tests/conftest.py
+++ provider/scripts/k8s/tests/conftest.py
@@ -1,6 +1,8 @@
 import os 
 
+import dill
 import pytest
+
 
 real_path = os.path.realpath(__file__)
 dir_path = os.path.dirname(real_path)
@@ -16,6 +18,18 @@ def local_variables_success():
         "TRANSFORMATION": "SELECT * FROM source_0",
     }
 
+
+@pytest.fixture(scope="module")
+def local_df_variables_success():
+    return {
+        "MODE": "local", 
+        "OUTPUT_URI": f"{dir_path}/test_files/output/local_test/", 
+        "SOURCES": f"{dir_path}/test_files/inputs/transaction",
+        "TRANSFORMATION_TYPE": "df",
+        "TRANSFORMATION": f"{dir_path}/test_files/transformations/same_df.pkl",
+    }
+
+
 @pytest.fixture(scope="module")
 def local_variables_failure():
     return {}
@@ -29,6 +43,8 @@ def k8s_variables_success():
         "SOURCES": f"{dir_path}/test_files/output/local_test",
         "TRANSFORMATION_TYPE": "sql",
         "TRANSFORMATION": "SELECT * FROM source_0",
+        "ETCD_HOST": "HOST_PATH",
+        "ETCD_PORT": "2379,2380",
         "ETCD_USERNAME": "username",
         "ETCD_PASSWORD": "password",
     }
@@ -42,3 +58,29 @@ def k8s_variables_failure():
         "TRANSFORMATION_TYPE": "sql",
         "TRANSFORMATION": "SELECT * FROM source_0",
     }
+
+
+@pytest.fixture(scope="module")
+def k8s_variables_port_not_provided_failure():
+    return {
+        "MODE": "k8s", 
+        "OUTPUT_URI": f"{dir_path}/test_files/output/local_test", 
+        "SOURCES": f"{dir_path}/test_files/output/local_test",
+        "TRANSFORMATION_TYPE": "sql",
+        "TRANSFORMATION": "SELECT * FROM source_0",
+        "ETCD_HOST": "HOST_PATH",
+        "ETCD_USERNAME": "username",
+        "ETCD_PASSWORD": "password",
+    }
+
+
+@pytest.fixture(scope="module")
+def df_transformation():
+    file_path = f"{dir_path}/test_files/transformations/same_df.pkl"
+
+    def transformation(transaction):
+        return transaction
+
+    with open(file_path, "wb") as f:
+        dill.dump(transformation.__code__, f)
+    return file_path

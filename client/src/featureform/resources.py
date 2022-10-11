c49ddcd1ef324f58ee33986e15c626c9bc3b267c
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2022-10-10 21:22:00 -0700

    Adding the k8s azure client  (#480)
    * adding K8sOffline class
    
    * adding k8s azure provider to client
    
    * fixing doc
    
    * fixing the df_transformation doc string
    
    * fixed K8sConfig to K8sAzureConfig
    
    * updated the k8s_azure config
    
    * adding path to k8s_azure config
    
    * fixed the k8s_azure config to correspond with expected config in the offline provider

--- client/src/featureform/resources.py
+++ client/src/featureform/resources.py
@@ -307,8 +307,38 @@ class SparkAWSConfig:
         return bytes(json.dumps(config), "utf-8")
 
 
+@typechecked
+@dataclass
+class K8sAzureConfig:
+    account_name: str
+    account_key: str
+    container_name: str
+    path: str = ""
+
+    def software(self) -> str:
+        return "k8s"
+
+    def type(self) -> str:
+        return "K8S_OFFLINE"
+
+    def serialize(self) -> bytes:
+        config = {
+            "ExecutorType": "K8S",
+            "ExecutorConfig": {},
+            "StoreType": "AZURE",
+            "StoreConfig": {
+                "AccountName": self.account_name,
+                "AccountKey": self.account_key,
+                "BucketName": self.container_name,
+                "Path": self.path,
+            }
+        }
+        return bytes(json.dumps(config), "utf-8")
+
+
+
 Config = Union[
-    RedisConfig, SnowflakeConfig, PostgresConfig, RedshiftConfig, LocalConfig, BigQueryConfig, FirestoreConfig, SparkAWSConfig]
+    RedisConfig, SnowflakeConfig, PostgresConfig, RedshiftConfig, LocalConfig, BigQueryConfig, FirestoreConfig, SparkAWSConfig, K8sAzureConfig]
 
 
 @typechecked

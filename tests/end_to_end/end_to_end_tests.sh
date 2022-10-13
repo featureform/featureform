358d50d44d3c3c75facb067b86b994f023e86bba
Ahmad Nazeri <nazeriahmed@gmail.com>
2022-10-13 07:46:31 -0700

    adding df and sql definitions

--- tests/end_to_end/end_to_end_tests.sh
+++ tests/end_to_end/end_to_end_tests.sh
@@ -9,7 +9,17 @@ featureform apply $TESTING_DIRECTORY/spark_sql_definition.py
 python $TESTING_DIRECTORY/spark_serving.py
 echo -e "Spark SQL Job Completed.\n\n"
 
-echo "Running the spark sql definition $TESTING_DIRECTORY/spark_df_definition.py script"
+echo "Running the spark df definition $TESTING_DIRECTORY/spark_df_definition.py script"
 featureform apply $TESTING_DIRECTORY/spark_df_definition.py
 python $TESTING_DIRECTORY/spark_serving.py
 echo "Spark DF Job Completed."
+
+echo "Running the k8s df definition $TESTING_DIRECTORY/k8s_df_definition.py script"
+featureform apply $TESTING_DIRECTORY/k8s_df_definition.py
+python $TESTING_DIRECTORY/serving.py
+echo "K8s DF Job Completed."
+
+echo "Running the k8s sql definition $TESTING_DIRECTORY/k8s_sql_definition.py script"
+featureform apply $TESTING_DIRECTORY/k8s_sql_definition.py
+python $TESTING_DIRECTORY/serving.py
+echo "K8s SQL Job Completed."
---
+++ tests/end_to_end/k8s_df_definition.py
@@ -0,0 +1,77 @@
+import os
+from dotenv import load_dotenv
+
+import featureform as ff
+
+
+featureform_location = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+env_file_path = os.path.join(featureform_location, ".env")
+load_dotenv(env_file_path)
+
+def get_random_string():
+    import random
+    import string
+    return "".join(random.choice(string.ascii_lowercase) for _ in range(10))
+
+VERSION=get_random_string()
+os.environ["TEST_CASE_VERSION"]=VERSION
+
+# Start of Featureform Definitions
+ff.register_user("featureformer").make_default_owner()
+
+redis = ff.register_redis(
+    name = "redis-quickstart",
+    host="quickstart-redis", # The internal dns name for redis
+    port=6379,
+    description = "A Redis deployment we created for the Featureform quickstart"
+)
+
+k8s = ff.register_k8s_azure(
+    name="k8s",
+    account_name=os.getenv("AZURE_ACCOUNT_NAME", ""),
+    account_key=os.getenv("AZURE_ACCOUNT_KEY", ""),
+    container_name=os.getenv("AZURE_CONTAINER_NAME", ""),
+    path="testing/ff",
+)
+
+transactions = k8s.register_file(
+    name=f"transactions_{VERSION}",
+    variant="quickstart",
+    description="A dataset of fraudulent transactions",
+    path="featureform/testing/primary/name/variant/transactions_short.csv"
+)
+
+@k8s.df_transformation(name=f"average_user_transaction_{VERSION}", 
+                        variant="quickstart",
+                        inputs=[(f"transactions_{VERSION}", "quickstart")])
+def average_user_transaction(transactions):
+    """the average transaction amount for a user """
+    user_tsc = transactions[["CustomerID","TransactionAmount","Timestamp"]]
+    return user_tsc.groupby("CustomerID").agg({'TransactionAmount':'mean','Timestamp':'max'})
+
+user = ff.register_entity("user")
+
+# Register a column from our transformation as a feature
+average_user_transaction.register_resources(
+    entity=user,
+    entity_column="CustomerID",
+    inference_store=redis,
+    features=[
+        {"name": f"avg_transactions_{VERSION}", "variant": "quickstart", "column": "TransactionAmount", "type": "float32"},
+    ],
+)
+
+# Register label from our base Transactions table
+transactions.register_resources(
+    entity=user,
+    entity_column="CustomerID",
+    labels=[
+        {"name": f"fraudulent_{VERSION}", "variant": "quickstart", "column": "IsFraud", "type": "bool"},
+    ],
+)
+
+ff.register_training_set(
+    f"fraud_training_{VERSION}", "quickstart",
+    label=(f"fraudulent_{VERSION}", "quickstart"),
+    features=[(f"avg_transactions_{VERSION}", "quickstart")],
+)

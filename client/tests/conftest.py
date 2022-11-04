a45d30f39c7e77550795532e3baf5a0fb5a7f1ba
Ahmad Nazeri <nazeriahmed@gmail.com>
2022-11-04 08:49:17 -0700

    adding client for spark with databricks

--- client/tests/conftest.py
+++ client/tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 import sys
+
 sys.path.insert(0, 'client/src/')
+
 from featureform.register import Registrar, OfflineSparkProvider
-from featureform.resources import SparkAWSConfig, Provider
+from featureform.resources import SparkConfig, Provider, DatabricksCredentials, AzureFileStoreConfig
 
 pytest_plugins = [
     'connection_test',
@@ -12,7 +14,10 @@ pytest_plugins = [
 def spark_provider():
     r = Registrar()
     r.set_default_owner("tester")
-    config = SparkAWSConfig(emr_cluster_id="",bucket_path="",emr_cluster_region="",bucket_region="",aws_access_key_id="",aws_secret_access_key="")
+    databricks = DatabricksCredentials(username="a", password="b")
+    azure_blob = AzureFileStoreConfig(account_name="", account_key="", container_name="", root_path="")   
+    
+    config = SparkConfig(executor=databricks, filestore=azure_blob)
     provider = Provider(name="spark", function="OFFLINE", description="", team="", config=config)
     
     return OfflineSparkProvider(r, provider)

ae49346c8a40869776a22304deebcae3980cb1ee
sdreyer <sterling@featureform.com>
2022-11-05 14:13:28 -0700

    Update databricks_definition.py (#525)

--- tests/end_to_end/definitions/databricks_definition.py
+++ tests/end_to_end/definitions/databricks_definition.py
@@ -30,7 +30,7 @@ ff.register_user("featureformer").make_default_owner()
 
 # Register blob store because it can be used standalone as an online store as well
 azure_blob = ff.register_blob_store(
-    name="",
+    name="blob-store",
     account_name="testingstoragegen",
     account_key="",
     container_name="databricksdemo",
@@ -52,129 +52,209 @@ spark = ff.register_spark(
     filestore=azure_blob
 )
 
-redis = ff.register_redis(
-    name=f"redis-quickstart",
-    host="quickstart-redis",  # The internal dns name for redis
-    port=6379,
-    description="A Redis deployment we created for the Featureform quickstart"
+mongo = ff.register_mongodb(
+    name="mongodb-quickstart",
+    host=os.getenv("MONGODB_HOST"),
+    port=os.getenv("MONGODB_PORT"),
+    username=os.getenv("MONGODB_USERNAME"),
+    password=os.getenv("MONGODB_PASSWORD"),
+    database=os.getenv("MONGODB_DATABASE"),
+    throughput=10000
 )
 
 payments = spark.register_parquet_file(
     name="payments",
-    variant=f"{VERSION}",
+    variant="default",
     description="This dataset includes data about the orders payment options.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_order_payments_dataset.parquet",
 )
 
 orders = spark.register_parquet_file(
     name="orders",
-    variant=f"{VERSION}",
+    variant="default",
     description="This is the core dataset. From each order you might find all other information.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_orders_dataset.parquet",
 )
 
 customers = spark.register_parquet_file(
     name="customers",
-    variant=f"{VERSION}",
+    variant="default",
     description="This dataset has information about the customer and its location. Use it to identify unique customers in the orders dataset and to find the orders delivery location.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_customers_dataset.parquet",
 )
 
 reviews = spark.register_parquet_file(
     name="reviews",
-    variant=f"{VERSION}",
+    variant="default",
     description="This dataset includes data about the reviews made by the customers.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_order_reviews_dataset.parquet",
 )
 
 order_items = spark.register_parquet_file(
     name="order_items",
-    variant=f"{VERSION}",
+    variant="default",
     description="This dataset includes data about the items purchased within each order.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_order_items_dataset.parquet",
 )
 
 product_category_translation = spark.register_parquet_file(
     name="product_category_translation",
-    variant=f"{VERSION}",
+    variant="default",
     description="Translates the productcategoryname to english.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_product_category_name_translation.parquet",
 )
 
 products = spark.register_parquet_file(
     name="products",
-    variant=f"{VERSION}",
+    variant="default",
     description="This dataset includes data about the products sold by Olist.",
     file_path="https://testingstoragegen.blob.core.windows.net/databricksdemo/ecommerce_products_dataset.parquet",
 )
 
 
-@spark.sql_transformation(name="total_paid_per_customer_day",
-                          variant=f"{VERSION}",
-                          description="the total order value transaction amount for a customer per day")
-def total_paid_per_customer_day():
-    return "select trunc(order_approved_at,'day') as day_date, customer_unique_id, sum(p.payment_value) as total_order_paid " \
+@spark.sql_transformation(name="total_paid_per_customer_per_day",
+                          variant="default",
+                          description="Get the daily total value of payments per customer.")
+def total_paid_per_customer_per_day():
+    return "select trunc(order_approved_at,'day') as day_date, c.customer_unique_id, sum(p.payment_value) as total_customer_order_paid " \
            "from {{payments.default}} p " \
-           "left join {{orders.default}} o on (o.order_id = p.order_id) " \
-           "left join {{customer.default}} c on (o.customer_id = c.customer_id) " \
-           "group by 1,2 "
+           "join {{orders.default}} o on (o.order_id = p.order_id) " \
+           "join {{customers.default}} c on (c.customer_id = o.customer_id) " \
+           "group by 1,2 order by 1 asc "
 
+# Get total payments per day
 @spark.sql_transformation(name="total_paid_per_day",
-                          variant=f"{VERSION}",
-                          description="the total order value transaction amount for a user")
+                          variant="default",
+                          description="Get the daily total value of payments.")
 def total_paid_per_day():
     return "select trunc(order_approved_at,'day') as day_date, sum(p.payment_value) as total_order_paid " \
            "from {{payments.default}} p " \
-           "left join {{orders.default}} o on (o.order_id = p.order_id) " \
-           "left join {{customer.default}} c on (o.customer_id = c.customer_id) " \
-           "group by 1 "
+           "join {{orders.default}} o on (o.order_id = p.order_id) " \
+           "group by 1 order by 1 asc "
+
+@spark.df_transformation(inputs=[("total_paid_per_customer_per_day", "default")], variant="default")
+def average_daily_transaction(df):
+    from pyspark.sql.functions import mean
+    df.groupBy("day_date").agg(mean("total_customer_order_paid").alias("average_order_value"))
+    return df
 
 @spark.sql_transformation(name="reviews_by_order",
-                          variant=f"{VERSION}",
+                          variant="default",
                           description="get reviews by order")
 def reviews_by_order():
     return "select order_id, review_score, " \
            "concat(review_comment_title, '-',review_comment_message) as review_text " \
-           "from {{reviews.default}} r"
+           "from {{reviews.default}} r " \
+           "where (review_comment_title is not null) and (review_comment_message is not null) " \
+           "and (review_comment_title <> '') and (review_comment_message <> '') " \
+           "limit 50"
 
-@spark.sql_transformation(name="product_item_spend_by_customer_order",
-                          variant=f"{VERSION}",
-                          description="product items and spend by order")
-def product_item_spend_by_customer_order():
-    return "select concat(o.order_id,'-',p.product_id) as order_product_id, " \
-           "c.customer_unique_id, o.order_id, t.product_category_name_english, i.price " \
-           "from {{order_items.default}} i " \
-           "left join {{orders.default}} o on (o.order_id = i.order_id) " \
-           "left join {{products.default}} p on (p.product_id  = i.product_id) " \
-           "left join {{customer.default}} c on (c.customer_id = o.customer_id) " \
-           "left join {{product_category_translation.default}} t on (t.product_category_name = p.product_category_name) "
+@spark.sql_transformation(name="calculate_month_1_month_6_dates",
+                          variant="default",
+                          description="Calculate month 1 - 6")
+def calculate_month_1_month_6_dates():
+    return "select customer_unique_id, to_date(earliest_purchase) as earliest_datetime_purchase, " \
+           "date_add(to_date(earliest_purchase),30) as  month_1_datetime, " \
+           "date_add(to_date(earliest_purchase),150) as  month_6_datetime " \
+           "from (select c.customer_unique_id, min(o.order_approved_at) as earliest_purchase " \
+           "from {{orders.default}} o join {{customers.default}} c on (c.customer_id = o.customer_id) group by 1 " \
+           "limit 100)"
 
-@spark.sql_transformation(name="order_value_by_customer_order",
-                          variant=f"{VERSION}",
-                          description="order value by customer, order")
-def order_value_by_customer_order():
-    return "select o.order_id, c.customer_unique_id, o.order_approved_at, p.payment_value " \
+@spark.sql_transformation(name="get_month_1_customer_spend",
+                          variant="default",
+                          description="Get month 1 customer spend")
+def get_month_1_customer_spend():
+    return "select cm.customer_unique_id, sum(p.payment_value) as month_1_value " \
            "from {{orders.default}} o " \
-           "left join {{customers.default}} c on (c.customer_id = o.customer_id) " \
-           "left join {{payments.default}} p on (p.order_id = o.order_id) "
+           "join {{customers.default}} c on (c.customer_id = o.customer_id) " \
+           "join {{calculate_month_1_month_6_dates.default}} cm on (cm.customer_unique_id = c.customer_unique_id) " \
+           "join {{payments.default}} p on (p.order_id = o.order_id) " \
+           "where (to_date(o.order_approved_at) < month_1_datetime) or (to_date(o.order_approved_at) = month_1_datetime) " \
+           "group by 1"
+
+@spark.sql_transformation(name="get_month_6_customer_spend",
+                          variant="default",
+                          description="Get month 6 customer spend")
+def get_month_6_customer_spend():
+    return "select cm.customer_unique_id, sum(p.payment_value) as month_6_value " \
+           "from {{orders.default}} o " \
+           "join {{customers.default}} c on (c.customer_id = o.customer_id) " \
+           "join {{calculate_month_1_month_6_dates.default}} cm on (cm.customer_unique_id = c.customer_unique_id) " \
+           "join {{payments.default}} p on (p.order_id = o.order_id) " \
+           "where (to_date(o.order_approved_at) < month_6_datetime) and (to_date(o.order_approved_at) > month_1_datetime) " \
+           "group by 1"
 
 order = ff.register_entity("order")
 customer = ff.register_entity("customer")
+daydate = ff.register_entity("daydate")
+
+total_paid_per_day.register_resources(
+    entity=daydate,
+    entity_column="day_date",
+    inference_store=mongo,
+    labels=[
+        {"name": "total_order_paid", "variant": "default", "column": "total_order_paid", "type": "float32"},
+    ]
+)
+
+average_daily_transaction.register_resources(
+    entity=daydate,
+    entity_column="day_date",
+    inference_store=mongo,
+    features=[
+        {"name": "average_order_value", "variant": "default", "column": "average_order_value", "type": "float32"},
+    ],
+)
 
 reviews_by_order.register_resources(
     entity=order,
     entity_column="order_id",
-    inference_store=redis,
+    inference_store=mongo,
     features=[
-        {"name": "review_text", "variant": f"{VERSION}", "column": "review_text", "type": "str"},
+        {"name": "review_text", "variant": "default", "column": "review_text", "type": "str"},
     ],
     labels=[
-        {"name": "review_score", "variant": f"{VERSION}", "column": "review_score", "type": "int"},
+        {"name": "review_score", "variant": "default", "column": "review_score", "type": "int"},
+    ]
+)
+
+calculate_month_1_month_6_dates.register_resources(
+    entity=customer,
+    entity_column="customer_unique_id",
+    inference_store=mongo,
+    features=[
+        {"name": "earliest_datetime_purchase", "variant": "default", "column": "earliest_datetime_purchase", "type": "date"},
+        {"name": "month_1_datetime", "variant": "default", "column": "month_1_datetime", "type": "date"},
+        {"name": "month_6_datetime", "variant": "default", "column": "month_6_datetime", "type": "date"},
+    ],
+)
+
+get_month_1_customer_spend.register_resources(
+    entity=customer,
+    entity_column="customer_unique_id",
+    inference_store=mongo,
+    features=[
+        {"name": "month_1_value", "variant": "default", "column": "month_1_value", "type": "float32"},
+    ],
+)
+
+get_month_6_customer_spend.register_resources(
+    entity=customer,
+    entity_column="customer_unique_id",
+    inference_store=mongo,
+    labels=[
+        {"name": "month_6_value", "variant": "default", "column": "month_6_value", "type": "float32"},
     ]
 )
 
 ff.register_training_set(
+    "customerLTV_training",
+    label="month_6_value",
+    features=["month_1_value"],
+)
+
+ff.register_training_set(
     "sentiment_prediction",
-    label=("review_score", f"{VERSION}"),
-    features=[("review_text", f"{VERSION}")],
+    label="review_score",
+    features=["review_text"],
 )

f9432eefc5ae808d6a714e25d90f7d30b62ac3c1
sdreyer <sterling@featureform.com>
2022-11-10 12:30:53 -0800

    Removed Files

--- training.py
+++
@@ -1,9 +0,0 @@
-from featureform import ServingClient
-
-client = ServingClient(insecure=True)
-dataset = client.training_set("fraud_training")
-
-for i, batch in enumerate(dataset):
-    print(batch)
-    if i > 25:
-        break

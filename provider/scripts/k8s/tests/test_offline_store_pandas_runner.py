2853ee1172bc52411d7b55e1e96da2901eb61a30
Ahmad Nazeri <nazeriahmed@gmail.com>
2022-11-15 12:26:35 -0500

    fixed local tests

--- provider/scripts/k8s/tests/test_offline_store_pandas_runner.py
+++ provider/scripts/k8s/tests/test_offline_store_pandas_runner.py
@@ -168,7 +168,7 @@ def test_upload_blob_to_blob_store(blob, file, container_client):
 def set_environment_variables(variables, delete=False):
     for key, value in variables.items():
         if key == "AZURE_CONNECTION_STRING":
-            pass 
+            continue 
 
         if delete:
             os.environ.pop(key)

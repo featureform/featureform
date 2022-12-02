dfb8207ced5a43fdc76757b72d3163a98c74e7a0
Sam Inloes <sam@featureform.com>
2022-12-02 11:07:51 -0800

    make test paramaterized

--- client/tests/get_status_test.py
+++ client/tests/get_status_test.py
@@ -1,155 +1,56 @@
 import pytest
 import featureform as ff
-from featureform.resources import TrainingSet, Feature, ResourceColumnMapping
+from featureform.resources import TrainingSet, Feature, ResourceColumnMapping, ResourceStatus
 from featureform.serving import ServingClient
 
 from datetime import timedelta
 
 from .proto import metadata_pb2_grpc, metadata_pb2
 
-def test_pending_training_set_hosted(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetTrainingSetVariants'
-        return_value=metadata_pb2.TrainingSetVariant(
-            name="",
-            status="PENDING"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_training_set("", "").get_status()
-    assert status == ff.ResourceStatus.PENDING
-    
+def resource_with_status(resource, status):
+    resource.status = status
+    return resource
 
-def test_pending_feature(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetFeatures'
-        return_value=metadata_pb2.FeatureVariant(
-            name="",
-            status="PENDING"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_feature("", "").get_status()
-    assert status == ff.ResourceStatus.PENDING
+proto_get_functions = {
+    "training_set": {
+        "proto_stub": 'metadata_pb2_grpc.ApiStub.GetTrainingSetVariants',
+        "proto_resource", metadata_pb2.TrainingSet(name="name", variant="variant", description="description")
+        "mock_return": resource_with_status,
+        "client_function": ResourceClient(host="mock_host", local=False, insecure=True, cert_path="").get_training_set("",""),
+    },
+    "feature": {
+        "proto_stub": 'metadata_pb2_grpc.ApiStub.GetFeatureVariants',
+        "proto_resource", metadata_pb2.Feature(name="name", variant="variant", description="description")
+        "mock_return": resource_with_status,
+        "client_function": ResourceClient(host="mock_host", local=False, insecure=True, cert_path="").get_feature("",""),
+    },
+    "source": {
+        "proto_stub": 'metadata_pb2_grpc.ApiStub.GetSourceVariants',
+        "proto_resource", metadata_pb2.Source(name="name", variant="variant", description="description")
+        "mock_return": resource_with_status,
+        "client_function": ResourceClient(host="mock_host", local=False, insecure=True, cert_path="").get_source("",""),
+    },
+    "label": {
+        "proto_stub": 'metadata_pb2_grpc.ApiStub.GetLabelVariants',
+        "proto_resource", metadata_pb2.Label(name="name", variant="variant", description="description")
+        "mock_return": resource_with_status,
+        "client_function": ResourceClient(host="mock_host", local=False, insecure=True, cert_path="").get_label("",""),
+    }
+}
 
-def test_pending_source(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetSources'
-        return_value=metadata_pb2.SourceVariant(
-            name="",
-            status="PENDING"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_source("", "").get_status()
-    assert status == ff.ResourceStatus.PENDING
+status_map = {
+    ResourceStatus.CREATED: metadata_pb2.ResourceStatus(status=metadata_pb2.CREATED),
+    ResourceStatus.PENDING: metadata_pb2.ResourceStatus(status=metadata_pb2.PENDING),
+    ResourceStatus.READY: metadata_pb2.ResourceStatus(status=metadata_pb2.READY),
+    ResourceStatus.FAILED: metadata_pb2.ResourceStatus(status=metadata_pb2.FAILED)
+}
 
-def test_pending_label(mocker):
+@pytest.mark.parametrize("mocker,functions"
+    [(mocker, value) for value in proto_get_functions.values()])
+@pytest.mark.parametrize("status", [(key, status_map[key]) for key in status_map.key()])
+def test_expected_status(mocker, functions, status):
     mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetLabels'
-        return_value=metadata_pb2.LabelVariant(
-            name="",
-            status="PENDING"
-        )
+        functions["proto_stub"],
+        return_value=[functions["mock_return"](functions["proto_resource"],status[1])]
     )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_label("", "").get_status()
-    assert status == ff.ResourceStatus.PENDING
-Hosted Ready
-def test_ready_training_set_hosted(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetTrainingSetVariants'
-        return_value=metadata_pb2.TrainingSetVariant(
-            name="",
-            status="READY"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_training_set("", "").get_status()
-    assert status == ff.ResourceStatus.READY
-    
-
-def test_ready_feature(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetFeatures'
-        return_value=metadata_pb2.FeatureVariant(
-            name="",
-            status="READY"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_feature("", "").get_status()
-    assert status == ff.ResourceStatus.READY
-
-def test_ready_source(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetSources'
-        return_value=metadata_pb2.SourceVariant(
-            name="",
-            status="READY"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_source("", "").get_status()
-    assert status == ff.ResourceStatus.READY
-
-def test_ready_label(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetLabels'
-        return_value=metadata_pb2.LabelVariant(
-            name="",
-            status="READY"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_label("", "").get_status()
-    assert status == ff.ResourceStatus.READY
-Hosted Failed
-def test_failed_training_set_hosted(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetTrainingSetVariants'
-        return_value=metadata_pb2.TrainingSetVariant(
-            name="",
-            status="FAILED"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_training_set("", "").get_status()
-    assert status == ff.ResourceStatus.FAILED
-    
-
-def test_failed_feature(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetFeatures'
-        return_value=metadata_pb2.FeatureVariant(
-            name="",
-            status="FAILED"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_feature("", "").get_status()
-    assert status == ff.ResourceStatus.FAILED
-
-def test_failed_source(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetSources'
-        return_value=metadata_pb2.SourceVariant(
-            name="",
-            status="FAILED"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_source("", "").get_status()
-    assert status == ff.ResourceStatus.FAILED
-
-def test_failed_label(mocker):
-    mocker.patch(
-        'metadata_pb2_grpc.ApiStub.GetLabels'
-        return_value=metadata_pb2.LabelVariant(
-            name="",
-            status="FAILED"
-        )
-    )
-    client=ResourceClient(host="mock_host", local=False, insecure=True, cert_path="")
-    status = client.get_label("", "").get_status()
-    assert status == ff.ResourceStatus.FAILED
+    assert status[0] == functions["client_function"].get_status()

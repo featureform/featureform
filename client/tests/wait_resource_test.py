2c423c5a41684edb8a052007b1672b6a394ae6f3
Sam Inloes <sam@featureform.com>
2022-12-02 12:03:38 -0800

    paramaterize wait test

--- client/tests/wait_resource_test.py
+++ client/tests/wait_resource_test.py
@@ -5,61 +5,37 @@ from featureform.serving import ServingClient
 
 from datetime import timedelta
 
-
 def wait_function_success(timeout=None):
     time.sleep(1)
 
-def wait_function_fail(timeout=None):
+def wait_function_failure(timeout=None):
     raise ValueError("Resource took to long to return")
     
+test_resources = {
+    "feature": Feature(),
+    "label": Label(),
+    "training_set": TrainingSet(),
+    "source": Source(),
+}
 
-def test_wait_feature(wait_function):
-    feature = Feature(name="test_name",
-                variant="test_variant")
-    feature.wait_function = wait_function
+wait_functions = {
+    "success": wait_function_success,
+    "failure": wait_function_failure,
+}
+
+@pytest.mark.parametrize("resource", [resource for resource in test_resources.values()])
+def test_wait_success(resource):
+    resource.wait_function = wait_function_success
     try:
-        feature.wait()
+        resource.wait()
     
 
-def test_wait_label(wait_function):
-    feature = Feature(name="test_name",
-                variant="test_variant")
-    feature.wait_function = wait_function
+@pytest.mark.parametrize("resource", [resource for resource in test_resources.values()])
+def test_wait_failure(resource):
+    resource.wait_function = wait_function_failure
     try:
-        feature.wait()
-
-def test_wait_source(wait_function):
-    feature = Feature(name="test_name",
-                variant="test_variant")
-    feature.wait_function = wait_function
-    try:
-        feature.wait()
-
-def test_wait_trainingset(wait_function):
-    feature = Feature(name="test_name",
-                variant="test_variant")
-    feature.wait_function = wait_function
-    try:
-        feature.wait()
+        resource.wait()
+    except ValueError as actual_error:
+        assert actual_error.args[0] == f'Resource took to long to return'
 
 
-def test_waits():
-    wait_tests = {
-        "feature": test_wait_feature,
-        "training set": test_wait_trainingset,
-        "source": test_wait_source,
-        "label": test_wait_label
-    }
-
-    wait_functions = {
-        "success": wait_function_success,
-        "failure": wait_function_failure
-    }
-
-    for test in wait_tests:
-        wait_tests[test](wait_functions["success"])
-    for test in wait_tests:
-        try:
-            wait_tests[test](wait_functions["failure"])
-        except ValueError as actual_error:
-            actual_error.args[0] == f'Resource took to long to return'

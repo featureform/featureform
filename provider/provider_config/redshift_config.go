4b862c6332470f6f5993a72d2b3ae7cb5805aeb7
Erik Eppel <erik@featureform.com>
2023-02-27 12:50:19 -0800

    adds additional config methods to redshift config with unit tests

--- provider/provider_config/redshift_config.go
+++ provider/provider_config/redshift_config.go
@@ -1,6 +1,10 @@
 package provider_config
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	ss "github.com/featureform/helpers/string_set"
+)
 
 type RedshiftConfig struct {
 	Endpoint string
@@ -24,4 +28,16 @@ func (rs *RedshiftConfig) Serialize() []byte {
 		panic(err)
 	}
 	return conf
+}
+
+func (rs RedshiftConfig) MutableFields() ss.StringSet {
+	return ss.StringSet{
+		"Username": true,
+		"Password": true,
+		"Port":     true,
+	}
+}
+
+func (a RedshiftConfig) DifferingFields(b RedshiftConfig) (ss.StringSet, error) {
+	return differingFields(a, b)
 }

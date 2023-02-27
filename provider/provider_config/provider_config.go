2e0745851ea2f280085593000b1f183c19c3dacc
Erik Eppel <erik@featureform.com>
2023-02-27 10:32:08 -0800

    moves examples GCP credentials into central location

--- provider/provider_config/provider_config.go
+++ provider/provider_config/provider_config.go
@@ -7,6 +7,19 @@ import (
 	si "github.com/featureform/helpers/struct_iterator"
 )
 
+const EXAMPLE_GCP_CREDENTIALS = `{
+	"type": "service_account",
+	"project_id": "",
+	"private_key_id": "",
+	"private_key": "-----BEGIN PRIVATE KEY----------END PRIVATE KEY-----\n",
+	"client_email": "",
+	"client_id": "",
+	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+	"token_uri": "https://oauth2.googleapis.com/token",
+	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+	"client_x509_cert_url": ""
+}`
+
 type SerializedConfig []byte
 
 func differingFields(a, b interface{}) (ss.StringSet, error) {

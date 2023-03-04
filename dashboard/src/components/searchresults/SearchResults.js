ba84e5e2dc06368649233ad561362024c8afe7d3
sdreyer <sterling@featureform.com>
2023-03-04 10:44:41 -0800

    Revert "Fixes for dashboard search (#660)"
    This reverts commit 1ea101850b02742fada09240c43d080413bfb7c9.

--- dashboard/src/components/searchresults/SearchResults.js
+++ dashboard/src/components/searchresults/SearchResults.js
@@ -30,18 +30,13 @@ const SearchResults = ({ searchResults, api, setVariant, ...props }) => {
   return (
     <div>
       {searchResults.resources ? (
-          <div>
-            {console.log("blah", searchResults.resources)}
         <SearchResultsView
           results={searchResults.resources}
           search_query={search_query}
           setVariant={setVariant}
         />
-          </div>
       ) : (
-        <div>
-          No Results
-        </div>
+        <div></div>
       )}
     </div>
   );

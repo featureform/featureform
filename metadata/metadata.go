1ea101850b02742fada09240c43d080413bfb7c9
sdreyer <sterling@featureform.com>
2023-03-04 10:44:27 -0800

    Fixes for dashboard search (#660)

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1085,8 +1085,8 @@ func NewMetadataServer(config *Config) (*MetadataServer, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not configure storage provider: %v", err)
 	}
-	if config.TypeSenseParams != nil {
-		searcher, errInitializeSearch := search.NewTypesenseSearch(config.TypeSenseParams)
+	if config.SearchParams != nil {
+		searcher, errInitializeSearch := search.NewMeilisearch(config.SearchParams)
 		if errInitializeSearch != nil {
 			return nil, errInitializeSearch
 		}
@@ -1174,7 +1174,7 @@ func (sp EtcdStorageProvider) GetResourceLookup() (ResourceLookup, error) {
 
 type Config struct {
 	Logger          *zap.SugaredLogger
-	TypeSenseParams *search.TypeSenseParams
+	SearchParams    *search.MeilisearchParams
 	StorageProvider StorageProvider
 	Address         string
 }

7b9dfd1acc03a54bfd797def62eb125cd9797c26
sdreyer <sterling@featureform.com>
2023-03-07 09:07:37 -0800

    Added featureform version to the helm chart

--- config/config.go
+++ config/config.go
@@ -7,3 +7,7 @@ const PandasBaseImage = "featureformcom/k8s_runner"
 func GetPandasRunnerImage() string {
 	return helpers.GetEnv("PANDAS_RUNNER_IMAGE", PandasBaseImage)
 }
+
+func GetFeatureformVersion() string {
+	return helpers.GetEnv("FEATUREFORM_VERSION", "0.0.0")
+}

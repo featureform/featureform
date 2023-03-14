94a32fe8bed64421b41b00326034ea595d1549bc
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-03-14 11:28:41 -0400

    removed log

--- provider/filestore.go
+++ provider/filestore.go
@@ -298,7 +298,6 @@ func NewGCSFileStore(config Config) (FileStore, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not serialize GCS config: %v", err)
 	}
-	fmt.Println("Serialized", serializedFile)
 
 	creds, err := google.CredentialsFromJSON(context.TODO(), serializedFile, "https://www.googleapis.com/auth/cloud-platform")
 	if err != nil {

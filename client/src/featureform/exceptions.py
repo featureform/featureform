c81a349e0c6c64b9a5ff14a0c93fa3fc3014afa9
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-03-30 14:28:48 -0400

    renamed type to category

--- client/src/featureform/exceptions.py
+++ client/src/featureform/exceptions.py
@@ -1,10 +1,10 @@
 
-class InvalidTrainingSetFeatureType(Exception):
-    def __init__(self, feature_name, feature_variant, type='ON_DEMAND_CLIENT', message=None):
+class InvalidTrainingSetFeatureCategory(Exception):
+    def __init__(self, feature_name, feature_variant, category='ON_DEMAND_CLIENT', message=None):
         if message == None:
             message = (
-                f"Feature '{feature_name}:{feature_variant}' is of type '{type}'. "
-                f"Cannot use '{type}' feature variant type for training sets. "
+                f"Feature '{feature_name}:{feature_variant}' is of category '{category}'. "
+                f"Cannot use '{category}' feature variant category for training sets. "
             )
 
         Exception.__init__(self, message)

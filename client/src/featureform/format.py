cbb0a37f3bdaf45bebc4e0df156276e2b02172cd
Ali Olfat <ali@featureform.com>
2023-04-01 23:03:21 -0700

    Display statuses for hosted mode

--- client/src/featureform/format.py
+++ client/src/featureform/format.py
@@ -1,8 +1,19 @@
+import time
+from typing import Type, List
+
+from dataclasses import dataclass
+from featureform import Feature, TrainingSet, Label, Source, Provider
+from rich.console import Console
+from rich.live import Live
+from rich.table import Table
+from rich.text import Text
+
 two_row_spacing = "{:<30} {:<25}"
 three_row_spacing = "{:<30} {:<30} {:<30}"
 four_row_spacing = "{:<30} {:<30} {:<30} {:<30}"
 five_row_spacing = "{:<30} {:<30} {:<30} {:<30} {:<30}"
 divider = "-----------------------------------------------"
+
 
 def format_rows(format_obj, format_obj_2=None, format_obj_3=None, format_obj_4=None, format_obj_5=None):
     # Base case for when `format_obj` is a string
@@ -20,6 +31,98 @@ def format_rows(format_obj, format_obj_2=None, format_obj_3=None, format_obj_4=N
     else:
         print(five_row_spacing.format(format_obj, format_obj_2, format_obj_3, format_obj_4, format_obj_5))
 
+
 def format_pg(s=""):
     print(divider)
     print(s)
+
+
+def display_statuses(stub, resources):
+    @dataclass
+    class Status:
+        resource_type: Type
+        name: str
+        variant: str
+        status: str
+        error: str
+
+    def get_statuses() -> List[Status]:
+        statuses = []
+        resources_to_check = {Feature, TrainingSet, Label, Source, Provider}
+        filtered_resources = filter(lambda r: type(r) in resources_to_check, resources)
+        for r in filtered_resources:
+            if r.name == "local-model":
+                continue
+            resource = r.get(stub)
+            variant = getattr(resource, "variant", "")
+            status = Status(
+                resource_type=type(r),
+                name=resource.name,
+                variant=variant,
+                status=resource.status,
+                error=resource.error,
+            )
+            statuses.append(status)
+        return statuses
+
+    def is_finished(statuses):
+        return all(
+            s.status == "READY"
+            or s.status == "FAILED"
+            or (s.resource_type is Provider and s.status == "NO_STATUS")
+            for s in statuses
+        )
+
+    print()
+    console = Console()
+    with Live(console=console, auto_refresh=True, screen=False) as live:
+        while True:
+            statuses = get_statuses()
+            finished_running = is_finished(statuses)
+
+            title = f"[green]COMPLETED[/]" if finished_running else f"[yellow]RUNNING...[/]"
+            table = Table(
+                title=title,
+                title_justify="left",
+                show_header=True,
+                header_style="bold",
+                box=None,
+            )
+            table.add_column("Resource Type")
+            table.add_column("Name (Variant)", width=50, no_wrap=True)
+            table.add_column("Status", width=10)
+            table.add_column("Error", style="red")
+
+            for status in statuses:
+                error = f" {status.error}" if status.error else ""
+                resource_type = status.resource_type.__name__
+                name = status.name
+                status_text = status.status
+
+                if status.resource_type is Provider:
+                    if status.status == "NO_STATUS":
+                        status_text = "CREATED"
+                        status_style = "green"
+                elif status.status == "READY":
+                    status_style = "green"
+                elif status.status == "PENDING":
+                    status_style = "yellow"
+                elif status.status == "NO_STATUS":
+                    status_style = "blue"
+                else:
+                    status_style = "red"
+
+                table.add_row(
+                    resource_type,
+                    f"{name} ({status.variant})",
+                    Text(status_text, style=status_style),
+                    error,
+                )
+
+            live.update(table)
+            live.refresh()
+
+            if finished_running:
+                break
+
+            time.sleep(1)
--- gen_grpc.sh
+++ gen_grpc.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-mkdir client/src/featureform/proto/
+mkdir -p client/src/featureform/proto/
 set -e
 cp metadata/proto/metadata.proto client/src/featureform/proto/metadata.proto
 cp proto/serving.proto client/src/featureform/proto/serving.proto

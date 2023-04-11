db5b3ebc6c88d82419ce17ee299f959dcac4959e
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-04-11 10:49:47 -0700

    dynamic bucket path

--- tests/end_to_end/definitions/spark_emr_s3_definitions.py
+++ tests/end_to_end/definitions/spark_emr_s3_definitions.py
@@ -55,10 +55,14 @@ emr = ff.EMRCredentials(
     credentials=aws_creds,
 )
 
+bucket_path = os.getenv("S3_BUCKET_PATH", None)
+if bucket_path:
+    bucket_path = f"{bucket_path}/end_to_end_tests/{VERSION}"
+
 s3 = ff.register_s3(
     name=f"s3-quickstart_{VERSION}",
     credentials=aws_creds,
-    bucket_path=os.getenv("S3_BUCKET_PATH", None),
+    bucket_path=bucket_path,
     bucket_region=os.getenv("S3_BUCKET_REGION", None),
 )
 

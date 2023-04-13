2e8ff6f62a1f10a0492ee4143033cee7e1f5b73f
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-04-13 06:19:25 -0700

    reverted back to original doc link

--- dashboard/src/components/resource-list/ResourceListView.js
+++ dashboard/src/components/resource-list/ResourceListView.js
@@ -467,7 +467,7 @@ const NoDataMessage = ({ type }) => {
   const classes = useStyles();
 
   function redirect() {
-    window.location.href = "https://docs.featureform.com/getting-started/overview";
+    window.location.href = "https://docs.featureform.com/quickstart";
   }
   return (
     <Container>

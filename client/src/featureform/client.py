d0f22bcc820181d03d91fc7ec30e7785973587aa
Erik Eppel <erik@featureform.com>
2023-04-20 16:02:16 -0700

    adds support for passing a source to compute_df and docstrings to new client class

--- client/src/featureform/client.py
+++ client/src/featureform/client.py
@@ -1,11 +1,34 @@
+from typing import Union
 import warnings
-from .register import ResourceClient
+from .register import (
+    ResourceClient,
+    SourceRegistrar,
+    LocalSource,
+    SubscriptableTransformation,
+)
 from .serving import ServingClient
 from .enums import ApplicationMode
 import pandas as pd
 
 
 class Client(ResourceClient, ServingClient):
+    """
+    Client for interacting with Featureform APIs (resources and serving)
+
+    **Using the Client:**
+    ```py title="definitions.py"
+    import featureform as ff
+    from featureform import Client
+
+    client = Client("http://localhost:8080")
+
+    # Example 1: Get a registered provider
+    redis = client.get_provider("redis-quickstart")
+
+    # Example 2: Compute a dataframe from a registered source
+    transactions_df = client.compute_df("transactions", "quickstart")
+    """
+
     def __init__(
         self, host=None, local=False, insecure=False, cert_path=None, dry_run=False
     ):
@@ -25,7 +48,34 @@ class Client(ResourceClient, ServingClient):
                 ApplicationMode.LOCAL if local else ApplicationMode.HOSTED
             )
 
-    def compute_df(self, name, variant="default"):
+    def compute_df(
+        self,
+        source: Union[SourceRegistrar, LocalSource, SubscriptableTransformation, str],
+        variant="default",
+    ):
+        """
+        Compute a dataframe from a registered source or transformation
+
+        Args:
+            source (Union[SourceRegistrar, LocalSource, SubscriptableTransformation, str]): The source or transformation to compute the dataframe from
+            variant (str): The source variant; defaults to "default" and is ignored if source argument is not a string
+
+        **Example:**
+        ```py title="definitions.py"
+        transactions_df = client.compute_df("transactions", "quickstart")
+
+        avg_user_transaction_df = transactions_df.groupby("CustomerID")["TransactionAmount"].mean()
+        """
+        if isinstance(
+            source, (SourceRegistrar, LocalSource, SubscriptableTransformation)
+        ):
+            name, variant = source.name_variant()
+        elif isinstance(source, str):
+            name = source
+        else:
+            raise ValueError(
+                f"source must be of type SourceRegistrar, LocalSource, SubscriptableTransformation or str, not {type(source)}"
+            )
         if self.application_mode == ApplicationMode.LOCAL:
             return self.impl.get_input_df(name, variant)
         elif self.application_mode == ApplicationMode.HOSTED:

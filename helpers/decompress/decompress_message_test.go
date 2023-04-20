ad6c6632fe4263e7eca4f89c0cd8b7ef6b7be41e
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2023-04-20 12:11:48 -0700

    returning error messages from EMR step to user (#747)

---
+++ helpers/decompress/decompress_message_test.go
@@ -0,0 +1,61 @@
+package decompress
+
+import (
+	"bytes"
+	"compress/gzip"
+	"testing"
+)
+
+func TestDecompressMessage(t *testing.T) {
+	type TestCase struct {
+		InputString string
+	}
+
+	tests := map[string]TestCase{
+		"hiWorld": {
+			InputString: "hi world",
+		},
+		"featureStore": {
+			InputString: "featureform is a virtual feature store",
+		},
+	}
+
+	runTestCase := func(t *testing.T, test TestCase) {
+		inputByte := []byte(test.InputString)
+
+		// create a buffer to hold the compressed data
+		var compressed bytes.Buffer
+
+		// create a gzip writer that writes to the buffer
+		gz := gzip.NewWriter(&compressed)
+
+		// write the inputByte to the gzip writer
+		if _, err := gz.Write(inputByte); err != nil {
+			t.Fatal(err)
+		}
+
+		// close the gzip writer to flush any remaining data
+		if err := gz.Close(); err != nil {
+			t.Fatal(err)
+		}
+
+		// print the compressed data as a byte slice
+		compressedBytes := compressed.Bytes()
+
+		outputString, err := GZipMessageToString(compressedBytes)
+		if err != nil {
+			t.Fatalf("could not decompress message: %v", err)
+		}
+
+		if outputString != test.InputString {
+			t.Fatalf("strings do not match: expected '%s' but got '%s'", test.InputString, outputString)
+		}
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			runTestCase(t, test)
+		})
+	}
+}

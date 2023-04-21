b2b37ffef4b415294422756bf254fb6dcdfa716e
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-04-21 13:14:23 -0700

    linting

--- provider/scripts/spark/tests/test_files/scripts/test_emr_error.py
+++ provider/scripts/spark/tests/test_files/scripts/test_emr_error.py
@@ -3,11 +3,13 @@
 
 import sys
 
+
 def main():
     if len(sys.argv) == 2:
         raise Exception(f"ERROR: {sys.argv[1]}")
     else:
         raise Exception("There is an Error")
+
 
 if __name__ == "__main__":
     main()

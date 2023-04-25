e33ec591f194d091909ec932ba0f57c7ca530f3f
Erik Eppel <erik@featureform.com>
2023-04-25 15:10:16 -0700

    Source Dataframes in the Client (Local Mode) (#760)

--- client/src/featureform/__init__.py
+++ client/src/featureform/__init__.py
@@ -1,3 +1,21 @@
+# To properly display pending deprecation warnings for the ResourceClient
+# and ServingClient classes, we need to set the formatwarning function
+# to a custom function to avoid the default behavior of printing the
+# the absolute path to the file where the warning was raised. Additionally,
+# we need to set the default filter to 'default' to avoid the default
+# behavior of ignoring all warnings.
+# TODO: Remove this code once the ResourceClient and ServingClient classes
+# are deprecated.
+import warnings
+
+
+def custom_warning_formatter(message, category, filename, lineno, file=None, line=None):
+    return f"{category.__name__}: {message}\n"
+
+
+warnings.formatwarning = custom_warning_formatter
+warnings.simplefilter("default")
+
 from .register import *
 from .serving import ServingClient
 from .resources import (
@@ -7,9 +25,11 @@ from .resources import (
     GCPCredentials,
     SparkCredentials,
 )
+from .client import Client
 
 ServingClient = ServingClient
 ResourceClient = ResourceClient
+Client = Client
 
 
 # Executor Credentials

227ecd1033891f7a34dd35693f38a5847e298f4b
Ali Olfat <ali@featureform.com>
2023-04-28 21:52:22 -0700

    set a jobs limit

--- helpers/env.go
+++ helpers/env.go
@@ -1,6 +1,9 @@
 package helpers
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 // GetEnv Takes a environment variable key and returns the value if it exists.
 // Otherwise returns the fallback value provided
@@ -10,4 +13,31 @@ func GetEnv(key, fallback string) string {
 		return fallback
 	}
 	return value
+}
+
+func getEnvGeneric(key string, fallback interface{}, converter func(string) (interface{}, error)) interface{} {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return fallback
+	}
+
+	parsedValue, err := converter(value)
+	if err != nil {
+		return fallback
+	}
+	return parsedValue
+}
+
+func GetEnvInt(key string, fallback int) int {
+	return getEnvGeneric(key, fallback, func(val string) (interface{}, error) {
+		parsedValue, err := strconv.Atoi(val)
+		return parsedValue, err
+	}).(int)
+}
+
+func GetEnvBool(key string, fallback bool) bool {
+	return getEnvGeneric(key, fallback, func(val string) (interface{}, error) {
+		parsedValue, err := strconv.ParseBool(val)
+		return parsedValue, err
+	}).(bool)
 }

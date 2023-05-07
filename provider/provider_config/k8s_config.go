8033dcc725e99a1ca76d0dfea80e69453a7eff51
Ahmad Nazeri <nazeriahmed@gmail.com>
2023-05-06 18:14:22 -0700

    modified config to support different filestores

--- provider/provider_config/k8s_config.go
+++ provider/provider_config/k8s_config.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 
 	ss "github.com/featureform/helpers/string_set"
+	"github.com/mitchellh/mapstructure"
 )
 
 type K8sConfig struct {
 	ExecutorType   ExecutorType
 	ExecutorConfig interface{}
 	StoreType      FileStoreType
-	StoreConfig    AzureFileStoreConfig
+	StoreConfig    FileStoreConfig
 }
 
 func (k8s *K8sConfig) Serialize() ([]byte, error) {
@@ -27,11 +28,69 @@ func (k8s *K8sConfig) Deserialize(config SerializedConfig) error {
 	if err != nil {
 		return fmt.Errorf("deserialize k8s config: %w", err)
 	}
-	if k8s.ExecutorConfig == "" {
-		k8s.ExecutorConfig = ExecutorConfig{}
-	} else {
-		return k8s.executorConfigFromMap()
+	return nil
+}
+
+func (k8s *K8sConfig) UnmarshalJSON(data []byte) error {
+	type tempConfig struct {
+		ExecutorType   ExecutorType
+		ExecutorConfig map[string]interface{}
+		StoreType      FileStoreType
+		StoreConfig    map[string]interface{}
 	}
+
+	var temp tempConfig
+	err := json.Unmarshal(data, &temp)
+	if err != nil {
+		return fmt.Errorf("unmarshal: %w", err)
+	}
+
+	k8s.ExecutorType = temp.ExecutorType
+	k8s.StoreType = temp.StoreType
+
+	err = k8s.decodeExecutor(temp.ExecutorType, temp.ExecutorConfig)
+	if err != nil {
+		return fmt.Errorf("could not decode executor: %w", err)
+	}
+
+	err = k8s.decodeFileStore(temp.StoreType, temp.StoreConfig)
+	if err != nil {
+		return fmt.Errorf("could not decode filestore: %w", err)
+	}
+
+	return nil
+}
+
+func (k8s *K8sConfig) decodeExecutor(executorType ExecutorType, configMap map[string]interface{}) error {
+	serializedExecutor, err := json.Marshal(configMap)
+	if err != nil {
+		return fmt.Errorf("could not marshal executor config: %w", err)
+	}
+	excConfig := ExecutorConfig{}
+	err = excConfig.Deserialize(serializedExecutor)
+	if err != nil {
+		return fmt.Errorf("could not deserialize config into ExecutorConfig: %w", err)
+	}
+	k8s.ExecutorConfig = excConfig
+	return nil
+}
+
+func (k8s *K8sConfig) decodeFileStore(fileStoreType FileStoreType, configMap map[string]interface{}) error {
+	var fileStoreConfig FileStoreConfig
+	switch fileStoreType {
+	case Azure:
+		fileStoreConfig = &AzureFileStoreConfig{}
+	case S3:
+		fileStoreConfig = &S3FileStoreConfig{}
+	default:
+		return fmt.Errorf("the file store type '%s' is not supported ", fileStoreType)
+	}
+
+	err := mapstructure.Decode(configMap, fileStoreConfig)
+	if err != nil {
+		return fmt.Errorf("could not decode file store map: %w", err)
+	}
+	k8s.StoreConfig = fileStoreConfig
 	return nil
 }
 
@@ -39,7 +98,14 @@ func (k8s K8sConfig) MutableFields() ss.StringSet {
 	result := ss.StringSet{
 		"ExecutorConfig": true,
 	}
-	storeFields := k8s.StoreConfig.MutableFields()
+
+	var storeFields ss.StringSet
+	switch k8s.StoreType {
+	case Azure:
+		storeFields = k8s.StoreConfig.(*AzureFileStoreConfig).MutableFields()
+	case S3:
+		storeFields = k8s.StoreConfig.(*S3FileStoreConfig).MutableFields()
+	}
 
 	for field, val := range storeFields {
 		result["Store."+field] = val
@@ -59,13 +125,23 @@ func (a K8sConfig) DifferingFields(b K8sConfig) (ss.StringSet, error) {
 	if err != nil {
 		return result, err
 	}
-	storeFields, err := a.StoreConfig.DifferingFields(b.StoreConfig)
-	if err != nil {
-		return result, err
-	}
 
 	if len(executorFields) > 0 {
 		result["ExecutorConfig"] = true
+	}
+
+	var storeFields ss.StringSet
+	switch a.StoreType {
+	case Azure:
+		storeFields, err = a.StoreConfig.(*AzureFileStoreConfig).DifferingFields(*b.StoreConfig.(*AzureFileStoreConfig))
+	case S3:
+		storeFields, err = a.StoreConfig.(*S3FileStoreConfig).DifferingFields(*b.StoreConfig.(*S3FileStoreConfig))
+	default:
+		return nil, fmt.Errorf("unsupported store type: %v", a.StoreType)
+	}
+
+	if err != nil {
+		return result, err
 	}
 
 	for field, val := range storeFields {
@@ -79,21 +155,3 @@ const (
 	GoProc ExecutorType = "GO_PROCESS"
 	K8s    ExecutorType = "K8S"
 )
-
-func (config *K8sConfig) executorConfigFromMap() error {
-	cfgMap, ok := config.ExecutorConfig.(map[string]interface{})
-	if !ok {
-		return fmt.Errorf("could not get ExecutorConfig values")
-	}
-	serializedExecutor, err := json.Marshal(cfgMap)
-	if err != nil {
-		return fmt.Errorf("could not marshal executor config: %w", err)
-	}
-	excConfig := ExecutorConfig{}
-	err = excConfig.Deserialize(serializedExecutor)
-	if err != nil {
-		return fmt.Errorf("could not deserialize config into ExecutorConfig: %w", err)
-	}
-	config.ExecutorConfig = excConfig
-	return nil
-}

f7bf1a1e8e6348242d0676dc05fa8f343e274af8
Erik Eppel <erik@featureform.com>
2023-05-30 04:48:06 -0700

    Add support for Vector32 to Redis (#829)

--- provider/redis.go
+++ provider/redis.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 
 	pc "github.com/featureform/provider/provider_config"
 	pt "github.com/featureform/provider/provider_type"
-	"github.com/go-redis/redis/v8"
+
+	"github.com/redis/rueidis"
 )
 
 type redisTableKey struct {
@@ -21,7 +23,7 @@ func (t redisTableKey) String() string {
 }
 
 type redisOnlineStore struct {
-	client *redis.Client
+	client rueidis.Client
 	prefix string
 	BaseProvider
 }
@@ -34,21 +36,24 @@ func redisOnlineStoreFactory(serialized pc.SerializedConfig) (Provider, error) {
 	if redisConfig.Prefix == "" {
 		redisConfig.Prefix = "Featureform_table__"
 	}
-	return NewRedisOnlineStore(redisConfig), nil
+	return NewRedisOnlineStore(redisConfig)
 }
 
-func NewRedisOnlineStore(options *pc.RedisConfig) *redisOnlineStore {
-	redisOptions := &redis.Options{
-		Addr:     options.Addr,
-		Password: options.Password,
-		DB:       options.DB,
+func NewRedisOnlineStore(options *pc.RedisConfig) (*redisOnlineStore, error) {
+	redisOptions := rueidis.ClientOption{
+		InitAddress: []string{options.Addr},
+		Password:    options.Password,
+		SelectDB:    options.DB,
 	}
-	redisClient := redis.NewClient(redisOptions)
+	redisClient, err := rueidis.NewClient(redisOptions)
+	if err != nil {
+		return nil, err
+	}
 	return &redisOnlineStore{redisClient, options.Prefix, BaseProvider{
 		ProviderType:   pt.RedisOnline,
 		ProviderConfig: options.Serialized(),
 	},
-	}
+	}, nil
 }
 
 func (store *redisOnlineStore) AsOnlineStore() (OnlineStore, error) {
@@ -56,37 +61,49 @@ func (store *redisOnlineStore) AsOnlineStore() (OnlineStore, error) {
 }
 
 func (store *redisOnlineStore) Close() error {
-	// from the docs:
-	//
-	// "It is rare to Close a Client, as the Client is meant to be
-	// long-lived and shared between many goroutines.""
-	//
-	return store.client.Close()
+	store.client.Close()
+	return nil
 }
 
 func (store *redisOnlineStore) GetTable(feature, variant string) (OnlineStoreTable, error) {
 	key := redisTableKey{store.prefix, feature, variant}
-	vType, err := store.client.HGet(context.TODO(), fmt.Sprintf("%s__tables", store.prefix), key.String()).Result()
+	cmd := store.client.B().
+		Hget().
+		Key(fmt.Sprintf("%s__tables", store.prefix)).
+		Field(key.String()).
+		Build()
+	vType, err := store.client.Do(context.TODO(), cmd).ToString()
 	if err != nil {
 		return nil, &TableNotFound{feature, variant}
 	}
-	table := &redisOnlineTable{client: store.client, key: key, valueType: ValueType(vType)}
+	table := &redisOnlineTable{client: &store.client, key: key, valueType: ValueType(vType)}
 	return table, nil
 }
 
 func (store *redisOnlineStore) CreateTable(feature, variant string, valueType ValueType) (OnlineStoreTable, error) {
 	key := redisTableKey{store.prefix, feature, variant}
-	exists, err := store.client.HExists(context.TODO(), fmt.Sprintf("%s__tables", store.prefix), key.String()).Result()
+	cmd := store.client.B().
+		Hexists().
+		Key(fmt.Sprintf("%s__tables", store.prefix)).
+		Field(key.String()).
+		Build()
+	exists, err := store.client.Do(context.TODO(), cmd).AsBool()
 	if err != nil {
 		return nil, err
 	}
 	if exists {
 		return nil, &TableAlreadyExists{feature, variant}
 	}
-	if err := store.client.HSet(context.TODO(), fmt.Sprintf("%s__tables", store.prefix), key.String(), string(valueType)).Err(); err != nil {
-		return nil, err
+	cmd = store.client.B().
+		Hset().
+		Key(fmt.Sprintf("%s__tables", store.prefix)).
+		FieldValue().
+		FieldValue(key.String(), string(valueType)).
+		Build()
+	if resp := store.client.Do(context.TODO(), cmd); resp.Error() != nil {
+		return nil, resp.Error()
 	}
-	table := &redisOnlineTable{client: store.client, key: key, valueType: valueType}
+	table := &redisOnlineTable{client: &store.client, key: key, valueType: valueType}
 	return table, nil
 }
 
@@ -95,48 +112,98 @@ func (store *redisOnlineStore) DeleteTable(feature, variant string) error {
 }
 
 func (table redisOnlineTable) Set(entity string, value interface{}) error {
-	val := table.client.HSet(context.TODO(), table.key.String(), entity, value)
-	if val.Err() != nil {
-		return val.Err()
+	client := *table.client
+	switch v := value.(type) {
+	case nil:
+		value = "nil"
+	case string:
+		value = v
+	case int:
+		value = strconv.Itoa(v)
+	case int32:
+		value = strconv.FormatInt(int64(v), 10)
+	case int64:
+		value = strconv.FormatInt(v, 10)
+	case float32:
+		value = strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		value = strconv.FormatFloat(v, 'f', -1, 64)
+	case bool:
+		// The previous Redis client stored booleans as 1 or 0; to maintain backwards compatibility
+		// we do the same here, stringifying the value to satisfy the interface. See redis_test.go
+		// lines 59-66 for more reasons why we do this.
+		if v {
+			value = "1"
+		} else {
+			value = "0"
+		}
+	case time.Time:
+		value = v.Format(time.RFC3339)
+	case []float32:
+		value = rueidis.VectorString32(v)
+	default:
+		return fmt.Errorf("type %T of value %v is unsupported", value, value)
+	}
+	cmd := client.B().
+		Hset().
+		Key(table.key.String()).
+		FieldValue().
+		FieldValue(entity, value.(string)).
+		Build()
+	res := client.Do(context.TODO(), cmd)
+	if res.Error() != nil {
+		return res.Error()
 	}
 	return nil
 }
 
 func (table redisOnlineTable) Get(entity string) (interface{}, error) {
-	val := table.client.HGet(context.TODO(), table.key.String(), entity)
-	if val.Err() != nil {
+	client := *table.client
+	cmd := client.B().
+		Hget().
+		Key(table.key.String()).
+		Field(entity).
+		Build()
+	resp := client.Do(context.TODO(), cmd)
+	if resp.Error() != nil {
 		return nil, &EntityNotFound{entity}
 	}
 	var result interface{}
 	var err error
+	val, err := resp.ToString()
+	if err != nil {
+		return nil, err
+	}
 	switch table.valueType {
 	case NilType, String:
-		result, err = val.Result()
+		result, err = val, nil
 	case Int:
-		result, err = val.Int()
+		result, err = strconv.Atoi(val)
 	case Int32:
-		if res, err := val.Int(); err == nil {
-			result = int32(res)
+		if result, err = strconv.ParseInt(val, 10, 32); err == nil {
+			result = int32(result.(int64))
 		}
 	case Int64:
-		result, err = val.Int64()
+		result, err = strconv.ParseInt(val, 10, 64)
 	case Float32:
-		result, err = val.Float32()
-	case Float64:
-		result, err = val.Float64()
-	case Bool:
-		result, err = val.Bool()
-	case Timestamp, Datetime: // Maintains compatibility with previously create timestamp tables
-		var t time.Time
-		t, err = val.Time()
-		if err == nil {
-			result = t.Local()
+		if result, err = strconv.ParseFloat(val, 32); err == nil {
+			result = float32(result.(float64))
 		}
+	case Float64:
+		result, err = strconv.ParseFloat(val, 64)
+	case Bool:
+		result, err = strconv.ParseBool(val)
+	case Timestamp, Datetime: // Maintains compatibility with previously create timestamp tables
+		// maintains compatibility with go-redis implementation:
+		// https://github.com/redis/go-redis/blob/v8.11.5/command.go#L939
+		result, err = time.Parse(time.RFC3339Nano, val)
+	case Vector32:
+		result, err = rueidis.ToVector32(val), nil
 	default:
-		result, err = val.Result()
+		result, err = val, nil
 	}
 	if err != nil {
-		return nil, fmt.Errorf("could not cast value: %s to %s: %w", val.String(), table.valueType, err)
+		return nil, fmt.Errorf("could not cast value: %v to %s: %w", resp, table.valueType, err)
 	}
 	return result, nil
 }

3fbac5c7a78a9b05959fb39ac315dcebc96fd497
Carlos R. Feliz <carlos@featureform.com>
2023-07-06 10:29:55 -0700

    improve validation class design and resulting error messages

--- client/src/featureform/lib/validation.py
+++ client/src/featureform/lib/validation.py
@@ -5,32 +5,32 @@ from typeguard import typechecked
 
 @typechecked
 class Validation:
-    def __init__(self, value: any, validation_context: str = ""):
-        self.value = value
-        self.validation_context = validation_context + ": " if validation_context != "" else ""
+    def __init__(self, validation_context: str = ""):
+        self.validation_context = validation_context
         self.validation_error_messages = []
 
-    def validate(self, pred, validation_failure_message: str):
-        if pred(self.value):
+    def validate(self, condition, validation_failure_message: str):
+        if not condition:
             self.validation_error_messages.append(validation_failure_message)
 
-    def throw_or_pass(self):
+    def raise_or_pass(self):
         if self.validation_error_messages:
-            raise ValueError(self.validation_context + " ".join(self.validation_error_messages))
+            error_messages = ';'.join(self.validation_error_messages)
+            raise ValueError(f'{self.validation_context} Validation failed with errors: {error_messages}')
         else:
             pass
 
     @staticmethod
-    @typechecked
-    def validate_non_empty_list(value: list, validation_failure_message: str):
-        validation = Validation(value)
-        validation.validate(lambda ls: len(ls) == 0, validation_failure_message)
-        validation.throw_or_pass()
+    def validate_non_empty_list(ls: list, validation_failure_message: str):
+        validation = Validation()
+        # Read as "validate that the length of ls is greater than zero"
+        validation.validate(len(ls) > 0, validation_failure_message)
+        validation.raise_or_pass()
 
     @staticmethod
-    @typechecked
     def validate_name_variant(nvar: Tuple[str, str], validation_context: str):
-        validation = Validation(nvar, validation_context)
-        validation.validate(lambda v: v[0] == "", "Name must not be an empty string.")
-        validation.validate(lambda v: v[1] == "", "Variant must not be an empty string.")
-        validation.throw_or_pass()
+        validation = Validation(validation_context)
+        # Read as "validate that nvar[0] is not an empty string"
+        validation.validate(nvar[0] != "", "Name must not be an empty string.")
+        validation.validate(nvar[1] != "", "Variant must not be an empty string.")
+        validation.raise_or_pass()

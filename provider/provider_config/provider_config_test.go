37183b664d2e9c06ea4fe5d527fed4fe1e0010ff
anthonylasso <lasso_anthony@yahoo.com>
2023-08-08 17:19:34 -0500

    Update provider_config_test.go

--- provider/provider_config/provider_config_test.go
+++ provider/provider_config/provider_config_test.go
@@ -1,7 +1,10 @@
 package provider_config
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	pt "github.com/featureform/provider/provider_type"
@@ -38,4 +41,66 @@ func TestAllProviderTypesHasMapEntry(t *testing.T) {
 		errorMsg := fmt.Sprintf("This provider type (%s) is not present in the providerMap.", currentProvider)
 		assert.NotEmpty(t, foundEntry, errorMsg)
 	}
+}
+
+func getConnectionConfigs() ([]byte, error) {
+	connectionConfigs, err := os.ReadFile("../connection/connection_configs.json")
+	if err != nil {
+		return nil, err
+	}
+	return connectionConfigs, nil
+}
+
+func TestRedis(t *testing.T) {
+	connectionConfigs, err := getConnectionConfigs()
+	if err != nil {
+		println(err)
+		t.FailNow()
+	}
+
+	var jsonDict map[string]interface{}
+	if err = json.Unmarshal(connectionConfigs, &jsonDict); err != nil {
+		println(err)
+		t.FailNow()
+	}
+
+	config := jsonDict["RedisConfig"].(map[string]interface{})
+	addr := config["Addr"].(string)
+	password := config["Password"].(string)
+	db := int(config["DB"].(float64))
+
+	instance := RedisConfig{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	}
+
+	assert.NotNil(t, instance)
+}
+
+func TestGCS(t *testing.T) {
+	connectionConfigs, err := getConnectionConfigs()
+	if err != nil {
+		println(err)
+		t.FailNow()
+	}
+
+	var jsonDict map[string]interface{}
+	if err = json.Unmarshal(connectionConfigs, &jsonDict); err != nil {
+		println(err)
+		t.FailNow()
+	}
+
+	config := jsonDict["PineconeConfig"].(map[string]interface{})
+	projectId := strconv.FormatFloat(config["ProjectID"].(float64), 'f', -1, 64)
+	env := config["Environment"].(string)
+	apiKey := config["ApiKey"].(string)
+
+	instance := PineconeConfig{
+		ProjectID:   projectId,
+		Environment: env,
+		ApiKey:      apiKey,
+	}
+
+	assert.NotNil(t, instance)
 }

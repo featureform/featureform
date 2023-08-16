43d73ffcd5e0587fe935a4065a0d230488a39018
Ali Olfat <ali@featureform.com>
2023-08-16 15:39:41 -0700

    Metadata repository and tests

---
+++ client/src/featureform/metadata_repository.py
@@ -0,0 +1,386 @@
+import json
+from abc import ABC, abstractmethod
+from typing import List
+from typeguard import typechecked
+
+from . import SQLiteMetadata
+from .resources import (
+    Feature,
+    Resource,
+    FeatureVariant,
+    Source,
+    SourceVariant,
+    Provider,
+    TrainingSet,
+    Label,
+    Model,
+    Entity,
+    LabelVariant,
+    TrainingSetVariant,
+    User,
+    ResourceColumnMapping,
+    EmptyConfig,
+)
+
+
+@typechecked
+class MetadataRepository(ABC):
+    @abstractmethod
+    def create_resource(self, resource: Resource):
+        pass
+
+    @abstractmethod
+    def update_resource(self, resource):
+        pass
+
+    @abstractmethod
+    def get_feature_variant(self, name: str, variant: str) -> FeatureVariant:
+        pass
+
+    @abstractmethod
+    def get_features(self) -> List[Feature]:
+        pass
+
+    @abstractmethod
+    def get_label_variant(self, name: str, variant: str) -> LabelVariant:
+        pass
+
+    @abstractmethod
+    def get_labels(self) -> List[Label]:
+        pass
+
+    @abstractmethod
+    def get_source_variant(self, name: str, variant: str) -> SourceVariant:
+        pass
+
+    @abstractmethod
+    def get_sources(self) -> List[Source]:
+        pass
+
+    @abstractmethod
+    def get_training_set_variant(self, name: str, variant: str) -> TrainingSetVariant:
+        pass
+
+    @abstractmethod
+    def get_training_sets(self) -> List[TrainingSet]:
+        pass
+
+    @abstractmethod
+    def get_entity(self, name: str) -> Entity:
+        pass
+
+    @abstractmethod
+    def get_entities(self) -> List[Entity]:
+        pass
+
+    @abstractmethod
+    def get_model(self, name: str) -> Model:
+        pass
+
+    @abstractmethod
+    def get_models(self) -> List[Model]:
+        pass
+
+    @abstractmethod
+    def get_provider(self, name: str) -> Provider:
+        pass
+
+    @abstractmethod
+    def get_providers(self) -> List[Provider]:
+        pass
+
+    @abstractmethod
+    def get_user(self, name: str) -> User:
+        pass
+
+    @abstractmethod
+    def get_users(self) -> List[User]:
+        pass
+
+    @abstractmethod
+    def get_tags_for_resource(
+        self, name: str, variant: str, resource_type: str
+    ) -> List[str]:
+        pass
+
+
+class MetadataRepositoryLocalImpl(MetadataRepository):
+    def __init__(self, db: SQLiteMetadata):
+        self.db = db
+
+    def create_resource(self, resource: Resource):
+        # TODO actual db creation should happen here -- I shouldn't call back to the resource
+        resource._create_local(self.db)
+
+    def update_resource(self, resource):
+        raise NotImplementedError
+
+    def get_model(self, name: str) -> Model:
+        model_row = self.db.get_model(name, should_fetch_tags_properties=True)
+        return Model(
+            name=model_row["name"],
+            tags=json.loads(model_row["tags"]) if model_row["tags"] else [],
+            properties=json.loads(model_row["properties"])
+            if model_row["properties"]
+            else {},
+        )
+
+    def get_models(self) -> List[Model]:
+        model_rows = self.db.query_resource("models", should_fetch_tags_properties=True)
+        return [
+            Model(
+                name=row["name"],
+                tags=json.loads(row["tags"]) if row["tags"] else [],
+                properties=json.loads(row["properties"]) if row["properties"] else {},
+            )
+            for row in model_rows
+        ]
+
+    def get_feature_variant(self, name, variant) -> FeatureVariant:
+        result = self.db.get_feature_variant(name, variant)
+        return FeatureVariant(
+            name=result["name"],
+            variant=result["variant"],
+            source=(result["source_name"], result["source_variant"]),
+            value_type=result["data_type"],
+            is_embedding=bool(result["is_embedding"]),
+            dims=result["dimension"],
+            entity=result["entity"],
+            owner=result["owner"],
+            provider=result["provider"],
+            location=ResourceColumnMapping(
+                result["source_entity"],
+                result["source_value"],
+                result["source_timestamp"],
+            ),
+            description=result["description"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+        )
+
+    def get_features(self) -> List[Feature]:
+        feature_rows = self.db.query_resource("features")
+        features = []
+        for feature_row in feature_rows:
+            variants = [
+                r["variant"]
+                for r in self.db.query_resource(
+                    "feature_variant", "name", feature_row["name"]
+                )
+            ]
+            features.append(
+                Feature(
+                    name=feature_row["name"],
+                    default_variant=feature_row["default_variant"],
+                    variants=variants,
+                )
+            )
+
+        return features
+
+    def get_label_variant(self, name: str, variant: str) -> LabelVariant:
+        result = self.db.get_label_variant(name, variant)
+        return LabelVariant(
+            name=result["name"],
+            variant=result["variant"],
+            source=(result["source_name"], result["source_variant"]),
+            value_type=result["data_type"],
+            entity=result["entity"],
+            owner=result["owner"],
+            provider=result["provider"],
+            location=ResourceColumnMapping(
+                result["source_entity"],
+                result["source_value"],
+                result["source_timestamp"],
+            ),
+            description=result["description"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+        )
+
+    def get_labels(self) -> List[Label]:
+        label_rows = self.db.query_resource("labels")
+        labels = []
+        for label_row in label_rows:
+            variants = [
+                r["variant"]
+                for r in self.db.query_resource(
+                    "label_variant", "name", label_row["name"]
+                )
+            ]
+            labels.append(
+                Label(
+                    name=label_row["name"],
+                    default_variant=label_row["default_variant"],
+                    variants=variants,
+                )
+            )
+
+        return labels
+
+    def get_source_variant(self, name: str, variant: str) -> SourceVariant:
+        result = self.db.get_source_variant(name, variant)
+        return SourceVariant(
+            name=result["name"],
+            definition=result["definition"],  # double check this
+            variant=result["variant"],
+            owner=result["owner"],
+            provider=result["provider"],
+            description=result["description"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+            status=result["status"],
+        )
+
+    def get_sources(self) -> List[Source]:
+        source_rows = self.db.query_resource("sources")
+        sources = []
+        for source_row in source_rows:
+            variants = [
+                r["variant"]
+                for r in self.db.query_resource(
+                    "source_variant", "name", source_row["name"]
+                )
+            ]
+            sources.append(
+                Source(
+                    name=source_row["name"],
+                    default_variant=source_row["default_variant"],
+                    variants=variants,
+                )
+            )
+
+        return sources
+
+    def get_training_set_variant(self, name: str, variant: str) -> TrainingSetVariant:
+        result = self.db.get_training_set_variant(name, variant)
+        ts_feature_rows = self.db.get_training_set_features(name, variant)
+        feature_name_variants = [
+            (r["feature_name"], r["feature_variant"]) for r in ts_feature_rows
+        ]
+        return TrainingSetVariant(  # does local mode use provider for TS
+            name=result["name"],
+            variant=result["variant"],
+            owner=result["owner"],
+            label=(result["label_name"], result["label_variant"]),
+            features=feature_name_variants,
+            description=result["description"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+            status=result["status"],
+        )
+
+    def get_training_set(self, name: str) -> TrainingSet:
+        result = self.db.get_training_set(name, should_fetch_tags_properties=False)
+        return TrainingSet(
+            name=result["name"],
+            default_variant=result["default_variant"],
+            variants=[
+                r["variant"]
+                for r in self.db.query_resource(
+                    "training_set_variant", "name", result["name"]
+                )
+            ],
+        )
+
+    def get_training_sets(self) -> List[TrainingSet]:
+        training_set_rows = self.db.query_resource("training_sets")
+        training_sets = []
+        for ts_row in training_set_rows:
+            variants = [
+                r["variant"]
+                for r in self.db.query_resource(
+                    "training_set_variant", "name", ts_row["name"]
+                )
+            ]
+            training_sets.append(
+                TrainingSet(
+                    name=ts_row["name"],
+                    default_variant=ts_row["default_variant"],
+                    variants=variants,
+                )
+            )
+
+        return training_sets
+
+    def get_entity(self, name: str) -> Entity:
+        entity_row = self.db.get_entity(name, should_fetch_tags_properties=True)
+        return Entity(
+            name=entity_row["name"],
+            description=entity_row["description"],
+            tags=json.loads(entity_row["tags"]) if entity_row["tags"] else [],
+            properties=json.loads(entity_row["properties"]) if entity_row["properties"] else {},
+        )
+
+    def get_entities(self) -> List[Entity]:
+        entity_rows = self.db.query_resource(
+            "entities", should_fetch_tags_properties=True
+        )
+        return [
+            Entity(
+                name=row["name"],
+                description=row["description"],
+                tags=json.loads(row["tags"]) if row["tags"] else [],
+                properties=json.loads(row["properties"]) if row["properties"] else {},
+            )
+            for row in entity_rows
+        ]
+
+    def get_provider(self, name: str) -> Provider:
+        result = self.db.get_provider(name, should_fetch_tags_properties=True)
+        return Provider(
+            name=result["name"],
+            description=result["description"],
+            team=result["team"],
+            config=EmptyConfig(),  # TODO add proper deserializer for this
+            function="",  # look into this
+            status=result["status"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+        )
+
+    def get_providers(self) -> List[Provider]:
+        provider_rows = self.db.query_resource(
+            "providers", should_fetch_tags_properties=True
+        )
+        return [
+            Provider(
+                name=row["name"],
+                description=row["description"],
+                team=row["team"],
+                config=EmptyConfig(),  # TODO add proper deserializer for this
+                function="",  # look into this
+                status=row["status"],
+                tags=json.loads(row["tags"]) if row["tags"] else [],
+                properties=json.loads(row["properties"]) if row["properties"] else {},
+            )
+            for row in provider_rows
+        ]
+
+    def get_user(self, name: str) -> User:
+        result = self.db.get_user(name, should_fetch_tags_properties=True)
+        return User(
+            name=result["name"],
+            tags=json.loads(result["tags"]) if result["tags"] else [],
+            properties=json.loads(result["properties"]) if result["properties"] else {},
+        )
+
+    def get_users(self) -> List[User]:
+        user_rows = self.db.query_resource("users", should_fetch_tags_properties=True)
+        return [
+            User(
+                name=row["name"],
+                tags=json.loads(row["tags"]) if row["tags"] else [],
+                properties=json.loads(row["properties"]) if row["properties"] else {},
+            )
+            for row in user_rows
+        ]
+
+    def get_tags_for_resource(self, name, variant, resource_type) -> List[str]:
+        row_data = self.db.get_tags(
+            name=name, variant=variant, resource_type=resource_type
+        )
+        if len(row_data):
+            return row_data[0][0]
+        else:
+            return []
---
+++ client/tests/test_metadata_repository.py
@@ -0,0 +1,276 @@
+import os
+import shutil
+import stat
+from dataclasses import replace
+
+from client.src.featureform import SQLiteMetadata, FeatureVariant, ScalarType
+from client.src.featureform.metadata_repository import (
+    MetadataRepository,
+    MetadataRepositoryLocalImpl,
+)
+
+import pytest
+
+from client.src.featureform.resources import (
+    ResourceLocation,
+    Feature,
+    User,
+    LabelVariant,
+    Provider,
+    EmptyConfig,
+    SourceVariant,
+    PrimaryData,
+    SQLTable, Label, Model, Entity, TrainingSetVariant, TrainingSet,
+)
+
+test_user = User(name="test_user", tags=["tag1"], properties={"key1": "value1"})
+
+model = Model(name="model")
+
+entity = Entity(name="entity", description="fake description")
+
+provider = Provider(
+    name="provider",
+    description="fake description",
+    team="fake_team",
+    config=EmptyConfig(),
+    function="",
+    status="ready",  # look into this
+)
+
+resource_location = ResourceLocation(
+    entity="Dummy Entity", value="Dummy Value", timestamp="12345"
+)
+feature_variant = FeatureVariant(
+    name="feature",
+    source=("source", "variant1"),
+    value_type=ScalarType.FLOAT32.value,
+    entity="fake_entity",
+    owner="fake_owner",
+    provider="fake_provider",
+    location=resource_location,
+    description="fake description",
+    variant="variant1",
+    is_embedding=False,
+    dims=0,
+    tags=["tag1"],
+    properties={"key1": "value1"},
+)
+
+source_variant = SourceVariant(
+    name="source",
+    variant="variant1",
+    definition=PrimaryData(location=SQLTable(name="")),
+    owner="fake_owner",
+    provider="fake_provider",
+    description="fake description",
+    tags=[],
+    properties={},
+)
+
+label_variant = LabelVariant(
+    name="label",
+    source=("source", "variant1"),
+    value_type=ScalarType.FLOAT32.value,
+    entity="fake_entity",
+    owner="fake_owner",
+    provider="fake_provider",
+    location=resource_location,
+    description="fake description",
+    variant="variant1",
+    tags=["tag1"],
+    properties={"key1": "value1"},
+)
+
+training_set_variant = TrainingSetVariant(
+    name="training_set",
+    owner="fake_owner",
+    label=('label', 'variant1'),
+    features=[('feature', 'variant1')],
+    description="fake description",
+    variant="variant1",
+    tags=["tag1"],
+    properties={"key1": "value1"},
+    status="ready",
+)
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    yield
+    shutil.rmtree(".featureform", onerror=del_rw)
+
+
+def del_rw(action, name, exc):
+    if os.path.exists(name):
+        os.chmod(name, stat.S_IWRITE)
+        os.remove(name)
+
+
+@pytest.fixture(scope="function")
+def setup() -> MetadataRepository:
+    db = SQLiteMetadata()
+
+    yield MetadataRepositoryLocalImpl(db)
+
+
+def test_create_model(setup):
+    repo = setup
+    repo.create_resource(model)
+    assert repo.get_model("model") == model
+
+
+def test_get_models(setup):
+    repo = setup
+    repo.create_resource(model)
+    model2 = replace(model, name="fake_model2")
+    repo.create_resource(model2)
+
+    models = repo.get_models()
+    assert len(models) == 2  # there's one more for local mode
+    assert model in models
+    assert model2 in models
+
+
+def test_create_entity(setup):
+    repo = setup
+    repo.create_resource(entity)
+    assert repo.get_entity("entity") == entity
+
+
+def test_get_entities(setup):
+    repo = setup
+    repo.create_resource(entity)
+    entity2 = replace(entity, name="entity2")
+    repo.create_resource(entity2)
+
+    entities = repo.get_entities()
+    assert len(entities) == 2  # there's one more for local mode
+    assert entity in entities
+    assert entity2 in entities
+
+
+def test_create_provider(setup):
+    repo = setup
+    repo.create_resource(provider)
+    assert repo.get_provider("provider") == provider
+
+
+def test_get_providers(setup):
+    repo = setup
+    repo.create_resource(provider)
+    provider2 = replace(provider, name="fake_provider2")
+    repo.create_resource(provider2)
+
+    providers = repo.get_providers()
+    assert len(providers) == 2  # there's one more for local mode
+    assert provider in providers
+    assert provider2 in providers
+
+
+def test_create_source_variant(setup):
+    repo = setup
+    repo.create_resource(source_variant)
+    assert repo.get_source_variant("source", "variant1") == source_variant
+
+
+def test_get_sources(setup):
+    repo = setup
+    repo.create_resource(source_variant)
+    source2 = replace(source_variant, variant="variant2")
+    repo.create_resource(source2)
+
+    sources = repo.get_sources()
+    assert len(sources) == 1
+    assert sources[0].variants == ["variant1", "variant2"]
+
+
+def test_create_label_variant(setup):
+    repo = setup
+    repo.create_resource(label_variant)
+    assert repo.get_label_variant("label", "variant1") == label_variant
+
+
+def test_get_labels(setup):
+    repo = setup
+    repo.create_resource(label_variant)
+    label_variant2 = replace(label_variant, variant="variant2")
+    repo.create_resource(label_variant2)
+
+    labels = repo.get_labels()
+    assert len(labels) == 1
+    assert labels[0] == Label(
+        name="label",
+        default_variant="variant1",
+        variants=["variant1", "variant2"],
+    )
+
+
+def test_create_feature_variant(setup):
+    repo = setup
+    repo.create_resource(feature_variant)
+    assert repo.get_feature_variant("feature", "variant1") == feature_variant
+
+
+def test_get_features(setup):
+    repo = setup
+    repo.create_resource(feature_variant)
+    feature_variant2 = replace(feature_variant, variant="variant2")
+    repo.create_resource(feature_variant2)
+
+    features = repo.get_features()
+    assert len(features) == 1
+    assert features[0] == Feature(
+        name="feature",
+        default_variant="variant1",
+        variants=["variant1", "variant2"],
+    )
+
+
+def test_create_training_set_variant(setup):
+    repo = setup
+    repo.create_resource(label_variant)
+    repo.create_resource(feature_variant)
+    repo.create_resource(training_set_variant)
+    assert repo.get_training_set_variant("training_set", "variant1") == training_set_variant
+
+
+def test_get_training_sets(setup):
+    repo = setup
+    repo.create_resource(label_variant)
+    repo.create_resource(feature_variant)
+    repo.create_resource(training_set_variant)
+    training_set_variant2 = replace(training_set_variant, variant="variant2")
+    repo.create_resource(training_set_variant2)
+
+    training_sets = repo.get_training_sets()
+    assert len(training_sets) == 1
+    assert training_sets[0] == TrainingSet(
+        name="training_set",
+        default_variant="variant1",
+        variants=["variant1", "variant2"],
+    )
+
+
+def test_create_user(setup):
+    repo: MetadataRepository = setup
+    repo.create_resource(test_user)
+    retrieved_user = repo.get_user(test_user.name)
+    assert retrieved_user == test_user
+
+
+def test_get_users(setup):
+    repo: MetadataRepository = setup
+
+    user1 = test_user
+    user2 = replace(test_user, name="test_user2")
+    user3 = replace(test_user, name="test_user3")
+
+    users = [user1, user2, user3]
+
+    for user in users:
+        repo.create_resource(user)
+
+    retrieved_users = repo.get_users()
+    assert len(retrieved_users) == 3
+    assert retrieved_users == users

f8dc261a028fadb2c0c72ca2214b8e866a485320
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2023-08-16 21:39:49 -0700

    adding sslmode to tests for postgres

--- client/src/featureform/register.py
+++ client/src/featureform/register.py
@@ -1723,7 +1723,7 @@ class Registrar:
             postgres (OfflineSQLProvider): Provider
         """
         mock_config = PostgresConfig(
-            host="", port="", database="", user="", password=""
+            host="", port="", database="", user="", password="", sslmode="",
         )
         mock_provider = Provider(
             name=name, function="OFFLINE", description="", team="", config=mock_config

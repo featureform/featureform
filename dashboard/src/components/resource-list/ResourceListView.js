0b215d175bb1153172956129e9616df89bfba6df
anthonylasso <34227093+anthonylasso@users.noreply.github.com>
2023-09-26 12:32:58 -0500

    Link Update (#998)

--- dashboard/src/components/resource-list/ResourceListView.js
+++ dashboard/src/components/resource-list/ResourceListView.js
@@ -494,7 +494,8 @@ const NoDataMessage = ({ type }) => {
   const classes = useStyles();
 
   function redirect() {
-    window.location.href = 'https://docs.featureform.com/quickstart';
+    window.location.href =
+      'https://docs.featureform.com/getting-started/overview';
   }
   return (
     <Container>

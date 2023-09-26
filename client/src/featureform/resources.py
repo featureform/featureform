2aa83ac707f38fb75b62418374f7fbe118a8a352
sdreyer <sterling@featureform.com>
2023-09-26 12:36:46 -0700

    fixed test

--- client/src/featureform/resources.py
+++ client/src/featureform/resources.py
@@ -171,7 +171,7 @@ class AWSCredentials:
         if secret_key == "":
             raise Exception("'AWSCredentials' secret_key cannot be empty")
 
-        self.access_key = secret_key
+        self.access_key = access_key
         self.secret_key = secret_key
 
     def type(self):

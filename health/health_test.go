98797fb5b03c556e5425f0a6d224e930bf2c9369
Erik Eppel <erik@featureform.com>
2023-10-17 17:08:05 -0700

    implements health check for Spark and adds tests

--- health/health_test.go
+++ health/health_test.go
@@ -2,12 +2,15 @@ package health
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"os"
 	"testing"
 
+	fs "github.com/featureform/filestore"
 	"github.com/featureform/metadata"
 	pc "github.com/featureform/provider/provider_config"
 	pt "github.com/featureform/provider/provider_type"
@@ -29,7 +32,93 @@ func checkEnv(envVar string) string {
 	return value
 }
 
-func initProvider(providerType pt.Type) pc.SerializedConfig {
+func initSpark(t *testing.T, executorType pc.SparkExecutorType, storeType fs.FileStoreType) (pc.SerializedConfig, pc.SparkConfig) {
+	var executorConfig pc.SparkExecutorConfig
+
+	switch executorType {
+	case pc.SparkGeneric:
+		executorConfig = &pc.SparkGenericConfig{
+			Master:        os.Getenv("GENERIC_SPARK_MASTER"),
+			DeployMode:    os.Getenv("GENERIC_SPARK_DEPLOY_MODE"),
+			PythonVersion: os.Getenv("GENERIC_SPARK_PYTHON_VERSION"),
+		}
+	case pc.Databricks:
+		executorConfig = &pc.DatabricksConfig{
+			Host:    os.Getenv("DATABRICKS_HOST"),
+			Token:   os.Getenv("DATABRICKS_TOKEN"),
+			Cluster: os.Getenv("DATABRICKS_CLUSTER"),
+		}
+	case pc.EMR:
+		executorConfig = &pc.EMRConfig{
+			Credentials: pc.AWSCredentials{
+				AWSAccessKeyId: os.Getenv("AWS_ACCESS_KEY_ID"),
+				AWSSecretKey:   os.Getenv("AWS_SECRET_KEY"),
+			},
+			ClusterRegion: os.Getenv("AWS_EMR_CLUSTER_REGION"),
+			ClusterName:   os.Getenv("AWS_EMR_CLUSTER_ID"),
+		}
+	default:
+		t.Fatalf("Invalid executor type: %v", executorType)
+	}
+
+	var fileStoreConfig pc.SparkFileStoreConfig
+	switch storeType {
+	case fs.S3:
+		fileStoreConfig = &pc.S3FileStoreConfig{
+			Credentials: pc.AWSCredentials{
+				AWSAccessKeyId: os.Getenv("AWS_ACCESS_KEY_ID"),
+				AWSSecretKey:   os.Getenv("AWS_SECRET_KEY"),
+			},
+			BucketRegion: os.Getenv("S3_BUCKET_REGION"),
+			BucketPath:   os.Getenv("S3_BUCKET_PATH"),
+			Path:         os.Getenv(""),
+		}
+	case fs.GCS:
+		credsFile := os.Getenv("GCP_CREDENTIALS_FILE")
+		content, err := ioutil.ReadFile(credsFile)
+		if err != nil {
+			t.Errorf("Error when opening file: %v", err)
+		}
+		var creds map[string]interface{}
+		err = json.Unmarshal(content, &creds)
+		if err != nil {
+			t.Errorf("Error during Unmarshal() creds: %v", err)
+		}
+
+		fileStoreConfig = &pc.GCSFileStoreConfig{
+			BucketName: os.Getenv("GCS_BUCKET_NAME"),
+			BucketPath: "",
+			Credentials: pc.GCPCredentials{
+				ProjectId: os.Getenv("GCP_PROJECT_ID"),
+				JSON:      creds,
+			},
+		}
+	case fs.Azure:
+		fileStoreConfig = &pc.AzureFileStoreConfig{
+			AccountName:   os.Getenv("AZURE_ACCOUNT_NAME"),
+			AccountKey:    os.Getenv("AZURE_ACCOUNT_KEY"),
+			ContainerName: os.Getenv("AZURE_CONTAINER_NAME"),
+			Path:          os.Getenv("AZURE_CONTAINER_PATH"),
+		}
+	default:
+		t.Fatalf("Invalid store type: %v", storeType)
+	}
+
+	var sparkConfig = pc.SparkConfig{
+		ExecutorType:   executorType,
+		ExecutorConfig: executorConfig,
+		StoreType:      storeType,
+		StoreConfig:    fileStoreConfig,
+	}
+
+	serializedConfig, err := sparkConfig.Serialize()
+	if err != nil {
+		t.Fatalf("Cannot serialize Spark config with %s executor and %s files tore: %v", executorType, storeType, err)
+	}
+	return serializedConfig, sparkConfig
+}
+
+func initProvider(t *testing.T, providerType pt.Type, executorType pc.SparkExecutorType, storeType fs.FileStoreType) pc.SerializedConfig {
 	switch providerType {
 	case pt.RedisOnline:
 		port := checkEnv("REDIS_INSECURE_PORT")
@@ -52,6 +141,9 @@ func initProvider(providerType pt.Type) pc.SerializedConfig {
 			SSLMode:  "disable",
 		}
 		return postgresConfig.Serialize()
+	case pt.SparkOffline:
+		serializedConfig, _ := initSpark(t, executorType, storeType)
+		return serializedConfig
 	default:
 		panic(fmt.Sprintf("Unsupported provider type: %s", providerType))
 	}
@@ -73,7 +165,7 @@ func TestHealth_Check(t *testing.T) {
 	tests := []testMember{}
 
 	if *providerType == "redis" || *providerType == "" {
-		config := initProvider(pt.RedisOnline)
+		config := initProvider(t, pt.RedisOnline, "", "")
 		tests = append(tests, testMember{
 			providerDef: metadata.ProviderDef{
 				Name:             "redis",
@@ -87,13 +179,55 @@ func TestHealth_Check(t *testing.T) {
 	}
 
 	if *providerType == "postgres" || *providerType == "" {
-		config := initProvider(pt.PostgresOffline)
+		config := initProvider(t, pt.PostgresOffline, "", "")
 		tests = append(tests, testMember{
 			providerDef: metadata.ProviderDef{
 				Name:             "postgres",
 				Type:             string(pt.PostgresOffline),
 				SerializedConfig: config,
 				Software:         "postgres",
+				Tags:             metadata.Tags{},
+				Properties:       metadata.Properties{},
+			},
+		})
+	}
+
+	if *providerType == "spark-databricks-s3" || *providerType == "" {
+		config := initProvider(t, pt.SparkOffline, pc.Databricks, fs.S3)
+		tests = append(tests, testMember{
+			providerDef: metadata.ProviderDef{
+				Name:             "spark-databricks-s3",
+				Type:             string(pt.SparkOffline),
+				SerializedConfig: config,
+				Software:         "spark",
+				Tags:             metadata.Tags{},
+				Properties:       metadata.Properties{},
+			},
+		})
+	}
+
+	if *providerType == "spark-databricks-abs" || *providerType == "" {
+		config := initProvider(t, pt.SparkOffline, pc.Databricks, fs.Azure)
+		tests = append(tests, testMember{
+			providerDef: metadata.ProviderDef{
+				Name:             "spark-databricks-abs",
+				Type:             string(pt.SparkOffline),
+				SerializedConfig: config,
+				Software:         "spark",
+				Tags:             metadata.Tags{},
+				Properties:       metadata.Properties{},
+			},
+		})
+	}
+
+	if *providerType == "spark-emr-s3" || *providerType == "" {
+		config := initProvider(t, pt.SparkOffline, pc.EMR, fs.S3)
+		tests = append(tests, testMember{
+			providerDef: metadata.ProviderDef{
+				Name:             "spark-emr-s3",
+				Type:             string(pt.SparkOffline),
+				SerializedConfig: config,
+				Software:         "spark",
 				Tags:             metadata.Tags{},
 				Properties:       metadata.Properties{},
 			},

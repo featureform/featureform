0f8bc435159cea8fdc588ecde5e252695fbcb387
Erik Eppel <erik@featureform.com>
2023-10-18 21:27:19 -0700

    adds error message handling for common connection issues and introduces health to CreateProvider in API

--- health/health.go
+++ health/health.go
@@ -2,13 +2,18 @@ package health
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/featureform/metadata"
 	"github.com/featureform/provider"
 	pc "github.com/featureform/provider/provider_config"
 	pt "github.com/featureform/provider/provider_type"
 )
+
+const baseInitializationErrorMessage = "Featureform failed to connect to provider with the supplied credentials due to the following error:"
 
 type Health struct {
 	metadata *metadata.Client
@@ -30,9 +35,13 @@ func (h *Health) Check(providerName string) (bool, error) {
 	}
 	p, err := provider.Get(pt.Type(rec.Type()), pc.SerializedConfig(rec.SerializedConfig()))
 	if err != nil {
-		return false, err
+		return false, errors.New(h.handleError(err))
 	}
-	return p.Check()
+	isHealthy, err := p.Check()
+	if err != nil {
+		return false, errors.New(h.handleError(err))
+	}
+	return isHealthy, nil
 }
 
 func (h *Health) isSupportedProvider(t pt.Type) bool {
@@ -42,4 +51,32 @@ func (h *Health) isSupportedProvider(t pt.Type) bool {
 	default:
 		return false
 	}
+}
+
+func (h *Health) handleError(err error) string {
+	// GENERAL ERRORS
+	if strings.Contains(err.Error(), "no such host") {
+		return fmt.Sprintf("%s '%s' Check that the host name is correct and the server is running.", baseInitializationErrorMessage, err.Error())
+	}
+	if strings.Contains(err.Error(), "connection refused") {
+		return fmt.Sprintf("%s '%s' Check that the server is running and/or the port is correct.", baseInitializationErrorMessage, err.Error())
+	}
+	if strings.Contains(err.Error(), "operation timed out") {
+		return fmt.Sprintf("%s '%s' Check that the server is running and able to accept requests.", baseInitializationErrorMessage, err.Error())
+	}
+	// REDIS ERRORS
+	if strings.Contains(err.Error(), "WRONGPASS") {
+		return fmt.Sprintf("%s '%s' Check that the password is correct.", baseInitializationErrorMessage, err.Error())
+	}
+	if strings.Contains(err.Error(), "NOAUTH") {
+		return fmt.Sprintf("%s '%s' The server has been configured with the 'requirepass' directive, which requires a password for authentication.", baseInitializationErrorMessage, "NOAUTH HELLO")
+	}
+	// POSTGRES ERRORS
+	if match, matchErr := regexp.MatchString("pq: database .* does not exist", err.Error()); match && matchErr == nil {
+		return fmt.Sprintf("%s '%s' Check that the database name is correct.", baseInitializationErrorMessage, err.Error())
+	}
+	if match, matchErr := regexp.MatchString("pq: password authentication failed for user .*", err.Error()); match && matchErr == nil {
+		return fmt.Sprintf("%s '%s' Check that the password and username combination is correct.", baseInitializationErrorMessage, err.Error())
+	}
+	return err.Error()
 }

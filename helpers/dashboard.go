8bd969de2e04d266e605cc0aa76df0bab261cdde
Ali Olfat <ali@featureform.com>
2023-10-19 17:04:02 -0700

    Chore: Move libs from enterprise to OS (#1105)

---
+++ helpers/dashboard.go
@@ -0,0 +1,34 @@
+package helpers
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+var ResourceTypeToResourceURLMap = map[string]string{
+	"FEATURE_VARIANT":      "features",
+	"SOURCE_VARIANT":       "sources",
+	"LABEL_VARIANT":        "labels",
+	"TRAINING_SET_VARIANT": "training-sets",
+	// TODO add others here
+}
+
+func BuildDashboardUrl(host, resourceType, resourceName, resourceVariant string) (string, error) {
+	scheme := "https"
+	if strings.Contains(host, "localhost") {
+		scheme = "http"
+	}
+
+	// TODO fix url to include variant (if applicable)
+	resourceUrl, ok := ResourceTypeToResourceURLMap[resourceType]
+	if !ok {
+		return "", fmt.Errorf("resource type %s not found in map", resourceType)
+	}
+	dashboardUrl := &url.URL{
+		Scheme: scheme,
+		Host:   host,
+		Path:   fmt.Sprintf("/%s/%s", resourceUrl, resourceName),
+	}
+	return dashboardUrl.String(), nil
+}

4fcf426b023e98761da2b892fe1dd1ebf30d997a
Abhishek Manoj Pokale <98730215+Abhi132004@users.noreply.github.com>
2023-11-01 09:13:31 +0530

    Update qualityControl.py

--- dashboard/qualityControl.py
+++ dashboard/qualityControl.py
@@ -1,29 +1,39 @@
 import great_expectations as ge
 from great_expectations.datasource import PandasDatasource
 
-# Define the feature table (replace this with data source)
-data_context = ge.data_context.DataContext("/path/to/your/data_context_directory")
-data_asset_name = "your_feature_table"
-data_source = PandasDatasource("pandas_datasource")
-batch_kwargs = {"table": data_asset_name, "datasource": data_source}
+def run_data_quality_checks(data_context_dir, feature_table, column, value_set):
+    try:
+        # Create a Great Expectations data context
+        data_context = ge.data_context.DataContext(data_context_dir)
+        data_source = PandasDatasource("pandas_datasource")
+        
+        # Define batch parameters
+        batch_kwargs = {"table": feature_table, "datasource": data_source}
 
-# Create an expectation suite (You can define more detailed expectations)
-data_context.create_expectation_suite(data_asset_name, overwrite=True)
-# Add expectations to the suite
-data_context.expect(data_asset_name, "expect_column_values_to_be_in_set", column="your_column", value_set=[1, 2, 3])
+        # Create an expectation suite for the feature table
+        data_context.create_expectation_suite(feature_table, overwrite=True)
 
-# Validate the batch and store the result
-batch = data_context.get_batch(batch_kwargs, data_context.get_expectation_suite(data_asset_name))
-results = data_context.run_validation_operator(
-    "action_list_operator", [batch], run_id="batch_" + data_asset_name
-)
+        # Add expectations to the suite (customizable)
+        data_context.expect(
+            feature_table,
+            "expect_column_values_to_be_in_set",
+            column=column,
+            value_set=value_set
+        )
 
-# Check results and handle them as needed
-if results["success"]:
-    print("Data quality checks passed.")
-else:
-    print("Data quality checks failed.")
-    for result in results["results"]:
-        print(result)
+        # Validate the batch and store the result
+        batch = data_context.get_batch(batch_kwargs, data_context.get_expectation_suite(feature_table))
+        results = data_context.run_validation_operator("action_list_operator", [batch], run_id="batch_" + feature_table)
 
-# Now we can now display these results in your dashboard's data quality page
+        # Check results and handle them as needed
+        if results["success"]:
+            print(f"Data quality checks passed for {feature_table}.")
+        else:
+            print(f"Data quality checks failed for {feature_table}.")
+            for result in results["results"]:
+                print(result)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# Usage example for a specific feature
+run_data_quality_checks("/path/to/data_context", "your_feature_table", "your_column", [1, 2, 3])

f7c23af939e6bd96c8a481b57dc35a3bcedbe42a
ihkap11 <bpakhi97@gmail.com>
2023-11-08 19:35:08 +0000

    Update: cluster id in tests
    Why? Now that we have cluster_id validator we need to use right format for cluster id.
    
    file: client/tests/conftest.py
    
    change logs:
    def databricks():
        return DatabricksCredentials(
            username="a", password="b", cluster_id="abc-123def-ghijklmn"
        )
    
    AND
    
    def spark_provider(ff_registrar):
        databricks = DatabricksCredentials(
            username="a", password="b", cluster_id="abc-123def-ghijklmn"
        )
        azure_blob = AzureFileStoreConfig(
            account_name="", account_key="", container_name="", root_path=""
        )

--- client/tests/conftest.py
+++ client/tests/conftest.py
@@ -47,7 +47,9 @@ pytest_plugins = [
 
 @pytest.fixture(scope="module")
 def spark_provider(ff_registrar):
-    databricks = DatabricksCredentials(username="a", password="b", cluster_id="c_id")
+    databricks = DatabricksCredentials(
+        username="a", password="b", cluster_id="abc-123def-ghijklmn"
+    )
     azure_blob = AzureFileStoreConfig(
         account_name="", account_key="", container_name="", root_path=""
     )
@@ -274,7 +276,9 @@ def spark_executor_incorrect_deploy_mode():
 
 @pytest.fixture(scope="module")
 def databricks():
-    return DatabricksCredentials(username="a", password="b", cluster_id="c_id")
+    return DatabricksCredentials(
+        username="a", password="b", cluster_id="abc-123def-ghijklmn"
+    )
 
 
 @pytest.fixture(scope="module")

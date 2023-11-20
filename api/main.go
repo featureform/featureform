b36ec0cbbc2d01b2aedfd7a5dcadeab9a996ff88
sdreyer <sterling@featureform.com>
2023-11-19 23:51:03 -0800

    Moved resolver

--- api/main.go
+++ api/main.go
@@ -32,6 +32,41 @@ import (
 	"google.golang.org/grpc"
 )
 
+type exampleResolverBuilder struct{}
+
+func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &exampleResolver{
+		target: target,
+		cc:     cc,
+		addrsStore: map[string][]string{
+			"my_custom_service": {"10.0.2.230:8080", "10.0.3.90:8080", "10.0.3.99:8080"},
+		},
+	}
+	r.start()
+	return r, nil
+}
+
+func (*exampleResolverBuilder) Scheme() string { return "example" }
+
+// Define a custom resolver.
+type exampleResolver struct {
+	target     resolver.Target
+	cc         resolver.ClientConn
+	addrsStore map[string][]string
+}
+
+func (r *exampleResolver) start() {
+	addrStrs := r.addrsStore[r.target.Endpoint()]
+	addrs := make([]resolver.Address, len(addrStrs))
+	for i, s := range addrStrs {
+		addrs[i] = resolver.Address{Addr: s}
+	}
+	r.cc.UpdateState(resolver.State{Addresses: addrs})
+}
+
+func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
+func (*exampleResolver) Close()                                  {}
+
 type ApiServer struct {
 	Logger     *zap.SugaredLogger
 	address    string

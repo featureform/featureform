069226a7b1f2f59332ecadb97af4090587c5cb89
sdreyer <sterling@featureform.com>
2023-11-19 23:04:22 -0800

    Chage load balancing config

--- api/main.go
+++ api/main.go
@@ -790,7 +790,7 @@ func (serv *ApiServer) Serve() error {
 	}
 	opts := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingConfig": [{"round_robin":{}}]}`),
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
 	}
 	metaConn, err := grpc.Dial(serv.metadata.address, opts...)
 	if err != nil {

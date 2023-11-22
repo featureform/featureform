5e59a02d7286730d6cdd150a0f26e63dc0f71b9e
RiddhiBagadiaa <riddhi@featureform.com>
2023-11-22 13:50:43 -0800

    add behave tests

--- tests/end_to_end/features/serving.feature
+++ tests/end_to_end/features/serving.feature
@@ -5,10 +5,25 @@ Feature: Batch Serving
     Given Featureform is installed
     And The Snowflake container is running
     And Redis is running
-    When I register a Snowflake and with a "0884D0DD-468D-4C3A-8109-3C2BAAD72EF7" and "PUBLIC"
+    When I register Snowflake
     And I register redis
     And I register the tables from the database
-    And I define a User and register features
+    And I define a Snowflake User and register features
     And I create a "hosted" "insecure" client for "localhost:7878"
-    Then I serve batch features
+    Then I serve batch features <("table1_feature", "variant_628138"), ("table2_feature", "variant_628138"), ("table3_feature", "variant_628138"), ("table4_feature", "variant_628138")>
+    And I can get a list containing the entity name and a tuple with all the features
+
+
+  @wip
+  Scenario: Serving Batch Features (Spark)
+    Given Featureform is installed
+    And The Databricks env variables are available
+    And The S3 env variables are available
+    And Redis is running
+    When I register Spark with Databricks S3
+    And I register redis
+    And I register the files from the database
+    And I define a Spark User and register features
+    And I create a "hosted" "insecure" client for "localhost:7878"
+    Then I serve batch features <("transaction_feature", "variant_43636"), ("balance_feature", "variant_436536"), ("perc_feature", "variant_436536")>
     And I can get a list containing the entity name and a tuple with all the features
--- tests/end_to_end/features/steps/serving.py
+++ tests/end_to_end/features/steps/serving.py
@@ -1,14 +1,47 @@
-from behave import *
+import os
+import random
+
 import requests
+from behave import *
+from dotenv import load_dotenv
 
-    # And I apply "snowflake_config.py" file with a "hosted" "insecure" CLI
-    # And I create a "hosted" "insecure" client for "localhost:7878" (should be done)
-    # And I apply the "serve_batch_features.py" file
-    # Then I can iterate through the rows of the batch feature table
-    # And I can get a list containing the entity name and a tuple with all the features
 
-@when("I register a Snowflake and with a {database} and {schema}")
-def step_impl(context, database, schema):
+@given("The Databricks env variables are available")
+def step_impl(context):
+    featureform_location = os.path.dirname(os.path.dirname(""))
+    env_file_path = os.path.join(featureform_location, ".env")
+    load_dotenv(env_file_path)
+
+    context.databricks_host = os.getenv("DATABRICKS_HOST", None)
+    context.databrucks_token = os.getenv("DATABRICKS_TOKEN", None)
+    context.databricks_cluster_id = os.getenv("DATABRICKS_CLUSTER", None)
+
+    if context.databricks_host is None:
+        raise Exception("Databricks host is not set")
+    if context.databrucks_token is None:
+        raise Exception("Databricks token is not set")
+    if context.databricks_cluster_id is None:
+        raise Exception("Databricks cluster id is not set")
+
+
+@given("The S3 env variables are available")
+def step_impl(context):
+    context.s3_credentials = context.featureform.AWSCredentials(
+            access_key=os.getenv("AWS_ACCESS_KEY_ID", ""),
+            secret_key=os.getenv("AWS_SECRET_ACCESS_KEY", "")
+        ),
+
+    context.s3_bucket_name = os.getenv("S3_BUCKET_PATH", "")
+    context.s3_bucket_region = os.getenv("S3_BUCKET_REGION", "")
+
+    if context.s3_bucket_name == "":
+        raise Exception("S3 bucket name is not set")
+    if context.s3_bucket_region == "":
+        raise Exception("S3 bucket region is not set")
+
+
+@when("I register Snowflake")
+def step_impl(context):
     context.snowflake_name = "test_snowflake"
     try:
         context.snowflake = context.featureform.register_snowflake(
@@ -19,8 +52,37 @@ def step_impl(context, database, schema):
             password = context.snowflake_password,
             account = context.snowflake_account,
             organization = context.snowflake_organization,
-            database = database,
-            schema = schema,
+            database = "0884D0DD-468D-4C3A-8109-3C2BAAD72EF7",
+            schema = "PUBLIC",
+        )
+    except Exception as e:
+        context.exception = e
+
+@when("I register Spark with Databricks S3")
+def step_impl(context):
+    context.snowflake_name = "test_spark"
+    try:
+        databricks = context.featureform.DatabricksCredentials(
+            host=context.databricks_host,
+            token=context.databricks_token,
+            cluster_id=context.databricks_cluster_id,
+        )
+
+        s3 = context.featureform.register_s3(
+            name="s3",
+            credentials=context.s3_credentials,
+            bucket_name=context.s3_bucket_name,
+            path="",
+            bucket_region= context.s3_bucket_region,
+        )
+
+        # Offline store
+        context.spark = context.featureform.register_spark(
+            name="spark_provider",
+            description="A Spark deployment we created for the Featureform quickstart",
+            team="featureform-team",
+            executor=databricks,
+            filestore=s3
         )
     except Exception as e:
         context.exception = e
@@ -36,11 +98,35 @@ def step_impl(context):
     table="featureform_resource_feature__f4d42278-0889-44d7-9928-8aef22d23c16__6a6e8ff4-8a8f-4217-8096-bb360ae1e99b",
     )
 
-
-@then("I serve batch features")
+@when('I register the files from the database')
 def step_impl(context):
+    num = random.randint(0, 1000000)
+    context.transactions = context.spark.register_file(
+        name="transactions",
+        variant=f"variant_{num}",
+        description="A dataset of average transactions",
+        file_path="s3://featureform-internal-sandbox/featureform/Materialization/avg_trans/databricks-webinar/2023-11-14-13-54-43-521232/part-00000-tid-3732459543582589042-bf8734da-26f4-47ea-9920-fe7bd4b4bc8c-201-1-c000.snappy.parquet"
+    )
+
+    context.balance = context.spark.register_file(
+        name="balances",
+        variant=f"variant_{num}",
+        description="A dataset of balances",
+        file_path="s3://featureform-internal-sandbox/featureform/Materialization/balance/databricks-webinar/2023-11-14-13-54-44-141964/part-00000-tid-6907362395273500345-7899298e-e815-42d8-9e7e-98b7cab7e9be-205-1-c000.snappy.parquet"
+        )
+
+    context.perc = context.spark.register_file(
+        name="perc",
+        variant=f"variant_{num}",
+        description="A dataset of perc",
+        file_path="s3://featureform-internal-sandbox/featureform/Materialization/perc/databricks-webinar/2023-11-14-13-54-45-900722/part-00000-tid-8883202944540201246-2c2cc6c1-7d6b-49a3-86b6-36576fadf072-207-1-c000.snappy.parquet"
+    )
+
+
+@then("I serve batch features {features}")
+def step_impl(context, features):
     try:
-        context.iter = context.client.iterate_feature_set(("table1_feature", "variant_628138"), ("table2_feature", "variant_628138"), ("table3_feature", "variant_628138"), ("table4_feature", "variant_628138"))
+        context.iter = context.client.iterate_feature_set(features)
     except Exception as e:
         context.exception = e
         return
@@ -57,13 +143,13 @@ def step_impl(context):
     context.exception = None
 
 
-@when("I define a User and register features")
+@when("I define a Snowflake User and register features")
 def step_impl(context):
-    class User:
+    class SnowflakeUser:
         context.boolean_feature = context.featureform.Feature(
             context.boolean_table[['entity',' value', 'ts']],
             variant="batch_serving_test_15",
-            type=context.featureform.Float32,
+            type=context.featureform.Boolean,
             inference_store=context.redis,
         )
         context.numerical_feature = context.featureform.Feature(
@@ -79,5 +165,28 @@ def step_impl(context):
             inference_store=context.redis,
         )
 
-    context.featureform.entity(User)
+    context.featureform.entity(SnowflakeUser)
 
+@when("I define a Spark User and register features")
+def step_impl(context):
+    class SparkUser:
+        context.transaction_feature = ff.Feature(
+            transactions[['entity',' value', 'ts']],
+            variant=f"variant_{num}",
+            type=ff.Float32,
+            inference_store=redis,
+        )
+        context.balance_feature = ff.Feature(
+            balance[['entity',' value', 'ts']],
+            variant=f"variant_{num}",
+            type=ff.Float32,
+            inference_store=redis,
+        )
+        context.perc_feature = ff.Feature(
+            perc[['entity',' value', 'ts']],
+            variant=f"variant_{num}",
+            type=ff.String,
+            inference_store=redis,
+        )
+
+    context.featureform.entity(SparkUser)

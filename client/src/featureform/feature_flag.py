c8798759ac8ecad7e5e8b5c79a225ad24ef037f2
Ali Olfat <ali@featureform.com>
2023-11-28 09:45:37 -0800

    handle none case

--- client/src/featureform/feature_flag.py
+++ client/src/featureform/feature_flag.py
@@ -3,11 +3,11 @@ import os
 
 def is_enabled(feature_name, default=False):
     """
-    Returns True if the feature is enabled, currently using an environment variable
+    Returns True if the feature is enabled, currently using an environment variable.
+    Interprets the environment variable as a boolean if it's a valid representation, else defaults to the specified default.
     """
-    value = os.getenv(feature_name).lower()
-    if value == "true":
-        return True
-    elif value == "false":
-        return False
+    value = os.getenv(feature_name)
+    if value is not None:
+        return value.lower() == "true"
     return default
+

cdde2cea99b6b59a0d065ebf408a81edc327116d
sdreyer <sterling@featureform.com>
2023-11-30 16:55:42 -0800

    fixed script

--- .github/helpers/get_latest_release.sh
+++ .github/helpers/get_latest_release.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-prefix="release-"  # Specify the prefix you want to filter
+prefix="release/"  # Replace 'your-prefix' with the desired prefix
 
 # Check if the prefix is provided as an argument
 if [ $# -eq 1 ]; then
   prefix="$1"
 fi
 
-# List branches with the specified prefix
-branches=$(git for-each-ref --format="%(refname:short)" refs/heads/"$prefix"*)
+branches=()
 
-# Sort the branches numerically
-sorted_branches=$(echo "$branches" | grep -oE "$prefix[0-9.]+" | sort -t/ -k2n)
+# Iterate through all branches and filter by the specified prefix
+for branch in $(git for-each-ref --format="%(refname:short)" refs/heads/); do
+  if [[ "$branch" == "$prefix"* ]]; then
+    branches+=("$branch")
+  fi
+done
 
-# Get the most recent branch (the last one in the sorted list)
-most_recent_branch=$(echo "$sorted_branches" | tail -n 1)
+# Sort the list in descending order
+printf "%s\n" "${branches[@]}" | sort -t. -k1,1 -k2,2nr | head -n 1
 
-# Print the most recent branch
-echo "$most_recent_branch"

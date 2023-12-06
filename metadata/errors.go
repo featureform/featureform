26114f8b209c8264a573464d470db919172fab70
Ali Olfat <ali@featureform.com>
2023-12-06 10:38:26 -0800

    Task: Validate existing variant hasn't changed on key fields (#1196)

--- metadata/errors.go
+++ metadata/errors.go
@@ -1,6 +1,10 @@
 package metadata
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 type KeyNotFoundError struct {
 	key string
@@ -8,4 +12,56 @@ type KeyNotFoundError struct {
 
 func (e KeyNotFoundError) Error() string {
 	return fmt.Sprintf("Key Not Found: %s", e.key)
+}
+
+type ResourceExistsError struct {
+	ID ResourceID
+}
+
+func (err *ResourceExistsError) Error() string {
+	id := err.ID
+	name, variant, t := id.Name, id.Variant, id.Type
+	errMsg := fmt.Sprintf("%s Exists.\nName: %s", t, name)
+	if variant != "" {
+		errMsg += "\nVariant: " + variant
+	}
+	return errMsg
+}
+
+func (err *ResourceExistsError) GRPCStatus() *status.Status {
+	return status.New(codes.AlreadyExists, err.Error())
+}
+
+type ResourceNotFoundError struct {
+	ID ResourceID
+	E  error
+}
+
+func (err *ResourceNotFoundError) Error() string {
+	id := err.ID
+	var errMsg string
+	if err.E != nil {
+		errMsg = fmt.Sprintf("resource not found. %s err: %v", id.String(), err.E)
+	} else {
+		errMsg = fmt.Sprintf("resource not found. %s", id.String())
+	}
+	return errMsg
+}
+
+func (err *ResourceNotFoundError) GRPCStatus() *status.Status {
+	return status.New(codes.NotFound, err.Error())
+}
+
+type ResourceChangedError struct {
+	ResourceID
+}
+
+func (err *ResourceChangedError) Error() string {
+	id := err.ResourceID
+	errMsg := fmt.Sprintf("resource %s has changed. Please use a new variant.", id.String())
+	return errMsg
+}
+
+func (err *ResourceChangedError) GRPCStatus() *status.Status {
+	return status.New(codes.Internal, err.Error())
 }

9ce618c28f13bea78a6ed6b32eebc79ba4200baf
Ali Olfat <ali@featureform.com>
2023-12-13 18:02:51 -0800

    Create script and job to clean up databricks jobs

---
+++ scripts/databricks_job_cleanup.py
@@ -0,0 +1,52 @@
+import os
+import requests
+
+# Configuration
+DATABRICKS_HOST = os.getenv("DATABRICKS_HOST")
+DATABRICKS_TOKEN = os.getenv("DATABRICKS_TOKEN")
+HEADERS = {"Authorization": f"Bearer {DATABRICKS_TOKEN}"}
+
+
+def delete_databricks_jobs():
+    """List all jobs and trigger their deletion."""
+    url = f"{DATABRICKS_HOST}/api/2.1/jobs/list"
+    response = requests.get(url, headers=HEADERS)
+
+    if response.status_code == 200:
+        jobs = response.json().get("jobs", [])
+        delete_jobs(jobs)
+
+        while response.json().get("has_more", False):
+            next_token = response.json().get("next_page_token")
+            response = requests.get(
+                url, headers=HEADERS, params={"next_page_token": next_token}
+            )
+            delete_jobs(response.json().get("jobs", []))
+    else:
+        raise Exception(f"Error listing jobs: {response.content}")
+
+
+def delete_jobs(jobs):
+    """Delete a list of jobs."""
+    for job in jobs:
+        delete_job(job["job_id"])
+
+
+def delete_job(job_id):
+    """Delete a job by job ID."""
+    print(f"Deleting job ID: {job_id}")
+    url = f"{DATABRICKS_HOST}/api/2.1/jobs/delete"
+    response = requests.post(url, headers=HEADERS, json={"job_id": job_id})
+
+    if response.status_code == 200:
+        print(f"Deleted job ID: {job_id}")
+    else:
+        raise Exception(f"Error deleting job {job_id}: {response.content}")
+
+
+def main():
+    delete_databricks_jobs()
+
+
+if __name__ == "__main__":
+    main()

0f36f870d5cf3ab195ecd070f292c8ca418cffa7
Ali Olfat <ali@featureform.com>
2024-01-03 11:22:24 -0800

    lint?

--- client/tests/register_test.py
+++ client/tests/register_test.py
@@ -278,14 +278,17 @@ def run_before_and_after_tests(tmpdir):
         ("SELECT * FROM {{name.variant }}", True),
         ("SELECT * FROM     \n {{ name.variant }}", True),
         ("SELECT * FROM     \n {{name.variant}}", True),
-        ("SELECT * FROM {{data_lsbrqxjvevhqeat_transactions_short_csv.aexftowrixasmlv_2023-12-20T15-16-44}}", True),
+        (
+            "SELECT * FROM {{data_lsbrqxjvevhqeat_transactions_short_csv.aexftowrixasmlv_2023-12-20T15-16-44}}",
+            True,
+        ),
         ("SELECT * FROM     \n {{name . variant}}", False),
         (
             """
-            SELECT *
-            FROM {{ name.variant2 }}
-            WHERE x >= 5.
-            """,
+                SELECT *
+                FROM {{ name.variant2 }}
+                WHERE x >= 5.
+                """,
             True,
         ),
         (

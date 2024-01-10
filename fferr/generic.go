559b05083bae177666f1c8c2e7be998e948bc463
Erik Eppel <erik@featureform.com>
2024-01-09 16:54:16 -0800

    implements fferr package w/ initial error implementations

---
+++ fferr/generic.go
@@ -0,0 +1,47 @@
+package fferr
+
+import (
+	"fmt"
+
+	"github.com/rotisserie/eris"
+)
+
+const ENABLE_STACK_TRACE = true
+
+func NewGenericError(err error) GenericError {
+	msg := err.Error()
+	return GenericError{
+		msg:     msg,
+		err:     eris.New(msg),
+		details: map[string]string{},
+	}
+}
+
+type GenericError struct {
+	msg     string
+	err     error
+	details map[string]string
+}
+
+func (e *GenericError) Error() string {
+	msg := e.msg
+	if len(e.details) > 0 {
+		msg = fmt.Sprintf("%s\n", msg)
+	}
+	for key, value := range e.details {
+		msg = fmt.Sprintf("%s%s: %s\n", msg, key, value)
+	}
+	return msg
+}
+
+func (e *GenericError) Stack() JSONStackTrace {
+	return eris.ToJSON(e.err, ENABLE_STACK_TRACE)
+}
+
+func (e *GenericError) Details() map[string]string {
+	return e.details
+}
+
+func (e *GenericError) AddDetail(key, value string) {
+	e.details[key] = value
+}

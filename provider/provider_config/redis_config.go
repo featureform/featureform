436bbc087f3ccdf050faf0af10a53f747181a00b
sdreyer <sterling@featureform.com>
2024-01-17 16:17:29 -0800

    Adds Error handling for provider config deserialization (#1241)
    Co-authored-by: Erik Eppel <erik@featureform.com>
    Co-authored-by: RiddhiBagadiaa <riddhi@featureform.com>
    Co-authored-by: anthonylasso <lasso_anthony@yahoo.com>
    Co-authored-by: Ali Olfat <ali@featureform.com>
    Co-authored-by: anthonylasso <34227093+anthonylasso@users.noreply.github.com>

--- provider/provider_config/redis_config.go
+++ provider/provider_config/redis_config.go
@@ -2,6 +2,7 @@ package provider_config
 
 import (
 	"encoding/json"
+	"github.com/featureform/fferr"
 
 	ss "github.com/featureform/helpers/string_set"
 )
@@ -24,7 +25,7 @@ func (r RedisConfig) Serialized() SerializedConfig {
 func (r *RedisConfig) Deserialize(config SerializedConfig) error {
 	err := json.Unmarshal(config, r)
 	if err != nil {
-		return err
+		return fferr.NewInternalError(err)
 	}
 	return nil
 }

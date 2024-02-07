4450c2d1b58bc99b642b87708302e633e5097ddf
anthonylasso <lasso_anthony@yahoo.com>
2024-02-06 22:09:01 -0600

    move to own route

--- dashboard/pages/[type]/index.js
+++ dashboard/pages/[type]/index.js
@@ -2,22 +2,12 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import ResourcesAPI from '../../src/api/resources/Resources';
 import DataPage from '../../src/components/datapage/DataPage';
-import JobPage from '../../src/components/jobs';
 
 const DataPageRoute = () => {
   const router = useRouter();
   const { type } = router.query;
   const apiHandle = new ResourcesAPI();
-
-  // todox: need better routing. separate static pages from the dynamic pages?
-  let body;
-  if (type === 'jobs') {
-    body = <JobPage />;
-  } else {
-    body = <DataPage api={apiHandle} type={type} />;
-  }
-
-  return body;
+  return <DataPage api={apiHandle} type={type} />;
 };
 
 export default DataPageRoute;

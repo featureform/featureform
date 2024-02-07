e7671b39664c8d944b118ce34b2515cd0da22422
anthonylasso <lasso_anthony@yahoo.com>
2024-02-07 16:05:01 -0600

    remove todos

--- dashboard/src/components/tasks/index.js
+++ dashboard/src/components/tasks/index.js
@@ -2,22 +2,6 @@ import React from 'react';
 import TableDataWrapper from './tableDataWrapper';
 
 export default function TaskPage() {
-  /*
-    todox:
-    
-    need to create title component
-
-    wrap presentation component in data provider
-
-    all / active / complete options
-
-    need a table component with the various columns
-
-    popover view with metadata details (shouldn't require a load from server)
-
-
-
-    */
   return (
     <>
       <h1>Task Runs</h1>

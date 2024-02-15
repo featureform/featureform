f00a8b1a38a75d4c9fe744a68b262e8d19bb3023
RiddhiBagadiaa <riddhi@featureform.com>
2024-02-15 14:23:59 -0800

    prelinary setstatus test

--- scheduling/taskmanager_test.go
+++ scheduling/taskmanager_test.go
@@ -1,6 +1,7 @@
 package scheduling
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -534,3 +535,140 @@ func TestGetRunAll(t *testing.T) {
 //		})
 //	}
 //}
+
+func TestSetStatusByRunID(t *testing.T) {
+	type taskInfo struct {
+		Name   string
+		Type   TaskType
+		Target TaskTarget
+	}
+	type runInfo struct {
+		Name    string
+		TaskID  TaskID
+		Trigger Trigger
+	}
+	type TestCase struct {
+		Name        string
+		Tasks       []taskInfo
+		Runs        []runInfo
+		ForTask     TaskID
+		ForRun      TaskRunID
+		SetStatus   Status
+		SetError    error
+		shouldError bool
+	}
+	tests := []TestCase{
+		{
+			"Single",
+			[]taskInfo{{"name", ResourceCreation, NameVariant{"name", "variant"}}},
+			[]runInfo{{"name", 1, OneOffTrigger{"name"}}},
+			1,
+			1,
+			Success,
+			nil,
+			false,
+		},
+		{
+			"Multiple",
+			[]taskInfo{{"name", ResourceCreation, NameVariant{"name", "variant"}}},
+			[]runInfo{
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 1, OneOffTrigger{"name"}},
+			},
+			1,
+			2,
+			Pending,
+			nil,
+			false,
+		},
+		{
+			"WrongStatus",
+			[]taskInfo{{"name", ResourceCreation, NameVariant{"name", "variant"}}},
+			[]runInfo{
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 1, OneOffTrigger{"name"}},
+			},
+			1,
+			2,
+			"Random",
+			nil,
+			true,
+		},
+		{
+			"MultipleTasks",
+			[]taskInfo{
+				{"name", ResourceCreation, NameVariant{"name", "variant"}},
+				{"name", ResourceCreation, NameVariant{"name", "variant"}},
+			},
+			[]runInfo{
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 1, OneOffTrigger{"name"}},
+				{"name", 2, OneOffTrigger{"name"}},
+				{"name", 2, OneOffTrigger{"name"}},
+			},
+			2,
+			1,
+			Failed,
+			fmt.Errorf("Failed to create task"),
+			false,
+		},
+		// {
+		// 	"Fetch NonExistent",
+		// 	[]taskInfo{{"name", ResourceCreation, NameVariant{"name", "variant"}}},
+		// 	[]runInfo{{"name", 1, OneOffTrigger{"name"}}},
+		// 	1,
+		// 	2,
+		// 	true,
+		// },
+	}
+
+	fn := func(t *testing.T, test TestCase) {
+		storage := MemoryStorageProvider{}
+		manager := NewTaskManager(&storage)
+		for _, task := range test.Tasks {
+			_, err := manager.CreateTask(task.Name, task.Type, task.Target)
+			if err != nil && !test.shouldError {
+				t.Fatalf("failed to create task: %v", err)
+			}
+		}
+
+		var runDefs []TaskRunMetadata
+		for _, run := range test.Runs {
+			runDef, err := manager.CreateTaskRun(run.Name, run.TaskID, run.Trigger)
+			if err != nil && !test.shouldError {
+				t.Fatalf("failed to create task run: %v", err)
+			}
+			runDefs = append(runDefs, runDef)
+		}
+
+		err := manager.SetRunStatus(test.ForRun, test.ForTask, test.SetStatus, test.SetError)
+		if err != nil && test.shouldError {
+			return
+		} else if err != nil && !test.shouldError {
+			t.Fatalf("failed to set status correctly: %v", err)
+		} else if err == nil && test.shouldError {
+			t.Fatalf("expected error but did not receive one")
+		}
+
+		recvRun, err := manager.GetRunByID(test.ForTask, test.ForRun)
+		if err != nil {
+			t.Fatalf("failed to get run by ID %d: %v", test.ForTask, err)
+		}
+		recvStatus := recvRun.Status
+
+		if recvStatus != test.SetStatus {
+
+			t.Fatalf("Expcted %v, got: %v", test.SetStatus, recvStatus)
+
+		}
+
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			fn(t, tt)
+		})
+	}
+}

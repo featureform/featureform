0696db5101774e416a40d0731ba2703812c26e9b
sdreyer <sterling@featureform.com>
2024-02-18 14:54:19 -0800

    Fixed Serving issue

--- client/src/featureform/serving.py
+++ client/src/featureform/serving.py
@@ -503,7 +503,9 @@ class TrainingSetSplitIterator:
 
             if next_row.iterator_done:
                 self.complete = True
-                break
+                if batch_features is None:
+                    raise StopIteration
+                return batch_features, batch_label
 
             if batch_features is None:
                 row = Row(next_row.row)
@@ -516,8 +518,6 @@ class TrainingSetSplitIterator:
                 batch_label = np.append(batch_label, Row(next_row.row).label(), axis=0)
             i += 1
 
-        if self.complete:
-            raise StopIteration
 
         return batch_features, batch_label
 

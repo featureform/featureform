238608090c8b73004a9453a6b7cf48c4717a500a
anthonylasso <lasso_anthony@yahoo.com>
2024-02-23 11:07:11 -0600

    styling

---
+++ dashboard/src/components/triggers/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  inputRow: {
+    paddingBottom: 10,
+  },
+  triggerBox: {
+    height: 750,
+    width: 700,
+  },
+}));
+
+export { useStyles };

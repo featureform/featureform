ed16a22d53381e82ef2076df210708e7f6905a02
Ali Olfat <ali@featureform.com>
2024-02-28 14:06:28 -0800

    clean up client

--- client/src/featureform/grpc_client.py
+++ client/src/featureform/grpc_client.py
@@ -1,18 +1,54 @@
 import contextlib
 import logging
 import sys
+from dataclasses import dataclass, field
+from typing import Dict, Optional
 
 import grpc
-
 from google.rpc import error_details_pb2, status_pb2
-from google.protobuf import any_pb2
 
 logging.basicConfig(level=logging.DEBUG, filename="grpc_debug.log")
 
 
+@dataclass
+class FFGrpcErrorDetails:
+    """
+    FFGrpcErrorDetails is a dataclass that represents the details of an error returned by the Featureform gRPC server.
+    """
+
+    code: int
+    message: str
+    reason: str
+    metadata: Dict[str, str] = field(default_factory=dict)
+
+    @staticmethod
+    def from_grpc_error(e: grpc.RpcError) -> Optional["FFGrpcErrorDetails"]:
+        """
+        from_grpc_error is a static method that creates a FFGrpcErrorDetails object from a gRPC error.
+        """
+        status_proto = status_pb2.Status()
+        status_proto.MergeFromString(e.trailing_metadata()[0].value)
+
+        for detail in status_proto.details:
+            # should only be one detail
+            if detail.Is(error_details_pb2.ErrorInfo.DESCRIPTOR):
+                error_info = error_details_pb2.ErrorInfo()
+                detail.Unpack(error_info)
+
+                return FFGrpcErrorDetails(
+                    code=status_proto.code,
+                    message=status_proto.message,
+                    reason=error_info.reason,
+                    metadata=dict(error_info.metadata),
+                )
+            else:
+                logging.debug("Unknown error detail type: %s", detail)
+                return None
+
+
 class GrpcClient:
-    def __init__(self, wrapped, debug=False, insecure=False, host=None):
-        self.wrapped = wrapped
+    def __init__(self, grpc_stub, debug=False, insecure=False, host=None):
+        self._grpc_stub = grpc_stub
         self._insecure = insecure
         self._host = host
         self.debug = debug
@@ -38,7 +74,7 @@ class GrpcClient:
         )
 
     def __getattr__(self, name):
-        attr = getattr(self.wrapped, name)
+        attr = getattr(self._grpc_stub, name)
 
         def wrapper(*args, **kwargs):
             try:
@@ -53,35 +89,20 @@ class GrpcClient:
 
         return wrapper
 
-    def handle_grpc_error(self, e):
-        traceback_limit = 0
-        initial_ex = None
+    def handle_grpc_error(self, e: grpc.RpcError) -> None:
+        ex = e if self.debug else None
 
-        if self.debug:
-            initial_ex = e
-            traceback_limit = None
-
-        with limited_traceback(traceback_limit):
-            # get details from the error
+        with _limited_traceback(None if self.debug else 0):
             if e.code() in self.expected_codes:
-                status_proto = status_pb2.Status()
-                status_proto.MergeFromString(e.trailing_metadata()[0].value)
-
-                for detail in status_proto.details:
-                    any_msg = any_pb2.Any()
-                    any_msg.CopyFrom(detail)
-                    if any_msg.Is(error_details_pb2.ErrorInfo.DESCRIPTOR):
-                        error_info = error_details_pb2.ErrorInfo()
-                        any_msg.Unpack(error_info)
-                        reason = error_info.reason
-                        metadata = error_info.metadata
-
-                        logging.debug(
-                            f"Error: {status_proto.message}: {reason}\n{_format_metadata(metadata)}"
-                        )
+                self._handle_expected_error(e)
+            elif e.code() == grpc.StatusCode.UNAVAILABLE:
                 raise Exception(
-                    f"{reason}: {status_proto.message}\n{_format_metadata(metadata)}"
-                ) from None
+                    f"Could not connect to Featureform.\n"
+                    "Please check if your FEATUREFORM_HOST and FEATUREFORM_CERT environment variables are set "
+                    "correctly or are explicitly set in the client or command line.\n"
+                    f"Details: {e.details()}",
+                    details=e.details(),
+                ) from e
             elif e.code() == grpc.StatusCode.UNAVAILABLE:
                 print("\n")
                 raise Exception(
@@ -89,16 +110,32 @@ class GrpcClient:
                     "Please check if your FEATUREFORM_HOST and FEATUREFORM_CERT environment variables are set "
                     "correctly or are explicitly set in the client or command line.\n"
                     f"Details: {e.details()}"
-                ) from initial_ex
+                ) from ex
             elif e.code() == grpc.StatusCode.UNKNOWN:
-                raise Exception(f"Error: {e.details()}") from initial_ex
+                raise Exception(f"Error: {e.details()}") from ex
             else:
                 print("RAISING ERROR")
             raise e
 
+    def _handle_expected_error(self, e: Optional[grpc.RpcError]) -> None:
+        if self.debug:
+            logging.debug("Processing expected gRPC error with details", exc_info=True)
+
+        # With the introduction of new server errors, this extracts the details from the grpc error
+        grpc_error_details = FFGrpcErrorDetails.from_grpc_error(e)
+        if grpc_error_details:
+            detailed_message = (
+                f"{grpc_error_details.reason}: {grpc_error_details.message}\n"
+                f"{_format_metadata(grpc_error_details.metadata)}"
+            )
+            if self.debug:
+                logging.debug(detailed_message)
+            raise Exception(detailed_message) from (e if self.debug else None)
+        raise e
+
 
 @contextlib.contextmanager
-def limited_traceback(limit):
+def _limited_traceback(limit):
     original_limit = getattr(sys, "tracebacklimit", None)
     sys.tracebacklimit = limit
     try:

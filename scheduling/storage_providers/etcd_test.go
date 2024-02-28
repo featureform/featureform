bf4eae98f61383ab0df2e5511d317f1c16d27448
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2024-02-28 13:28:02 -0800

    clean up keys at the start of etcd tests

--- scheduling/storage_providers/etcd_test.go
+++ scheduling/storage_providers/etcd_test.go
@@ -14,6 +14,22 @@ func TestStorageProviderETCD(t *testing.T) {
 		t.Fatalf("failed to create etcd storage provider: %v", err)
 	}
 
+	// Clean up all keys
+	keys, err := storage.ListKeys("")
+	if err != nil {
+		t.Fatalf("unable to list keys: %v", err)
+	}
+	for _, key := range keys {
+		lockObject, err := storage.Lock(key)
+		if err != nil {
+			t.Fatalf("could not lock key: %v", err)
+		}
+		err = storage.Delete(key, lockObject)
+		if err != nil {
+			t.Fatalf("could not set key: %v", err)
+		}
+	}
+
 	test := StorageProviderTest{
 		t:       t,
 		storage: storage,

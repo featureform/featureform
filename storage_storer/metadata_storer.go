2e4158ad63306bfe8e136d4757e51ed9bb22a69d
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2024-02-29 17:53:24 -0800

    adding MetadataStorer; still need to add the tests

--- storage_storer/metadata_storer.go
+++ storage_storer/metadata_storer.go
@@ -1,5 +1,64 @@
 package storage_storer
 
+type MetadataStorer struct {
+	Locker MultiLock
+	Storer metadataStorerImplementation
+}
+
+func (s *MetadataStorer) Create(key string, value string) error {
+	lock, err := s.Locker.Lock(key)
+	if err != nil {
+		return err
+	}
+	defer s.Locker.Unlock(lock)
+
+	return s.Storer.Set(key, value)
+}
+
+func (s *MetadataStorer) Update(key string, updateFn func(string) (string, error)) error {
+	lock, err := s.Locker.Lock(key)
+	if err != nil {
+		return err
+	}
+	defer s.Locker.Unlock(lock)
+
+	currentValue, err := s.Storer.Get(key)
+	if err != nil {
+		return err
+	}
+
+	newValue, err := updateFn(currentValue)
+	if err != nil {
+		return err
+	}
+
+	return s.Storer.Set(key, newValue)
+}
+
+func (s *MetadataStorer) List(prefix string) (map[string]string, error) {
+	// TODO: how do we lock a prefix?
+	return s.Storer.List(prefix)
+}
+
+func (s *MetadataStorer) Delete(key string) (string, error) {
+	lock, err := s.Locker.Lock(key)
+	if err != nil {
+		return "", err
+	}
+	defer s.Locker.Unlock(lock)
+
+	value, err := s.Storer.Get(key)
+	if err != nil {
+		return "", err
+	}
+
+	err = s.Storer.Delete(key)
+	if err != nil {
+		return "", err
+	}
+	return value, nil
+}
+
 type metadataStorerImplementation interface {
 	Set(key string, value string) error            // Set stores the value for the key and updates it if it already exists
 	Get(key string) (string, error)                // Get returns the value for the key

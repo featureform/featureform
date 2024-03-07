04af0415db75fa5728563066826a14860f1cf41d
anthonylasso <lasso_anthony@yahoo.com>
2024-03-07 17:17:35 -0600

    map + test fix
    all jest tests pass

--- dashboard/src/components/tasks/tableDataWrapper.js
+++ dashboard/src/components/tasks/tableDataWrapper.js
@@ -268,10 +268,15 @@ export default function TableDataWrapper() {
           </Tooltip>
         </Box>
       </Box>
-      <TaskRunDataGrid taskRunList={taskRunList.map(function(object) {
-        console.log(object.taskRun.taskId + '.' + object.taskRun.runId)
-        return { ...object, id: object.taskRun.taskId + '.' + object.taskRun.runId };
-      })} />
+      <TaskRunDataGrid
+        taskRunList={taskRunList?.map(function (object) {
+          console.log(object.taskRun.taskId + '.' + object.taskRun.runId);
+          return {
+            ...object,
+            id: object.taskRun.taskId + '.' + object.taskRun.runId,
+          };
+        })}
+      />
     </>
   );
 }

25740bc33228b6ede6e27fe0046885cda1dd30ee
sdreyer <sterling@featureform.com>
2024-03-12 12:44:58 -0700

    Added metadata tests

--- metadata/client_test.go
+++ metadata/client_test.go
@@ -2,8 +2,10 @@ package metadata
 
 import (
 	"encoding/json"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"reflect"
 	"testing"
+	"time"
 
 	pb "github.com/featureform/metadata/proto"
 )
@@ -275,5 +277,367 @@ func TestVectorValueType(t *testing.T) {
 	}
 	if wfc.IsEmbedding() != true {
 		t.Errorf("expected embedding to be true, got %v", wfc.IsEmbedding())
+	}
+}
+
+func TestFeatureVariant_ToShallowMap(t *testing.T) {
+	tests := []struct {
+		name       string
+		serialized *pb.FeatureVariant
+		want       FeatureVariantResource
+	}{
+		{"Precomputed", &pb.FeatureVariant{
+			Name:    "name",
+			Variant: "variant",
+			Source: &pb.NameVariant{
+				Name:    "source name",
+				Variant: "source variant",
+			},
+			Type:    "datatype",
+			Entity:  "entity",
+			Created: &timestamppb.Timestamp{},
+			Owner:   "owner",
+			Location: &pb.FeatureVariant_Columns{
+				Columns: &pb.Columns{
+					Entity: "entity",
+					Value:  "value",
+					Ts:     "ts",
+				},
+			},
+			Description: "description",
+			Provider:    "provider",
+			Status: &pb.ResourceStatus{
+				Status:       pb.ResourceStatus_NO_STATUS,
+				ErrorMessage: "error",
+				ErrorStatus:  &pb.ErrorStatus{},
+			},
+			Trainingsets:         []*pb.NameVariant{},
+			LastUpdated:          &timestamppb.Timestamp{},
+			Schedule:             "* * * * *",
+			Tags:                 &pb.Tags{},
+			Properties:           &pb.Properties{},
+			Mode:                 pb.ComputationMode_PRECOMPUTED,
+			IsEmbedding:          false,
+			Dimension:            64,
+			AdditionalParameters: &pb.FeatureParameters{},
+		}, FeatureVariantResource{
+			Created:     time.UnixMilli(0).UTC(),
+			Description: "description",
+			Entity:      "entity",
+			Name:        "name",
+			Variant:     "variant",
+			Owner:       "owner",
+			Provider:    "provider",
+			DataType:    "datatype",
+			Status:      "NO_STATUS",
+			Error:       "error",
+			Location: map[string]string{
+				"Entity": "entity",
+				"Source": "",
+				"TS":     "ts",
+				"Value":  "value",
+			},
+			Source: NameVariant{
+				Name:    "source name",
+				Variant: "source variant",
+			},
+			TrainingSets: nil,
+			Tags:         Tags{},
+			Properties:   Properties{},
+			Mode:         "PRECOMPUTED",
+			IsOnDemand:   false,
+			Definition:   "",
+		}},
+		{"ClientComputed", &pb.FeatureVariant{
+			Name:    "name",
+			Variant: "variant",
+			Type:    "datatype",
+			Entity:  "entity",
+			Created: &timestamppb.Timestamp{},
+			Owner:   "owner",
+			Location: &pb.FeatureVariant_Function{
+				Function: &pb.PythonFunction{
+					Query: []byte("abcd"),
+				},
+			},
+			Description: "description",
+			Provider:    "provider",
+			Status: &pb.ResourceStatus{
+				Status:       pb.ResourceStatus_NO_STATUS,
+				ErrorMessage: "error",
+				ErrorStatus:  &pb.ErrorStatus{},
+			},
+			Trainingsets:         []*pb.NameVariant{},
+			LastUpdated:          &timestamppb.Timestamp{},
+			Schedule:             "* * * * *",
+			Tags:                 &pb.Tags{},
+			Properties:           &pb.Properties{},
+			Mode:                 pb.ComputationMode_CLIENT_COMPUTED,
+			IsEmbedding:          false,
+			Dimension:            64,
+			AdditionalParameters: &pb.FeatureParameters{},
+		}, FeatureVariantResource{
+			Created:     time.UnixMilli(0).UTC(),
+			Description: "description",
+			Name:        "name",
+			Variant:     "variant",
+			Owner:       "owner",
+			Status:      "NO_STATUS",
+			Error:       "error",
+			Location: map[string]string{
+				"query": "abcd",
+			},
+			Source: NameVariant{
+				Name:    "",
+				Variant: "",
+			},
+			TrainingSets: nil,
+			Tags:         Tags{},
+			Properties:   Properties{},
+			Mode:         "CLIENT_COMPUTED",
+			IsOnDemand:   true,
+			Definition:   "",
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			variant := &FeatureVariant{
+				serialized:           tt.serialized,
+				fetchTrainingSetsFns: fetchTrainingSetsFns{tt.serialized},
+				fetchProviderFns:     fetchProviderFns{tt.serialized},
+				fetchSourceFns:       fetchSourceFns{tt.serialized},
+				createdFn:            createdFn{tt.serialized},
+				lastUpdatedFn:        lastUpdatedFn{tt.serialized},
+				protoStringer:        protoStringer{tt.serialized},
+				fetchTagsFn:          fetchTagsFn{tt.serialized},
+				fetchPropertiesFn:    fetchPropertiesFn{tt.serialized},
+				fetchIsEmbeddingFn:   fetchIsEmbeddingFn{tt.serialized},
+				fetchDimensionFn:     fetchDimensionFn{tt.serialized},
+			}
+			if got := variant.ToShallowMap(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToShallowMap() = \n%#v, \nwant \n%#v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLabelVariant_ToShallowMap(t *testing.T) {
+	tests := []struct {
+		name       string
+		serialized *pb.LabelVariant
+		want       LabelVariantResource
+	}{
+		{
+			"Simple",
+			&pb.LabelVariant{
+				Name:    "name",
+				Variant: "variant",
+				Source: &pb.NameVariant{
+					Name:    "source name",
+					Variant: "source variant",
+				},
+				Type:    "datatype",
+				Entity:  "entity",
+				Created: &timestamppb.Timestamp{},
+				Owner:   "owner",
+				Location: &pb.LabelVariant_Columns{
+					Columns: &pb.Columns{
+						Entity: "entity",
+						Value:  "value",
+						Ts:     "ts",
+					},
+				},
+				Description: "description",
+				Provider:    "provider",
+				Status: &pb.ResourceStatus{
+					Status:       pb.ResourceStatus_NO_STATUS,
+					ErrorMessage: "error",
+					ErrorStatus:  &pb.ErrorStatus{},
+				},
+				Trainingsets: []*pb.NameVariant{},
+
+				Tags:       &pb.Tags{},
+				Properties: &pb.Properties{},
+			}, LabelVariantResource{
+				Created:     time.UnixMilli(0).UTC(),
+				Description: "description",
+				Entity:      "entity",
+				Name:        "name",
+				Variant:     "variant",
+				Owner:       "owner",
+				Provider:    "provider",
+				DataType:    "datatype",
+				Status:      "NO_STATUS",
+				Error:       "error",
+				Location: map[string]string{
+					"Entity": "entity",
+					"Source": "",
+					"TS":     "ts",
+					"Value":  "value",
+				},
+				Source: NameVariant{
+					Name:    "source name",
+					Variant: "source variant",
+				},
+				TrainingSets: nil,
+				Tags:         Tags{},
+				Properties:   Properties{},
+			}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			variant := &LabelVariant{
+				serialized:           tt.serialized,
+				fetchTrainingSetsFns: fetchTrainingSetsFns{tt.serialized},
+				fetchProviderFns:     fetchProviderFns{tt.serialized},
+				fetchSourceFns:       fetchSourceFns{tt.serialized},
+				createdFn:            createdFn{tt.serialized},
+				protoStringer:        protoStringer{tt.serialized},
+				fetchTagsFn:          fetchTagsFn{tt.serialized},
+				fetchPropertiesFn:    fetchPropertiesFn{tt.serialized},
+			}
+			if got := variant.ToShallowMap(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToShallowMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestSourceVariant_ToShallowMap(t *testing.T) {
+	tests := []struct {
+		name       string
+		serialized *pb.SourceVariant
+		want       SourceVariantResource
+	}{
+		{
+			"Simple",
+			&pb.SourceVariant{
+				Name:        "name",
+				Variant:     "variant",
+				Created:     &timestamppb.Timestamp{},
+				Owner:       "owner",
+				Description: "description",
+				Provider:    "provider",
+				Definition: &pb.SourceVariant_PrimaryData{
+					PrimaryData: &pb.PrimaryData{
+						Location: &pb.PrimaryData_Table{
+							Table: &pb.PrimarySQLTable{
+								Name: "table",
+							},
+						},
+					},
+				},
+				Status: &pb.ResourceStatus{
+					Status:       pb.ResourceStatus_NO_STATUS,
+					ErrorMessage: "error",
+					ErrorStatus:  &pb.ErrorStatus{},
+				},
+				Trainingsets: []*pb.NameVariant{},
+				LastUpdated:  &timestamppb.Timestamp{},
+				Schedule:     "* * * * *",
+				Tags:         &pb.Tags{},
+				Properties:   &pb.Properties{},
+			},
+			SourceVariantResource{
+				Created:        time.UnixMilli(0).UTC(),
+				Description:    "description",
+				Name:           "name",
+				Variant:        "variant",
+				Owner:          "owner",
+				Provider:       "provider",
+				Status:         "NO_STATUS",
+				Error:          "error",
+				TrainingSets:   nil,
+				Tags:           Tags{},
+				Properties:     Properties{},
+				Definition:     "table",
+				LastUpdated:    time.UnixMilli(0).UTC(),
+				Schedule:       "* * * * *",
+				SourceType:     "Primary Table",
+				Specifications: map[string]string{},
+				Inputs:         NameVariants{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			variant := &SourceVariant{
+				serialized:           tt.serialized,
+				fetchTrainingSetsFns: fetchTrainingSetsFns{tt.serialized},
+				fetchProviderFns:     fetchProviderFns{tt.serialized},
+				createdFn:            createdFn{tt.serialized},
+				lastUpdatedFn:        lastUpdatedFn{tt.serialized},
+				protoStringer:        protoStringer{tt.serialized},
+				fetchTagsFn:          fetchTagsFn{tt.serialized},
+				fetchPropertiesFn:    fetchPropertiesFn{tt.serialized},
+			}
+			if got := variant.ToShallowMap(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToShallowMap() = \n%#v, \nwant \n%#v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTrainingSetVariant_ToShallowMap(t *testing.T) {
+	tests := []struct {
+		name       string
+		serialized *pb.TrainingSetVariant
+		want       TrainingSetVariantResource
+	}{
+		{
+			"Simple",
+			&pb.TrainingSetVariant{
+				Name:        "name",
+				Variant:     "variant",
+				Created:     &timestamppb.Timestamp{},
+				Owner:       "owner",
+				Description: "description",
+				Provider:    "provider",
+				Label: &pb.NameVariant{
+					Name:    "label name",
+					Variant: "label variant",
+				},
+				Features: []*pb.NameVariant{
+					{Name: "feature name", Variant: "feature variant"},
+				},
+				Status: &pb.ResourceStatus{
+					Status:       pb.ResourceStatus_NO_STATUS,
+					ErrorMessage: "error",
+					ErrorStatus:  &pb.ErrorStatus{},
+				},
+				LastUpdated: &timestamppb.Timestamp{},
+				Schedule:    "* * * * *",
+				Tags:        &pb.Tags{},
+				Properties:  &pb.Properties{},
+			},
+			TrainingSetVariantResource{
+				Created:     time.UnixMilli(0).UTC(),
+				Description: "description",
+				Name:        "name",
+				Variant:     "variant",
+				Owner:       "owner",
+				Provider:    "provider",
+				Label:       NameVariant{Name: "label name", Variant: "label variant"},
+				Status:      "NO_STATUS",
+				Error:       "error",
+				Tags:        Tags{},
+				Properties:  Properties{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			variant := &TrainingSetVariant{
+				serialized:        tt.serialized,
+				fetchProviderFns:  fetchProviderFns{tt.serialized},
+				createdFn:         createdFn{tt.serialized},
+				lastUpdatedFn:     lastUpdatedFn{tt.serialized},
+				protoStringer:     protoStringer{tt.serialized},
+				fetchTagsFn:       fetchTagsFn{tt.serialized},
+				fetchPropertiesFn: fetchPropertiesFn{tt.serialized},
+			}
+			if got := variant.ToShallowMap(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToShallowMap() = \n%#v, \nwant \n%#v", got, tt.want)
+			}
+		})
 	}
 }

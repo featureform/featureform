177a75fe5d179dc06301e4cb2a8c025d2ba76579
ahmadnazeri <42309296+ahmadnazeri@users.noreply.github.com>
2024-04-29 13:12:40 -0700

    fixing behave tests

--- tests/end_to_end/features/model_registration.feature
+++ tests/end_to_end/features/model_registration.feature
@@ -1,15 +1,17 @@
 Feature: Model Registration
-  Scenario: Registerign a model with non-existing training set
+  Scenario Outline: Registering a model with both non-existing and existing feature and training set
     Given Featureform is installed
     When I create a "hosted" "insecure" client for "localhost:7878"
-    And I register a "<offline_provider>"
+    And I register an offline provider of type "<offline_provider_type>"
+    And I register an online provider of type "<online_provider_type>"
     And I register a dataset located at "<dataset_path>"
-    And I register a feature based on the column "<column_name>" with type "<feature_type>"
+    And I register a feature on "<feature_column>" with type "<feature_type>" with "<entity_column>" and "<label_column>"
     Then I can register a training set based on the feature
-    And I can serve the non-existing training set with the model
+    And I cannot serve the non-existing feature with the model
+    And I can serve the registered feature with the model
+    And I cannot serve the non-existing training set with the model
     And I can serve the registered training set with the model
-
+    
     Examples:
-      | offline_provider | dataset_path | column_name | feature_type |
-      |     Postgres     | transactions |    amount   |    float64   |
-      |     Postgres     | Transactions |    amount   |    float64   |
+      | online_provider_type | offline_provider_type | dataset_path |  feature_column   | feature_type | entity_column | label_column |
+      |        redis         |        postgres       | transactions | transactionamount |    float64   |   customerid  |   isfraud    |
--- tests/end_to_end/features/steps/model_registration.py
+++ tests/end_to_end/features/steps/model_registration.py
@@ -1,40 +1,60 @@
 import os
+import random
+import string
 
 import featureform as ff
-from featureform.enums import ScalarType
 from behave import when, then
+from dotenv import load_dotenv
+from featureform.enums import ScalarType
 
 
-@when("I register a '{offline_provider}'")
-def step_impl(context, offline_provider):
-    from dotenv import load_dotenv
+@when('I register an offline provider of type "{offline_provider_type}"')
+def step_impl(context, offline_provider_type):
+    version = random_word(5)
 
     load_dotenv("../../.env")
 
-    if offline_provider.lower() == "postgres":
+    if offline_provider_type.lower() == "postgres":
         context.offline_provider = ff.register_postgres(
-            name="postgres",
+            name=f"postgres_{version}",
             host=os.getenv("POSTGRES_HOST", "host.docker.internal"),
-            port=int(os.getenv("POSTGRES_PORT", "5432")),
+            port=os.getenv("POSTGRES_PORT", "5432"),
             user=os.getenv("POSTGRES_USER", "postgres"),
             password=os.getenv("POSTGRES_PASSWORD", "password"),
             database=os.getenv("POSTGRES_DB", "postgres"),
         )
-    elif offline_provider.lower() == "snowflake":
+    elif offline_provider_type.lower() == "snowflake":
         raise NotImplementedError("Snowflake registration is not implemented")
-    elif offline_provider.lower() == "bigquery":
+    elif offline_provider_type.lower() == "bigquery":
         raise NotImplementedError("BigQuery registration is not implemented")
-    elif offline_provider.lower() == "spark":
+    elif offline_provider_type.lower() == "spark":
         raise NotImplementedError("Spark registration is not implemented")
     else:
-        raise ValueError(f"Unknown offline provider {offline_provider}")
+        raise ValueError(f"Unknown offline provider {offline_provider_type}")
 
     context.client.apply(asynchronous=False, verbose=True)
 
 
-@when("I register a dataset located at '{dataset_path}'")
+@when('I register an online provider of type "{online_provider_type}"')
+def step_impl(context, online_provider_type):
+    version = random_word(5)
+    load_dotenv("../../.env")
+
+    if online_provider_type.lower() == "redis":
+        context.online_provider = ff.register_redis(
+            name=f"redis_{version}",
+            host=os.getenv("REDIS_HOST", "host.docker.internal"),
+            port=int(os.getenv("REDIS_PORT", "6379")),
+        )
+    else:
+        raise ValueError(f"Unknown offline provider {online_provider_type}")
+
+    context.client.apply(asynchronous=False, verbose=True)
+
+
+@when('I register a dataset located at "{dataset_path}"')
 def step_impl(context, dataset_path):
-    if context.offline_provider.provider.config.type() == "SPARK_OFFLINE":
+    if context.offline_provider._OfflineProvider__provider.config.type() == "SPARK_OFFLINE":
         dataset = context.offline_provider.register_file(
             name="transactions",
             file_path=dataset_path,
@@ -49,51 +69,99 @@ def step_impl(context, dataset_path):
     context.dataset = dataset
 
 
-@when("I register a feature base on the colum '{column_name}' with type '{feature_type}'")
-def step_impl(context, column_name, feature_type):
+@when('I register a feature on "{feature_column}" with type "{feature_type}" with "{entity_column}" and "{label_column}"')
+def step_impl(context, feature_column, feature_type, entity_column, label_column):
     @ff.entity
     class User:
-        amount = ff.Feature(
-            context.dataset[column_name],
+        user_feature = ff.Feature(
+            context.dataset[[entity_column, feature_column]],
             type=ScalarType(feature_type.lower()),  # converts the string into Featureform type
             inference_store=context.online_provider,
         )
-        fraudulent = ff.Label(
-            context.dataset["IsFraud"],
+        user_label = ff.Label(
+            context.dataset[[entity_column, label_column]],
             type=ff.Bool,
         )
 
     context.client.apply(asynchronous=False, verbose=True)
-    context.feature = User.amount
-    context.label = User.fraudulent
+    context.user_feature = User.user_feature
+    context.user_label = User.user_label
 
 
 @then("I can register a training set based on the feature")
 def step_impl(context):
-    training_set = context.client.register_training_set(
+    training_set = ff.register_training_set(
         name="fraud_training",
-        features=[context.feature],
-        label=context.label,
+        features=[context.user_feature],
+        label=context.user_label,
     )
     context.client.apply(asynchronous=False, verbose=True)
     context.training_set = training_set
 
 
-@then("I can serve the non-existing training set with the model")
+@then("I cannot serve the non-existing feature with the model")
+def step_impl(context):
+    """
+    This step expects an error because the feature doesn't exist.
+    If it doesn't error while serving the non-existing feature, it will raise an error.
+    """
+
+    context.model = "fraud-model"
+    try:
+        _ = context.client.features(
+            [(context.user_feature.name, "non-existent")],
+            {"user": "C5841053"},
+            model=context.model,
+        )
+    except Exception as e:
+        if "metadata lookup failed" in str(e):
+            return
+        else:
+            raise ValueError(f"Unexpected error: {e}")
+
+    raise ValueError("Expected an error but none was raised")
+
+
+@then("I can serve the registered feature with the model")
 def step_impl(context):
     context.model = "fraud-model"
-    dataset = context.client.training_set(
-        name="fraud_training",
-        variant="non-existent",
+    feature = context.client.features(
+        [(context.user_feature.name, context.user_feature.variant)],
+        {"user": "C5841053"},
         model=context.model,
     )
 
-    training_dataset = dataset.repeat(10).shuffle(1000).batch(8)
-    for i, feature_batch in enumerate(training_dataset):
-        if i >= 1:
+    assert len(feature) > 0
+
+
+@then("I cannot serve the non-existing training set with the model")
+def step_impl(context):
+    """
+    This step expects an error because the training set doesn't exist.
+    If it doesn't error while serving the non-existing training set, it will raise an error.
+    """
+
+    context.model = "fraud-model"
+    try:
+        dataset = context.client.training_set(
+            name="fraud_training",
+            variant="non-existent",
+            model=context.model,
+        )
+
+        training_dataset = dataset.repeat(10).shuffle(1000).batch(8)
+        for i, _ in enumerate(training_dataset):
+            if i >= 1:
+                return
+
+    except Exception as e:
+        if "Key Not Found" in str(e):
             return
-        print(feature_batch.to_list())
-    
+        else:
+            raise ValueError(f"Unexpected error: {e}")
+
+    raise ValueError("Expected an error but none was raised")
+
 
 @then("I can serve the registered training set with the model")
 def step_impl(context):
@@ -108,3 +176,8 @@ def step_impl(context):
         if i >= 1:
             return
         print(feature_batch.to_list())
+
+
+def random_word(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))

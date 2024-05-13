fc00b36b82febd85d9fb0fa5ad0626550b4b6bea
Ali Olfat <ali@featureform.com>
2024-05-13 14:41:29 -0700

    fix get_source

--- client/src/featureform/register.py
+++ client/src/featureform/register.py
@@ -4795,7 +4795,7 @@ class ResourceClient:
 
         definition = self._get_source_definition(source)
 
-        return SourceVariant(
+        source_variant = SourceVariant(
             created=None,
             name=source.name,
             definition=definition,
@@ -4810,6 +4810,7 @@ class ResourceClient:
                 definition.source_text if type(definition) == DFTransformation else ""
             ),
         )
+        return ColumnSourceRegistrar(self, source_variant)
 
     def _get_source_definition(self, source):
         if source.primaryData.table.name:

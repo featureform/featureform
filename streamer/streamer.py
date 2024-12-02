10a32506af7b24b4b20288aac460134292f162f8
anthonylasso <lasso_anthony@yahoo.com>
2024-12-02 16:54:28 -0600

    initial streamer file
    with example test files. the long file won't need to be as long for unit/integration tests (or we can download it from our quickstart url)

---
+++ streamer/streamer.py
@@ -0,0 +1,30 @@
+import os
+import pyarrow as pa
+import pyarrow.csv as csv
+from pyarrow.flight import FlightServerBase, RecordBatchStream
+
+class StreamerService(FlightServerBase):
+    def __init__(self):
+        location = "grpc://0.0.0.0:8085"
+        super(StreamerService, self).__init__(location)
+
+    def do_get(self, context, ticket):
+        table_name = ticket.ticket.decode("utf-8")
+        table = self.load_table_from_iceberg(table_name)
+
+        return RecordBatchStream(table)
+
+    def load_table_from_iceberg(self, table_name):
+        print(f"Loading table: {table_name}")
+        csv_path = f"./{table_name}.csv"
+        if not os.path.exists(csv_path):
+            raise FileNotFoundError(f"Table {table_name} not found at path: {csv_path}")
+
+        # use pyarrow.csv to read the CSV file into an Arrow Table
+        table = csv.read_csv(csv_path)
+        return table
+
+if __name__ == "__main__":
+    print("Starting the streamer client service...")
+    server = StreamerService()
+    server.serve()

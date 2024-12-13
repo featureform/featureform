889d1009f946048a8e08bd284d2f137f279be9ac
anthonylasso <lasso_anthony@yahoo.com>
2024-12-12 18:14:07 -0600

    license tag

--- streamer/iceberg_streamer.py
+++ streamer/iceberg_streamer.py
@@ -1,4 +1,10 @@
-import os
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Copyright 2024 FeatureForm Inc.
+#
+
 import json
 from pyiceberg.catalog import load_catalog
 from pyarrow.flight import FlightServerBase, RecordBatchStream

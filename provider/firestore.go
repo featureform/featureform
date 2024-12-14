6970a977074057e7fe85e2462ec4eb49de5b7ec8
Kamal Sadek <kamal@featureform.com>
2024-12-13 17:46:58 -0800

    Implement serializer interface for firestore provider

--- provider/firestore.go
+++ provider/firestore.go
@@ -17,19 +17,62 @@ import (
 	"errors"
 	"fmt"
 	"github.com/featureform/logging"
+	se "github.com/featureform/provider/serialization"
 	"go.uber.org/zap"
 	"google.golang.org/api/iterator"
-
 	"google.golang.org/grpc/status"
 
 	"cloud.google.com/go/firestore"
 	"github.com/featureform/fferr"
 	pc "github.com/featureform/provider/provider_config"
 	pt "github.com/featureform/provider/provider_type"
-	"github.com/featureform/provider/types"
+	vt "github.com/featureform/provider/types"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc/codes"
 )
+
+// The serializer versions. If adding a new one make sure to add to the serializers map variable
+const (
+	// firestoreSerializeV0 serializes everything as strings, including numbers
+	firestoreSerializeV0 se.SerializeVersion = iota
+)
+
+type firestoreSerializerV0 struct{}
+
+func (ser firestoreSerializerV0) Version() se.SerializeVersion { return firestoreSerializeV0 }
+
+func (ser firestoreSerializerV0) Serialize(_ vt.ValueType, value any) (interface{}, error) {
+	// The client automatically handles this for us.
+	return value, nil
+}
+
+func (ser firestoreSerializerV0) Deserialize(t vt.ValueType, value interface{}) (any, error) {
+	// Firestore only has one integer and float type, each being converted into int64 and float64 respectively.
+	// For conversions, see https://pkg.go.dev/cloud.google.com/go/firestore@v1.15.0#DocumentSnapshot.DataTo
+	switch t {
+	case vt.Int:
+		return int(value.(int64)), nil
+	case vt.Int8:
+		return int8(value.(int64)), nil
+	case vt.Int16:
+		return int16(value.(int64)), nil
+	case vt.Int32:
+		return int32(value.(int64)), nil
+	case vt.Int64:
+		return value.(int64), nil
+	case vt.Float32:
+		return float32(value.(float64)), nil
+	case vt.Float64:
+		return value.(float64), nil
+	case vt.NilType, vt.Bool, vt.String, vt.Timestamp:
+		// These are all converted to the same types we use by the client
+		return value, nil
+	default:
+		err := fferr.NewInternalErrorf("Unsupported casting value %v of type %T into %v", value, value, t)
+		err.AddDetail("version", ser.Version().String())
+		return nil, err
+	}
+}
 
 func (t firestoreTableKey) String() string {
 	return fmt.Sprintf("%s__%s__%s", t.Collection, t.Feature, t.Variant)
@@ -43,9 +86,11 @@ type firestoreOnlineStore struct {
 }
 
 type firestoreOnlineTable struct {
-	document  *firestore.DocumentRef
-	key       firestoreTableKey
-	valueType types.ValueType
+	document   *firestore.DocumentRef
+	key        firestoreTableKey
+	valueType  vt.ValueType
+	serializer se.Serializer[interface{}]
+	logger     *zap.SugaredLogger
 }
 
 type firestoreTableKey struct {
@@ -133,14 +178,17 @@ func (store *firestoreOnlineStore) GetTable(feature, variant string) (OnlineStor
 		wrapped.AddDetail("table_name", tableName)
 		return nil, wrapped
 	}
+	logger := store.logger.With("table", tableName)
 	return &firestoreOnlineTable{
-		document:  table.Ref,
-		key:       key,
-		valueType: types.ScalarType(valueType.(string)),
+		document:   table.Ref,
+		key:        key,
+		valueType:  vt.ScalarType(valueType.(string)),
+		serializer: firestoreSerializerV0{},
+		logger:     logger,
 	}, nil
 }
 
-func (store *firestoreOnlineStore) CreateTable(feature, variant string, valueType types.ValueType) (OnlineStoreTable, error) {
+func (store *firestoreOnlineStore) CreateTable(feature, variant string, valueType vt.ValueType) (OnlineStoreTable, error) {
 	table, _ := store.GetTable(feature, variant)
 	if table != nil {
 		return nil, fferr.NewDatasetAlreadyExistsError(feature, variant, nil)
@@ -159,10 +207,13 @@ func (store *firestoreOnlineStore) CreateTable(feature, variant string, valueTyp
 	if err != nil {
 		return nil, fferr.NewResourceExecutionError(pt.FirestoreOnline.String(), feature, variant, fferr.FEATURE_VARIANT, err)
 	}
+	logger := store.logger.With("table", tableName)
 	return &firestoreOnlineTable{
-		document:  store.collection.Doc(tableName),
-		key:       key,
-		valueType: valueType,
+		document:   store.collection.Doc(tableName),
+		key:        key,
+		valueType:  valueType,
+		serializer: firestoreSerializerV0{},
+		logger:     logger,
 	}, nil
 
 }
@@ -200,8 +251,15 @@ func (store *firestoreOnlineStore) CheckHealth() (bool, error) {
 }
 
 func (table firestoreOnlineTable) Set(entity string, value interface{}) error {
+	serializedValue, err := table.serializer.Serialize(table.valueType, value)
+
+	if err != nil {
+		table.logger.Error("Error serializing value", "entity", entity, "value", value, "err", err)
+		return err
+	}
+
 	if _, err := table.document.Set(context.TODO(), map[string]interface{}{
-		entity: value,
+		entity: serializedValue,
 	}, firestore.MergeAll); err != nil {
 		wrapped := fferr.NewResourceExecutionError(pt.FirestoreOnline.String(), table.key.Feature, table.key.Variant, fferr.ENTITY, err)
 		wrapped.AddDetail("entity", entity)

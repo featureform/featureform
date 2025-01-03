458ee03678e6fa41a32f8bf8c325df3ed5bec002
anthonylasso <lasso_anthony@yahoo.com>
2025-01-03 16:24:34 -0600

    client edits

--- client/src/featureform/constants.py
+++ client/src/featureform/constants.py
@@ -8,3 +8,6 @@
 # For fetching sources as dataframes, the default limit is set to -1
 # to denote that all records should be fetched.
 NO_RECORD_LIMIT = -1
+
+# 1 million records
+ONE_MILLION_RECORD_LIMIT = 1_000_000

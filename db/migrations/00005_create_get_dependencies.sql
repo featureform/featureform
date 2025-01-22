41b69ba9020361eaf5ad052aa2138ba12e5fda5e
Ali Olfat <ali@featureform.com>
2025-01-22 12:22:36 -0800

    add depth protection

--- db/migrations/00005_create_get_dependencies.sql
+++ db/migrations/00005_create_get_dependencies.sql
@@ -1,7 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
--- Migration script to create the get_dependencies function
+-- Migration script to create the get_dependencies function with depth protection
 DROP FUNCTION IF EXISTS get_dependencies(integer, varchar, varchar);
+
 -- Create the function
 CREATE FUNCTION get_dependencies(
     p_from_resource_type integer,
@@ -25,7 +26,8 @@ BEGIN
             e.from_resource_variant,
             e.to_resource_type,
             e.to_resource_name,
-            e.to_resource_variant
+            e.to_resource_variant,
+            1 AS depth -- Initialize depth
         FROM edges e
         WHERE e.from_resource_type = p_from_resource_type
             AND e.from_resource_name = p_from_resource_name
@@ -40,12 +42,14 @@ BEGIN
             e.from_resource_variant,
             e.to_resource_type,
             e.to_resource_name,
-            e.to_resource_variant
+            e.to_resource_variant,
+            dc.depth + 1 -- Increment depth
         FROM edges e
         INNER JOIN dependency_chain dc 
             ON e.from_resource_type = dc.to_resource_type
             AND e.from_resource_name = dc.to_resource_name
             AND e.from_resource_variant = dc.to_resource_variant
+        WHERE dc.depth < 500 -- Limit recursion depth
     )
     -- Select only unique downstream dependencies
     SELECT DISTINCT
@@ -53,9 +57,19 @@ BEGIN
         dc.to_resource_name,
         dc.to_resource_variant
     FROM dependency_chain dc;
+
+    -- If depth exceeds the limit, raise an error
+    IF EXISTS (
+        SELECT 1
+        FROM dependency_chain
+        WHERE depth >= 500
+    ) THEN
+        RAISE EXCEPTION 'Cycle detected or recursion depth exceeded limit of 500';
+    END IF;
 END;
 $function$;
 -- +goose StatementEnd
+
 -- +goose Down
 -- +goose StatementBegin
 -- Drop the function

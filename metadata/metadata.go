578d2733c9e85e921bfd6a637f4ed9f2f2f0138c
Simba Khadder <simba@featureform.com>
2025-01-23 15:17:20 -0800

    Make tests less flaky

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -2733,11 +2733,12 @@ func (serv *MetadataServer) ListSources(request *pb.ListRequest, stream pb.Metad
 func (serv *MetadataServer) CreateSourceVariant(ctx context.Context, variantRequest *pb.SourceVariantRequest) (*pb.Empty, error) {
 	ctx = logging.AttachRequestID(logging.RequestID(variantRequest.RequestId), ctx, serv.Logger)
 	logger := logging.GetLoggerFromContext(ctx).WithResource(logging.SourceVariant, variantRequest.SourceVariant.Name, variantRequest.SourceVariant.Variant)
-	logger.Info("Creating Source Variant", "source_name", variantRequest.SourceVariant.Name, "source_variant", variantRequest.SourceVariant.Variant)
+	logger.Info("Creating Source Variant")
 
 	variant := variantRequest.SourceVariant
 	variant.Created = tspb.New(time.Now())
 	taskTarget := scheduling.NameVariant{Name: variant.Name, Variant: variant.Variant, ResourceType: SOURCE_VARIANT.String()}
+	logger.Debug("Creating task for source variant")
 	task, err := serv.taskManager.CreateTask(ctx, "mytask", scheduling.ResourceCreation, taskTarget)
 	if err != nil {
 		logger.Errorw("failed to create task", "error", err)
@@ -2745,7 +2746,8 @@ func (serv *MetadataServer) CreateSourceVariant(ctx context.Context, variantRequ
 	}
 	variant.TaskIdList = []string{task.ID.String()}
 	if _, isTransformation := variant.Definition.(*pb.SourceVariant_Transformation); isTransformation {
-		if err := serv.addTransformationLocation(ctx, variant); err != nil {
+		logger.Debug("Adding transformation location to proto")
+		if err := serv.addTransformationLocation(ctx, variant, logger); err != nil {
 			logger.Errorw("failed to add transformation location", "error", err)
 			return nil, err
 		}
@@ -2764,8 +2766,9 @@ func (serv *MetadataServer) CreateSourceVariant(ctx context.Context, variantRequ
 
 // addTransformationLocation adds the location to the transformation field of the source variant given the client
 // isn't capable of populating the location field properly. Given metadata is the source of truth and "upstream"
-func (serv *MetadataServer) addTransformationLocation(ctx context.Context, sv *pb.SourceVariant) error {
-	logger := serv.Logger.With("source_name", sv.Name, "source_variant", sv.Variant)
+func (serv *MetadataServer) addTransformationLocation(
+	ctx context.Context, sv *pb.SourceVariant, logger logging.Logger,
+) error {
 	if sv.GetTransformation() == nil {
 		logger.Warn("Transformation not set on source variant")
 		return nil
@@ -2775,41 +2778,45 @@ func (serv *MetadataServer) addTransformationLocation(ctx context.Context, sv *p
 		return nil
 	}
 	providerName := sv.GetProvider()
-	logger.Debugw("Looking up provider for transformation location", "provider", providerName)
+	logger = logger.With("provider", providerName)
+	logger.Debug("Looking up provider for transformation location")
 	providerResource, err := serv.lookup.Lookup(ctx, ResourceID{Name: providerName, Type: PROVIDER})
 	if err != nil {
-		logger.Errorw("Failed to lookup provider resource", "provider", providerName, "error", err)
+		logger.Errorw("Failed to lookup provider resource", "error", err)
 		return err
 	}
-	logger.Debugw("Provider for transformation location", "provider", providerName)
+	logger.Debug("Provider for transformation location")
 	providerProto, isProviderProto := providerResource.Proto().(*pb.Provider)
 	if !isProviderProto {
-		logger.Errorw("Provider resource is not a provider proto", "provider", providerResource.Proto())
+		logger.Errorw("Provider resource is not a provider proto")
 		return fferr.NewInternalErrorf("provider resource is not a provider proto")
 	}
+	logger.Debug("Getting localizer")
 	localizer, err := GetLocalizer(pt.Type(providerProto.GetType()), providerProto.GetSerializedConfig())
 	if err != nil {
-		logger.Errorw("Failed to get localizer", "provider", providerName, "error", err)
+		logger.Errorw("Failed to get localizer", "error", err)
 		return err
 	}
+	logger.Debug("Localizing source")
 	location, err := localizer.Localize(sv)
 	if err != nil {
 		logger.Errorw("Failed to localize transformation location", "provider", providerName, "error", err)
 		return err
 	}
-	logger.Debugw("Localized transformation location", "location", location.Location(), "type", location.Type())
+	logger = logger.With("location", location.Location(), "location-type", location.Type())
+	logger.Debugw("Localized transformation location")
 	transformation := sv.GetTransformation()
 
 	switch lt := location.(type) {
 	case *pl.SQLLocation:
-		logger.Debugw("Adding SQL location to transformation", "location", lt.Location())
+		logger.Debugw("Added SQL location to transformation")
 		transformation.Location = &pb.Transformation_Table{
 			Table: &pb.SQLTable{
 				Name: lt.Location(),
 			},
 		}
 	case *pl.CatalogLocation:
-		logger.Debugw("Adding catalog location to transformation", "database", lt.Database(), "table", lt.Table())
+		logger.Debugw("Added catalog location to transformation", "database", lt.Database(), "table", lt.Table())
 		transformation.Location = &pb.Transformation_Catalog{
 			Catalog: &pb.CatalogTable{
 				Database:    lt.Database(),
@@ -2818,7 +2825,7 @@ func (serv *MetadataServer) addTransformationLocation(ctx context.Context, sv *p
 			},
 		}
 	case *pl.FileStoreLocation:
-		logger.Debugw("Adding filestore location to transformation", "path", lt.Filepath().ToURI())
+		logger.Debugw("Added filestore location to transformation", "path", lt.Filepath().ToURI())
 		transformation.Location = &pb.Transformation_Filestore{
 			Filestore: &pb.FileStoreTable{
 				Path: lt.Filepath().ToURI(),
@@ -2848,7 +2855,7 @@ func (serv *MetadataServer) GetSourceVariants(stream pb.Metadata_GetSourceVarian
 		if !isSourceVariant {
 			return fferr.NewInternalErrorf("expected source variant, got %T", msg)
 		}
-		if err := serv.sourceVariantBackwardsCompatibility(ctx, sv); err != nil {
+		if err := serv.sourceVariantBackwardsCompatibility(ctx, sv, logger); err != nil {
 			return err
 		}
 		return stream.Send(sv)
@@ -2859,8 +2866,8 @@ func (serv *MetadataServer) GetSourceVariants(stream pb.Metadata_GetSourceVarian
 // were created before the expansion of location type into `CatalogTable` and `FileStoreTable` (i.e. PR #1109); for primary
 // sources, it achieves this by checking the location "name" to see if it stars with `glue://`, for Iceberg catalog locations,
 // or `s3://` or `s3a://`, for S3 filestore locations and updating the source variant's location to the new format.
-func (serv *MetadataServer) sourceVariantBackwardsCompatibility(ctx context.Context, sv *pb.SourceVariant) error {
-	logger := serv.Logger.With("source_name", sv.Name, "source_variant", sv.Variant)
+func (serv *MetadataServer) sourceVariantBackwardsCompatibility(ctx context.Context, sv *pb.SourceVariant, logger logging.Logger) error {
+	logger = logger.WithResource(logging.SourceVariant, sv.Name, sv.Variant)
 	logger.Debug("Checking source variant for backwards compatibility")
 	switch def := sv.GetDefinition().(type) {
 	case *pb.SourceVariant_PrimaryData:
@@ -2930,14 +2937,14 @@ func (serv *MetadataServer) sourceVariantBackwardsCompatibility(ctx context.Cont
 		logger.Debug("Backfilling transformation location")
 		// addTransformationLocation already works to handle new transformations, so we just need to call it here
 		// on existing transformations to ensure they are correctly backfilled.
-		if err := serv.addTransformationLocation(ctx, sv); err != nil {
+		if err := serv.addTransformationLocation(ctx, sv, logger); err != nil {
 			return err
 		}
 	default:
 		return fferr.NewInternalErrorf("unknown source variant definition type: %T", def)
 	}
 	id := ResourceID{Name: sv.Name, Variant: sv.Variant, Type: SOURCE_VARIANT}
-	logger.Debugw("Setting source variant", "id", id)
+	logger.Debugw("Setting updated source variant")
 	if err := serv.lookup.Set(ctx, id, &sourceVariantResource{sv}); err != nil {
 		logger.Errorw("Failed to set source variant", "id", id, "error", err)
 		return err
@@ -3245,67 +3252,75 @@ type initParentFn func(name, variant string) Resource
 
 func (serv *MetadataServer) genericCreate(ctx context.Context, res Resource, init initParentFn) (*pb.Empty, error) {
 	logger := logging.GetLoggerFromContext(ctx).WithResource(res.ID().Type.ToLoggingResourceType(), res.ID().Name, res.ID().Variant)
-	logger.Debugw("Creating Generic Resource: ", res.ID().Name, res.ID().Variant)
+	logger.Debug("Creating Generic Resource")
 
 	id := res.ID()
+	logger.Debug("Checking if resource is named safely")
 	if err := resourceNamedSafely(id); err != nil {
 		logger.Errorw("Resource name is not valid", "error", err)
 		return nil, err
 	}
-	existing, err := serv.lookup.Lookup(ctx, id)
-	if _, isResourceError := err.(*fferr.KeyNotFoundError); err != nil && !isResourceError {
+	logger.Debug("Getting existing resource if it already exists")
+	existing, err := serv.lookup.Lookup(logger.AttachToContext(ctx), id)
+	if _, isKeyNotFoundErr := err.(*fferr.KeyNotFoundError); err != nil && !isKeyNotFoundErr {
 		logger.Errorw("Error looking up resource", "resource ID", id, "error", err)
-		// TODO: consider checking the GRPCError interface to avoid double wrapping error
 		return nil, fferr.NewInternalError(err)
 	}
-
 	if existing != nil {
-		err = serv.validateExisting(res, existing)
-		if err != nil {
+		logger.Debug("Resource exists, validating...")
+		if err := serv.validateExisting(logger.AttachToContext(ctx), res, existing); err != nil {
 			logger.Errorw("ID exists but is not equivalent", "error", err)
 			return nil, err
 		}
+		logger.Debug("Updating existing resource")
 		if err := existing.Update(serv.lookup, res); err != nil {
 			logger.Errorw("Error updating existing resource", "error", err)
 			return nil, err
 		}
 		res = existing
+	} else {
+		logger.Info("Resource does not exist, creating now")
 	}
 
 	// Create the parent first. Better to have a hanging parent than a hanging dependency.
-
+	logger.Debug("Checking if ID has parent")
 	parentId, hasParent := id.Parent()
 	if hasParent {
-		parentExists, err := serv.lookup.Has(ctx, parentId)
-		if err != nil {
-			logger.Errorw("Unable to check if parent exists", "parent-id", parentId, "error", err)
+		logger = logger.With("parent-id", parentId)
+		parent, err := serv.lookup.Lookup(logger.AttachToContext(ctx), parentId)
+		_, isParentNotFoundErr := err.(*fferr.KeyNotFoundError)
+		if err != nil && !isParentNotFoundErr {
+			logger.Errorw("Unable to check if parent exists", "error", err)
 			return nil, err
 		}
-
-		if !parentExists {
-			logger.Debug("Parent does not exist, creating new parent")
+		parentFound := !isParentNotFoundErr
+		logger.With("parent-found", parentFound)
+		if !parentFound {
 			parent := init(id.Name, id.Variant)
-			logger.Infow("Parent ID", "Parent ID", parent.ID().String())
-			err = serv.lookup.Set(ctx, parentId, parent)
+			logger.Debug("Parent does not exist, creating new parent")
+			err = serv.lookup.Set(logger.AttachToContext(ctx), parentId, parent)
 			if err != nil {
-				logger.Errorw("Unable to create new parent", "parent-id", parentId, "error", err)
+				logger.Errorw("Unable to create new parent", "error", err)
 				return nil, err
 			}
 		} else {
-			if err := serv.setDefaultVariant(ctx, parentId, res.ID().Variant); err != nil {
-				logger.Errorw("Error setting default variant", "parent-id", parentId, "variant", res.ID().Variant, "error", err)
+			logger.Debug("Parent exists, setting default variant")
+			if err := serv.setDefaultVariant(logger.AttachToContext(ctx), parent, res.ID().Variant); err != nil {
+				logger.Errorw("Error setting default variant", "error", err)
 				return nil, err
 			}
 		}
 	}
 
-	if err := serv.lookup.Set(ctx, id, res); err != nil {
+	logger.Debug("Writing resource to storage")
+	if err := serv.lookup.Set(logger.AttachToContext(ctx), id, res); err != nil {
 		logger.Errorw("Error setting resource to lookup", "error", err)
 		return nil, err
 	}
+	logger.Info("Wrote resource to storage")
 
 	if serv.needsJob(res) && existing == nil {
-		logger.Info("Creating Job")
+		logger.Info("Creating tasks")
 		var taskIDs []scheduling.TaskID
 		if r, ok := res.(resourceTaskImplementation); ok {
 			taskIDs, err = r.TaskIDs()
@@ -3317,7 +3332,7 @@ func (serv *MetadataServer) genericCreate(ctx context.Context, res Resource, ini
 
 		// This logic is going to get removed. Currently exists to support the old coordinator logic.
 		for _, id := range taskIDs {
-			logger.Info("Creating Job", res.ID().Name, res.ID().Variant)
+			logger.Info("Creating task run")
 			trigger := scheduling.OnApplyTrigger{TriggerName: "Apply"}
 			taskName := fmt.Sprintf("Create Resource %s (%s)", res.ID().Name, res.ID().Variant)
 			taskRun, err := serv.taskManager.CreateTaskRun(ctx, taskName, id, trigger)
@@ -3330,79 +3345,74 @@ func (serv *MetadataServer) genericCreate(ctx context.Context, res Resource, ini
 
 	}
 	if existing == nil {
-		if err := serv.propagateChange(ctx, res); err != nil {
-			logger.Errorw("Propagate change error", "error", err)
+		logger.Debug("Propogating change")
+		if err := serv.propagateChange(logger.AttachToContext(ctx), res); err != nil {
+			logger.Errorw("Failed to propogate change", "error", err)
 			return nil, err
 		}
 	}
 	return &pb.Empty{}, nil
 }
 
-func (serv *MetadataServer) setDefaultVariant(ctx context.Context, id ResourceID, defaultVariant string) error {
+func (serv *MetadataServer) setDefaultVariant(ctx context.Context, parent Resource, defaultVariant string) error {
 	logger := logging.GetLoggerFromContext(ctx)
-	parent, err := serv.lookup.Lookup(ctx, id)
-	if err != nil {
-		logger.Errorw("Unable to lookup parent", "parent-id", id, "error", err)
-		return err
-	}
+	logger.With("new-default-variant", defaultVariant)
+	logger.Debug("Setting default variant")
 	var parentResource Resource
 	if resource, ok := parent.(*sourceResource); ok {
+		logger.Debug("Parent is source")
 		resource.serialized.DefaultVariant = defaultVariant
 		parentResource = resource
 	}
 	if resource, ok := parent.(*labelResource); ok {
+		logger.Debug("Parent is label")
 		resource.serialized.DefaultVariant = defaultVariant
 		parentResource = resource
 	}
 	if resource, ok := parent.(*featureResource); ok {
+		logger.Debug("Parent is feature")
 		resource.serialized.DefaultVariant = defaultVariant
 		parentResource = resource
 	}
 	if resource, ok := parent.(*trainingSetResource); ok {
+		logger.Debug("Parent is training set")
 		resource.serialized.DefaultVariant = defaultVariant
 		parentResource = resource
 	}
-	logger.Debugw("set default variant for source", "source", parent.ID().Name, "source type", parent.ID().Type, "variant", defaultVariant)
-	err = serv.lookup.Set(ctx, id, parentResource)
-	if err != nil {
-		logger.Errorw("unable to set default variant", "parent-id", id, "error", err)
+	logger.Debug("Writing parent back")
+	if err := serv.lookup.Set(logger.AttachToContext(ctx), parent.ID(), parentResource); err != nil {
+		logger.Errorw("Unable to set default variant")
 		return err
 	}
+	logger.Debug("Successfully set default variant")
 	return nil
 }
-func (serv *MetadataServer) validateExisting(newRes Resource, existing Resource) error {
+func (serv *MetadataServer) validateExisting(ctx context.Context, newRes Resource, existing Resource) error {
 	// It's possible we found a resource with the same name and variant but different contents, if different contents
 	// we'll let the user know to ideally use a different variant
 	// i.e. user tries to register transformation with same name and variant but different definition.
-	_, isResourceVariant := newRes.(ResourceVariant)
-	if isResourceVariant {
-		isEquivalent, err := serv.isEquivalent(newRes, existing)
+	logger := logging.GetLoggerFromContext(ctx)
+	logger.Debug("Checking if new resource is equivalent to the existing one to avoid breaking immutability")
+	newResVariant, newIsVariant := newRes.(ResourceVariant)
+	existingVariant, existingIsVariant := existing.(ResourceVariant)
+	if newIsVariant != existingIsVariant {
+		logger.Errorw("Resource change breaks immutability, one does not support ResourceVariant")
+		return fferr.NewResourceChangedError(newRes.ID().Name, newRes.ID().Variant, fferr.ResourceType(newRes.ID().Type), nil)
+	}
+	if existingIsVariant && newIsVariant {
+		logger.Debug("Running isEquivalent")
+		isEquivalent, err := newResVariant.IsEquivalent(existingVariant)
 		if err != nil {
-			return err
+			logger.Errorw("Unable to check if resource change breaks immutability", "err", err)
+			return fferr.NewInternalError(err)
 		}
 		if !isEquivalent {
+			logger.Error("Resource change breaks immutability")
 			return fferr.NewResourceChangedError(newRes.ID().Name, newRes.ID().Variant, fferr.ResourceType(newRes.ID().Type), nil)
 		}
 	}
+	logger.Debug("Resource doesn't implement isEquivalent, it can be overwritten")
 	return nil
-}
-
-func (serv *MetadataServer) isEquivalent(newRes Resource, existing Resource) (bool, error) {
-	serv.Logger.Debugw("Checking resource variant equivalence", "newRes", newRes.Proto(), "existing", existing.Proto())
-
-	resVariant, ok := newRes.(ResourceVariant)
-	if !ok {
-		return false, nil
-	}
-	existingVariant, ok := existing.(ResourceVariant)
-	if !ok {
-		return false, nil
-	}
-	isEquivalent, err := resVariant.IsEquivalent(existingVariant)
-	if err != nil {
-		return false, fferr.NewInternalError(err)
-	}
-	return isEquivalent, nil
 }
 
 func (serv *MetadataServer) propagateChange(ctx context.Context, newRes Resource) error {

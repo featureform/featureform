a7dbd8064e69e898ba9460c5d849a9b1d4146c9b
anthonylasso <lasso_anthony@yahoo.com>
2025-02-04 13:27:00 -0600

    chore

--- metadata/dashboard/dashboard_list_test.go
+++ metadata/dashboard/dashboard_list_test.go
@@ -199,7 +199,7 @@ func TestGetSourceDataReturnsData(t *testing.T) {
 		logger: logger,
 	}
 
-	serv.GetNonStreamSourceData(ctx)
+	serv.GetSourceData(ctx)
 
 	iterator := provider.UnitTestIterator{}
 	var data SourceDataResponse
@@ -225,7 +225,7 @@ func TestGetSourceMissingNameOrVariantParamErrors(t *testing.T) {
 		logger: logger,
 	}
 
-	serv.GetNonStreamSourceData(ctx)
+	serv.GetSourceData(ctx)
 
 	var actualErrorMsg string
 	expectedMsg := "Error 400: Failed to fetch GetSourceData - Could not find the name or variant query parameters"
@@ -249,7 +249,7 @@ func TestGetSourceFaultyOrNilGrpcClientPanic(t *testing.T) {
 	}
 
 	didPanic := func() {
-		serv.GetNonStreamSourceData(ctx)
+		serv.GetSourceData(ctx)
 	}
 
 	assert.Panics(t, didPanic)

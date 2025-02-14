7bd93a2aa4e4ee4c84f9e75b388504c9c06f87db
Kamal Sadek <kamal@featureform.com>
2025-02-14 13:10:01 -0800

    Wrap quickstart test in try finally to release resources properly

--- tests/end_to_end/pytest/test_quickstart.py
+++ tests/end_to_end/pytest/test_quickstart.py
@@ -8,28 +8,30 @@
 import numpy as np
 
 def test_quickstart(ff_client, docker_quickstart_deployment):
-    assert docker_quickstart_deployment.start()
+    try:
+        assert docker_quickstart_deployment.start()
 
-    # Exec the definitions file downloaded by the docker_quickstart_deployment.
-    with open("./quickstart/definitions.py", "r") as file:
-        code = compile(file.read(), "definitions.py", "exec")
-        file_globals = {}
-        exec(code, file_globals)
+        # Exec the definitions file downloaded by the docker_quickstart_deployment.
+        with open("./quickstart/definitions.py", "r") as file:
+            code = compile(file.read(), "definitions.py", "exec")
+            file_globals = {}
+            exec(code, file_globals)
 
-    ff_client.apply()
+        ff_client.apply()
 
-    feature_value = ff_client.features(
-        [("avg_transactions", "quickstart")],
-        {"user": "C1214240"}
-    )
-    np.testing.assert_allclose(feature_value, [319.0])
+        feature_value = ff_client.features(
+            [("avg_transactions", "quickstart")],
+            {"user": "C1214240"}
+        )
+        np.testing.assert_allclose(feature_value, [319.0])
 
-    dataset = ff_client.training_set(
-        "fraud_training",
-        "quickstart"
-    )
-    # Just confirm that there are some values being returned.
-    # If the enumerator is empty, it will raise a StopIteration exception.
-    next(dataset)
+        dataset = ff_client.training_set(
+            "fraud_training",
+            "quickstart"
+        )
+        # Just confirm that there are some values being returned.
+        # If the enumerator is empty, it will raise a StopIteration exception.
+        next(dataset)
 
-    assert docker_quickstart_deployment.stop()
+    finally:
+        assert docker_quickstart_deployment.stop()

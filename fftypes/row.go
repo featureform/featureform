8613795043beaa34942f8760a9127f213e865ad3
anthonylasso <lasso_anthony@yahoo.com>
2025-02-21 17:23:46 -0600

    move types

---
+++ fftypes/row.go
@@ -0,0 +1,4 @@
+package types
+
+// todo: a row represents a list of featureform values, can also have extension methods
+type Row []Value
---
+++ fftypes/table.go
@@ -0,0 +1,4 @@
+package types
+
+// todo: table representation, we can use for higher level business logic or abstractions at the table level
+type Table []Row

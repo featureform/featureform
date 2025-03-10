c46708d646728440c922c0235d842c51446991ba
ff-kamal <kamal@featureform.com>
2025-03-10 16:01:10 -0700

    Adds custom ClickHouse quickstart flow (#1610)

---
+++ quickstart/static_files/clickhouse/definitions.py
@@ -0,0 +1,61 @@
+import featureform as ff
+
+
+clickhouse = ff.register_clickhouse(
+    name="clickhouse-quickstart",
+    host="172.17.0.1",  # The default Docker gateway address
+    port=9000,
+    user="default",
+    password="",
+    database="default",
+)
+
+
+redis = ff.register_redis(
+    name="redis-quickstart",
+    host="172.17.0.1",  # The default Docker gateway address
+    port=6379,
+)
+
+
+transactions = clickhouse.register_table(
+    name="transactions",
+    table="transactions",  # This is the table's name in ClickHouse
+)
+
+
+@clickhouse.sql_transformation(inputs=[transactions])
+def average_user_transaction(tr):
+    return (
+        "SELECT CustomerID as user_id, avg(TransactionAmount) "
+        "as avg_transaction_amt from {{tr}} GROUP BY user_id"
+    )
+
+
+@ff.entity
+class User:
+    avg_transactions = ff.Feature(
+        average_user_transaction[
+            ["user_id", "avg_transaction_amt"]
+        ],  # We can optional include the `timestamp_column` "timestamp" here
+        variant="quickstart",
+        type=ff.Float32,
+        inference_store=redis,
+    )
+
+    fraudulent = ff.Label(
+        transactions[["CustomerID", "IsFraud"]], 
+        variant="quickstart", 
+        type=ff.Bool,
+    )
+
+
+ff.register_training_set(
+    name="fraud_training",
+    label=User.fraudulent,
+    features=[User.avg_transactions],
+    variant="quickstart",
+)
+
+
+

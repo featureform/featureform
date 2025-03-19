cce122568603dc6e0bb62e05083a17c86c957bf7
Ali Olfat <ali@featureform.com>
2025-03-18 17:29:44 -0700

    try 45

--- provider/dynamodb.go
+++ provider/dynamodb.go
@@ -47,7 +47,7 @@ func init() {
 
 const (
 	// Default timeout when waiting for dynamoDB tables to be ready
-	defaultDynamoTableTimeout = 60 * time.Second
+	defaultDynamoTableTimeout = 45 * time.Second
 	// Serialization version to use for new tables
 	dynamoSerializationVersion = serializeV1
 	defaultMetadataTableName   = "FeatureformMetadata"

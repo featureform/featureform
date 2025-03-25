1e83fea6b7a7e316b8517b93dfda2b1b6f5c8b34
ff-kamal <kamal@featureform.com>
2025-03-25 13:26:15 -0700

    Add startup ports to rest of deployments

--- metadata/dashboard/main/main.go
+++ metadata/dashboard/main/main.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/featureform/config"
 	"github.com/featureform/config/bootstrap"
-	"github.com/featureform/health"
 	help "github.com/featureform/helpers"
 	"github.com/featureform/logging"
 	"github.com/featureform/metadata"
@@ -54,13 +53,6 @@ func main() {
 	manager, err := init.GetOrCreateTaskMetadataManager(initCtx)
 	if err != nil {
 		panic(err.Error())
-	}
-
-	apiStatusPort := help.GetEnv("API_STATUS_PORT", "8443")
-	logger.Infow("Retrieved API status port from ENV", "port", apiStatusPort)
-	if err = health.StartHttpServer(logger, apiStatusPort); err != nil {
-		logger.Errorw("Failed to start health check", "err", err)
-		panic(err)
 	}
 
 	metadataServer, err := dm.NewMetadataServer(logger, client, manager.Storage)

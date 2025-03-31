1d6eea057560fdf4cc130cc2762985479bb67d84
Ali Olfat <ali@featureform.com>
2025-03-31 16:30:00 -0700

    add mysql

---
+++ provider/msyql/value_converter.go
@@ -0,0 +1,162 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2025 FeatureForm Inc.
+//
+
+package mysql
+
+import (
+	"time"
+
+	"github.com/featureform/fferr"
+	types "github.com/featureform/fftypes"
+	"github.com/featureform/logging"
+	"github.com/featureform/provider/provider_type"
+)
+
+var MySqlConverter = Converter{}
+
+func init() {
+	Register()
+}
+
+func Register() {
+	logging.GlobalLogger.Info("Registering MySQL converter")
+	provider_type.RegisterConverter(provider_type.MySqlOffline, MySqlConverter)
+}
+
+type Converter struct{}
+
+func (c Converter) GetType(nativeType types.NativeType) (types.ValueType, error) {
+	conv, err := c.ConvertValue(nativeType, nil)
+	if err != nil {
+		return nil, err
+	}
+	return conv.Type, nil
+}
+
+// ConvertValue converts a value from its MySQL representation to a types.Value
+func (c Converter) ConvertValue(nativeType types.NativeType, value any) (types.Value, error) {
+	// Convert the value based on the native type
+	switch nativeType {
+	case "integer":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Int32,
+				Value:      nil,
+			}, nil
+		}
+		convertedValue, err := types.ConvertNumberToInt32(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.Int32,
+			Value:      convertedValue,
+		}, nil
+
+	case "bigint":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Int64,
+				Value:      nil,
+			}, nil
+		}
+		convertedValue, err := types.ConvertNumberToInt64(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.Int64,
+			Value:      convertedValue,
+		}, nil
+
+	case "float":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Float64,
+				Value:      nil,
+			}, nil
+		}
+		convertedValue, err := types.ConvertNumberToFloat64(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.Float64,
+			Value:      convertedValue,
+		}, nil
+
+	case "varchar":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.String,
+				Value:      nil,
+			}, nil
+		}
+		convertedValue, err := types.ConvertToString(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.String,
+			Value:      convertedValue,
+		}, nil
+
+	case "boolean":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Bool,
+				Value:      nil,
+			}, nil
+		}
+		convertedValue, err := types.ConvertToBool(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.Bool,
+			Value:      convertedValue,
+		}, nil
+
+	case "timestamp":
+		if value == nil {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Timestamp,
+				Value:      nil,
+			}, nil
+		}
+		if t, ok := value.(time.Time); ok {
+			return types.Value{
+				NativeType: nativeType,
+				Type:       types.Timestamp,
+				Value:      t.UTC(),
+			}, nil
+		}
+		convertedValue, err := types.ConvertDatetime(value)
+		if err != nil {
+			return types.Value{}, err
+		}
+		return types.Value{
+			NativeType: nativeType,
+			Type:       types.Timestamp,
+			Value:      convertedValue,
+		}, nil
+
+	default:
+		return types.Value{}, fferr.NewUnsupportedTypeError(string(nativeType))
+	}
+}
